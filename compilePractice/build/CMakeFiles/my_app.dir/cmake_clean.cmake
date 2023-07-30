69c749a17b184adbd2d56d72862f54f8a03d8ebb
ChongL <chongliu2021@163.com>
2023-07-31 00:17:30 +0800

    first commit

---
+++ compilePractice/build/CMakeFiles/my_app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_app.dir/main.c.o"
+  "my_app"
+  "my_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ compilePractice/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int c = a * 2;
+    printf("Hello World: %d\r\n", c);
+
+    return 0;
+}
