2f4149e4131a4d327e34793848af2b684da30fd5
Chong Liu <74563097+ImChong@users.noreply.github.com>
2023-08-04 11:58:20 +0800

    Update createLists.c

--- labuladong/00_Linked_List/00_createLinkedLists/createLists.c
+++ labuladong/00_Linked_List/00_createLinkedLists/createLists.c
@@ -6,7 +6,6 @@ struct ListNode {
     struct ListNode *next;
 };
 
-
 // 创建链表
 struct ListNode *create_list(int *array, int size) {
     struct ListNode *head = NULL;    // 头节点指针
@@ -17,7 +16,7 @@ struct ListNode *create_list(int *array, int size) {
         struct ListNode *new_node = (struct ListNode *)malloc(sizeof(struct ListNode));
         new_node->val = array[i];
         new_node->next = NULL;
-
+        
         // 如果链表为空，则将新结点设置为头结点
         if (head == NULL) {
             head = new_node;
@@ -25,10 +24,8 @@ struct ListNode *create_list(int *array, int size) {
             // 否则，将新结点追加到链表末尾
             current->next = new_node;
         }
-
         current = new_node;
     }
-
     return head;
 }
 
