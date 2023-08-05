d42bd23822503a9d5306487893a14b694004822d
Chong Liu <liuc12@miamioh.edu>
2023-08-05 23:08:07 +0800

    minSubArrayLen

---
+++ programmercarl_practice/01_Array/04_leetcode0209_minSubArrayLen/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int minSubArrayLen(int target, int *nums, int numsSize) {
+    int minLen = INT_MAX;
+    int sum = 0;
+
+    int left = 0, right = 0;
+    for (; right < numsSize; right++) {
+        sum += nums[right]; /* 添加右侧数字 */
+        while (sum >= target) { /* 当sum大于target时 */
+            int subLen = right - left + 1;  /* 计算子长度*/
+            minLen = minLen < subLen ? minLen : subLen; /* 更新最小子长度 */
+            sum -= nums[left++];    /* 去除左侧数字*/
+        }
+    }
+    return minLen == INT_MAX ? 0 : minLen;  /* 返回最小长度 */
+}
+
+int main(int argc, char const *argv[]) {
+    int nums[] = {2, 3, 1, 2, 4, 3};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int s = 7;
+
+    int result = minSubArrayLen(s, nums, numsSize);
+    printf("Minimum size of subarray whose sum is at least %d: %d\n", s, result);
+
+    return 0;
+}
