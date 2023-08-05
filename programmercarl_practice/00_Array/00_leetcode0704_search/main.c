cb3301ad3d596fb7798d82b4e8ff642d9da4abf0
Chong Liu <liuc12@miamioh.edu>
2023-08-05 16:38:53 +0800

    array binary search

---
+++ programmercarl_practice/00_Array/00_leetcode0704_search/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int search(int *nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int nums[] = {-1, 0, 3, 5, 9, 12};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int target = 0;
+
+    int result = search(nums, numsSize, target);
+    printf("Index of %d in the array: %d\n", target, result);
+
+    return 0;
+}
