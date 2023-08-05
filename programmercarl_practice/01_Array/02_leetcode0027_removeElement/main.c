e283589bd5feda72b9be9dc6404d27ab21300099
Chong Liu <liuc12@miamioh.edu>
2023-08-05 17:24:52 +0800

    array

--- programmercarl_practice/01_Array/02_leetcode0027_removeElement/main.c
+++ programmercarl_practice/01_Array/02_leetcode0027_removeElement/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int removeElement(int *nums, int numsSize, int val) {
+    int i, j;
+    for (i = 0, j = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
 }
 
 int main() {
