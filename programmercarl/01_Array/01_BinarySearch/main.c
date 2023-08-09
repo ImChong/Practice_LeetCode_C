4eb1792fb7a5d694e1c130c9f620bfd187c4ac34
Chong Liu <liuc12@miamioh.edu>
2023-08-09 13:15:51 +0800

    update

--- programmercarl/01_Array/01_BinarySearch/main.c
+++ programmercarl/01_Array/01_BinarySearch/main.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
 
 /* 704. 二分查找：https://leetcode.cn/problems/binary-search/ */
 /* https://programmercarl.com/0704.%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE */
+
+#include <stdio.h>
+
 int search(int *nums, int numsSize, int target) {
     int left = 0;
     int right = numsSize - 1;
--- programmercarl/01_Array/02_RemoveElement/main.c
+++ programmercarl/01_Array/02_RemoveElement/main.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-
 /* 27. 移除元素：https://leetcode.cn/problems/remove-element/ */
 /* https://programmercarl.com/0027.%E7%A7%BB%E9%99%A4%E5%85%83%E7%B4%A0.html */
+
+#include <stdio.h>
+
 int removeElement(int *nums, int numsSize, int val) {
-    int i, j;
-    for (i = 0, j = 0; i < numsSize; i++) {
-        if (nums[i] != val) {
-            nums[j] = nums[i];
-            j++;
+    int quick, slow;            /* 初始化快慢指针 */
+    for (quick = 0, slow = 0; quick < numsSize; quick++) {  /* 快指针遍历数组 */
+        if (nums[quick] != val) {       /* 当快指针所指向的值不等于val时*/
+            nums[slow] = nums[quick];   /* 慢指针的值等于快指针的值 */
+            slow++;                     /* 慢指针位置 + 1 */
         }
     }
-    return j;
+    return slow;    /* 返回慢指针 */
 }
 
 int main() {
