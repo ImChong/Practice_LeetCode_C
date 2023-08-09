1e1fd41df0e9c03261b696aff5a63f879f8cddee
Chong Liu <liuc12@miamioh.edu>
2023-08-09 20:40:18 +0800

    Update main.c

--- programmercarl/01_Array/03_sortedSquares/main.c
+++ programmercarl/01_Array/03_sortedSquares/main.c
@@ -5,25 +5,25 @@
 #include <stdlib.h>
 
 int *sortedSquares(int *nums, int numsSize, int *returnSize) {
-    int *result = (int *)malloc(numsSize * sizeof(int));
-    *returnSize = numsSize;
+    int *result = (int *)malloc(numsSize * sizeof(int));    /* 创建返回数组 */
+    *returnSize = numsSize;     /* 确定返回大小 */
 
-    int left = 0;
-    int right = numsSize - 1;
-    int index = numsSize - 1;
+    int left = 0;                   /* 左指针 */
+    int right = numsSize - 1;       /* 右指针 */
+    int index = numsSize - 1;       /* 当前索引：从后往前 */
 
-    while (left <= right) {
-        int leftSquare = nums[left] * nums[left];
-        int rightSquare = nums[right] * nums[right];
+    while (left <= right) {         /* 当左指针的位置 <= 右指针的位置时 */
+        int leftSquare = nums[left] * nums[left];       /* 计算左指针所指值的平方：左平方 */
+        int rightSquare = nums[right] * nums[right];    /* 计算右指针所指值的平方：右平方 */
 
-        if (leftSquare > rightSquare) {
-            result[index] = leftSquare;
-            left++;
-        } else {
-            result[index] = rightSquare;
-            right--;
+        if (leftSquare > rightSquare) {     /* 如果左平方 > 右平方 */
+            result[index] = leftSquare;     /* 返回数组当前索引的值设置为左平方 */
+            left++;                         /* 左指针位置 + 1 */
+        } else {                            /* 如果左平方 < 右平方 */
+            result[index] = rightSquare;    /* 返回数组当前索引的值设置为右平方 */
+            right--;                        /* 右指针位置 - 1 */
         }
-        index--;
+        index--;                            /* 索引值位置 - 1：从小到大排列 */
     }
     return result;
 }
