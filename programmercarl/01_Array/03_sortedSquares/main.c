a22ab76f0e1a4ee9bb5f43ed119a103b5f03665f
Chong Liu <liuc12@miamioh.edu>
2023-08-09 13:21:38 +0800

    update

--- programmercarl/01_Array/03_sortedSquares/main.c
+++ programmercarl/01_Array/03_sortedSquares/main.c
@@ -1,8 +1,9 @@
+/* 977.有序数组的平方：https://leetcode.cn/problems/squares-of-a-sorted-array/ */
+/* https://programmercarl.com/0977.%E6%9C%89%E5%BA%8F%E6%95%B0%E7%BB%84%E7%9A%84%E5%B9%B3%E6%96%B9.html */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 977.有序数组的平方：https://leetcode.cn/problems/squares-of-a-sorted-array/ */
-/* https://programmercarl.com/0977.%E6%9C%89%E5%BA%8F%E6%95%B0%E7%BB%84%E7%9A%84%E5%B9%B3%E6%96%B9.html */
 int *sortedSquares(int *nums, int numsSize, int *returnSize) {
     int *result = (int *)malloc(numsSize * sizeof(int));
     *returnSize = numsSize;
