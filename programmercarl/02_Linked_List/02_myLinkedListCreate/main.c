f0e3aef5e9850c3c006ce6e0ecabac02ad2c1f43
Chong Liu <liuc12@miamioh.edu>
2023-08-10 10:37:12 +0800

    Update main.c

--- programmercarl/02_Linked_List/02_myLinkedListCreate/main.c
+++ programmercarl/02_Linked_List/02_myLinkedListCreate/main.c
@@ -1,2 +1,49 @@
 /* 707. 设计链表：https://leetcode.cn/problems/design-linked-list/ */
 /* https://programmercarl.com/0707.%E8%AE%BE%E8%AE%A1%E9%93%BE%E8%A1%A8.html */
+#include <stdio.h>
+
+/* TODO: 目标函数 */
+typedef struct {
+
+} MyLinkedList;
+
+/* TODO: 目标函数 */
+MyLinkedList* myLinkedListCreate() {
+
+}
+
+/* TODO: 目标函数 */
+int myLinkedListGet(MyLinkedList* obj, int index) {
+
+}
+
+/* TODO: 目标函数 */
+void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
+
+}
+
+/* TODO: 目标函数 */
+void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
+
+}
+
+/* TODO: 目标函数 */
+void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
+
+}
+
+/* TODO: 目标函数 */
+void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
+
+}
+
+/* TODO: 目标函数 */
+void myLinkedListFree(MyLinkedList* obj) {
+
+}
+
+/* 主函数 */
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
