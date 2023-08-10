b78bf275917c3f54cb89f58fbe02885b33fa876d
Chong Liu <liuc12@miamioh.edu>
2023-08-10 09:08:05 +0800

    update

--- programmercarl/02_Linked_List/00_common/linkedListCommon.c
+++ programmercarl/02_Linked_List/00_common/linkedListCommon.c
@@ -1,7 +1,5 @@
-/* 203. 移除链表元素：https://leetcode.cn/problems/remove-linked-list-elements/ */
-/* https://programmercarl.com/0203.%E7%A7%BB%E9%99%A4%E9%93%BE%E8%A1%A8%E5%85%83%E7%B4%A0.html */
-
 #include <stdio.h>
+#include <stdlib.h>
 
 /* 节点定义 */
 struct ListNode {
@@ -15,16 +13,16 @@ struct ListNode *createList(int *array, int size) {
     struct ListNode *current = NULL;    /* 当前节点指针 */
 
     for (int i = 0; i < size; i++) {
-        // 创建新结点
+        /* 创建新结点 */
         struct ListNode *new_node = (struct ListNode *)malloc(sizeof(struct ListNode));
         new_node->val = array[i];
         new_node->next = NULL;
-        
-        // 如果链表为空，则将新结点设置为头结点
+
+        /* 如果链表为空，则将新结点设置为头结点 */
         if (head == NULL) {
             head = new_node;
         } else {
-            // 否则，将新结点追加到链表末尾
+            /* 否则，将新结点追加到链表末尾 */
             current->next = new_node;
         }
         current = new_node;
@@ -41,14 +39,9 @@ void printList(struct ListNode *listPtr) {
     printf("\n");
 }
 
-/* TODO: 题目函数 */
-struct ListNode* removeElements(struct ListNode* head, int val){
-    struct ListNode dummy;
-    dummy.next = head;
-    return 0;
-}
-
-int main(int argc, const char* argv[]) {
+/* 主函数 */
+int main()
+{
     int arr[5] = {1, 2, 3, 4, 5};
     struct ListNode *ansList = createList(arr, 5);
     printList(ansList);
