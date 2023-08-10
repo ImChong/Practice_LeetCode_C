568cf63102b7140be6cb8281418575b95a3c83af
Chong Liu <liuc12@miamioh.edu>
2023-08-10 08:40:20 +0800

    update

---
+++ programmercarl/02_Linked_List/01_removeElements/main_chong.c
@@ -0,0 +1,17 @@
+/* 203. 移除链表元素：https://leetcode.cn/problems/remove-linked-list-elements/ */
+/* https://programmercarl.com/0203.%E7%A7%BB%E9%99%A4%E9%93%BE%E8%A1%A8%E5%85%83%E7%B4%A0.html */
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+
+    return 0;
+}
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
