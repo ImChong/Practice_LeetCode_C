2c1b64d8a09524dbe14b68c2fa37bd0bbc15ec89
Chong Liu <liuc12@miamioh.edu>
2023-08-11 10:53:15 +0800

    Update main_chong.c

--- programmercarl/02_Linked_List/02_myLinkedListCreate/main_chong.c
+++ programmercarl/02_Linked_List/02_myLinkedListCreate/main_chong.c
@@ -1,5 +1,6 @@
 /* 707. 设计链表：https://leetcode.cn/problems/design-linked-list/ */
 /* https://programmercarl.com/0707.%E8%AE%BE%E8%AE%A1%E9%93%BE%E8%A1%A8.html */
+
 #include <stdio.h>
 #include <stdlib.h>
 
