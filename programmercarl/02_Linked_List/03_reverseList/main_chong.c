d12ce78fb3738a7dfc37547abf20ac69af7430e1
Chong Liu <liuc12@miamioh.edu>
2023-08-12 13:36:02 +0800

    update

--- programmercarl/02_Linked_List/03_reverseList/main_chong.c
+++ programmercarl/02_Linked_List/03_reverseList/main_chong.c
@@ -2,6 +2,7 @@
 /* https://programmercarl.com/0206.%E7%BF%BB%E8%BD%AC%E9%93%BE%E8%A1%A8.html */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /* NOTE: 节点定义 */
 struct ListNode {
@@ -32,14 +33,37 @@ struct ListNode *createList(int *array, int size) {
     return head;
 }
 
+/* NOTE: 打印链表 */
+void printList(struct ListNode *listPtr) {
+    printf("linked list: ");
+    while (listPtr != NULL) {
+        printf("%d ", listPtr->val);
+        listPtr = listPtr->next;
+    }
+    printf("\n");
+}
+
 /* TODO: 目标函数 */
 struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = head;
+    struct ListNode *next = NULL;
 
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
 }
 
 /* NOTE: 主函数 */
 int main(int argc, char const *argv[]) {
-
-
+    int arr[5] = {1, 2, 3, 4, 5};
+    struct ListNode *newList = createList(arr, 5);
+    printList(newList);
+    struct ListNode *ansList = reverseList(newList);
+    printList(ansList);
     return 0;
 }
