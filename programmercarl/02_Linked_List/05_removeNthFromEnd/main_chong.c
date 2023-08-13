871d466ce6a1f246b1197a313f0dd83dfdfd9cd4
Chong Liu <liuc12@miamioh.edu>
2023-08-13 11:00:36 +0800

    Update main_chong.c

--- programmercarl/02_Linked_List/05_removeNthFromEnd/main_chong.c
+++ programmercarl/02_Linked_List/05_removeNthFromEnd/main_chong.c
@@ -2,7 +2,36 @@
 /* https://programmercarl.com/0019.%E5%88%A0%E9%99%A4%E9%93%BE%E8%A1%A8%E7%9A%84%E5%80%92%E6%95%B0%E7%AC%ACN%E4%B8%AA%E8%8A%82%E7%82%B9.html */
 
 #include <stdio.h>
+#include <stdlib.h>
 
+/* NOTE: 节点定义 */
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+/* NOTE: 创建链表，不使用虚拟头结点 */
+struct ListNode *createList(int *array, int size) {
+    struct ListNode *head = NULL;       /* 头节点指针 */
+    struct ListNode *current = NULL;    /* 当前节点指针 */
+
+    for (int i = 0; i < size; i++) {
+        /* 创建新结点 */
+        struct ListNode *new_node = (struct ListNode *)malloc(sizeof(struct ListNode));
+        new_node->val = array[i];
+        new_node->next = NULL;
+
+        /* 如果链表为空，则将新结点设置为头结点 */
+        if (head == NULL) {
+            head = new_node;
+        } else {
+            /* 否则，将新结点追加到链表末尾 */
+            current->next = new_node;
+        }
+        current = new_node;
+    }
+    return head;
+}
 
 /* TODO: 目标函数 */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
