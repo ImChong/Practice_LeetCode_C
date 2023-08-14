f0f4ec12ad8e847713ee4de4109336e310a2262a
Chong Liu <liuc12@miamioh.edu>
2023-08-14 15:03:46 +0800

    Update main_chong.c

--- programmercarl/02_Linked_List/07_detectCycle/main_chong.c
+++ programmercarl/02_Linked_List/07_detectCycle/main_chong.c
@@ -4,6 +4,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* NOTE: 节点定义 */
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+/* NOTE: 创建链表，不使用虚拟头结点 */
+struct ListNode *createList(int *array, int size) {
+    struct ListNode *head = NULL;       /* 头节点指针 */
+    struct ListNode *current = NULL;    /* 当前节点指针 */
+
+    for (int i = 0; i < size; i++) {
+        /* 创建新结点 */
+        struct ListNode *new_node = (struct ListNode *)malloc(sizeof(struct ListNode));
+        new_node->val = array[i];
+        new_node->next = NULL;
+
+        /* 如果链表为空，则将新结点设置为头结点 */
+        if (head == NULL) {
+            head = new_node;
+        } else {
+            /* 否则，将新结点追加到链表末尾 */
+            current->next = new_node;
+        }
+        current = new_node;
+    }
+    return head;
+}
+
+/* NOTE: 打印链表 */
+void printList(struct ListNode *listPtr) {
+    while (listPtr != NULL) {
+        printf("%d ", listPtr->val);
+        listPtr = listPtr->next;
+    }
+    printf("\n");
+}
+
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
@@ -13,6 +51,7 @@ struct ListNode *detectCycle(struct ListNode *head) {
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
+/* NOTE: 主函数 */
 int main(int argc, const char* argv[]) {
 
     return 0;
