27b3eb10163830c95cf65c104ad8f4968f7105ee
Chong Liu <liuc12@miamioh.edu>
2023-08-14 20:33:00 +0800

    Update main_chong.c

--- programmercarl/02_Linked_List/07_detectCycle/main_chong.c
+++ programmercarl/02_Linked_List/07_detectCycle/main_chong.c
@@ -48,14 +48,22 @@ void printList(struct ListNode *listPtr) {
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
 struct ListNode *detectCycle(struct ListNode *head) {
-    struct ListNode *slow = head;
-    struct ListNode *fast = head;
-
+    struct ListNode *slow = head, *fast = head;
     while (fast && fast->next) {
-        break;
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            slow = head;
+            while (slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
     }
     return NULL;
 }
+
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
