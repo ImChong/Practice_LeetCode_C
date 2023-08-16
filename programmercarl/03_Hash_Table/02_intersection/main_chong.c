c1490909da9c0d1aa7ebadf3a2a12e2a3b7a2040
Chong Liu <liuc12@miamioh.edu>
2023-08-16 14:46:04 +0800

    Update main_chong.c

--- programmercarl/03_Hash_Table/02_intersection/main_chong.c
+++ programmercarl/03_Hash_Table/02_intersection/main_chong.c
@@ -2,13 +2,46 @@
 /* https://programmercarl.com/0349.%E4%B8%A4%E4%B8%AA%E6%95%B0%E7%BB%84%E7%9A%84%E4%BA%A4%E9%9B%86.html */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int *intersection(int *nums1, int nums1Size, int *nums2, int nums2Size, int *returnSize) {
-
-    return 0;
+/* ==================================================================================================== */
+/* ==================================================================================================== */
+int cmp(const void *a, const void *b) {
+    return *(int *)a - *(int *)b;
 }
 
-int main(int argc, char const *argv[]) {
+/* TODO: 目标函数 */
+int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
+    qsort(nums1, nums1Size, sizeof(int), cmp);
+    qsort(nums2, nums2Size, sizeof(int), cmp);
+    int *res = (int *)malloc(sizeof(int) * (nums1Size < nums2Size ? nums1Size : nums2Size));
+    int i = 0, j = 0, k = 0;
+    while (i < nums1Size && j < nums2Size) {
+        if (nums1[i] == nums2[j]) {
+            if (k == 0 || res[k - 1] != nums1[i]) {
+                res[k++] = nums1[i];
+            }
+            i++;
+            j++;
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    *returnSize = k;
+    return res;
+}
+/* ==================================================================================================== */
+/* ==================================================================================================== */
 
+/* NOTE:主函数 */
+int main(int argc, char const *argv[]) {
+    int nums1[] = {1, 2, 2, 1};
+    int nums2[] = {2, 2}; 
+    int returnSize = 0;
+
+    int *ans = intersection(nums1, 4, nums2, 2, &returnSize);
+    printf("return: %d\n", ans[0]);
     return 0;
 }
