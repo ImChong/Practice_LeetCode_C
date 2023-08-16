7e24b380f4a0266e42f0cac83e759dd29df040a7
Chong Liu <liuc12@miamioh.edu>
2023-08-16 19:54:00 +0800

    Update main_chong.c

--- programmercarl/03_Hash_Table/02_intersection/main_chong.c
+++ programmercarl/03_Hash_Table/02_intersection/main_chong.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/* NOTE: 打印数组 */
 void printArray(int* arr, int arrSize) {
     printf("Arr: ");
     for (int i = 0; i < arrSize; i++) {
@@ -15,37 +15,37 @@ void printArray(int* arr, int arrSize) {
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
+/* NOTE: 比较函数 */
 int cmp(const void *a, const void *b) {
     return *(int *)a - *(int *)b;
 }
 
 /* TODO: 目标函数 */
 int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
-    qsort(nums1, nums1Size, sizeof(int), cmp);
-    qsort(nums2, nums2Size, sizeof(int), cmp);
-    int *res = (int *)malloc(sizeof(int) * (nums1Size < nums2Size ? nums1Size : nums2Size));
-    int i = 0, j = 0, k = 0;
-    while (i < nums1Size && j < nums2Size) {
-        if (nums1[i] == nums2[j]) {
-            if (k == 0 || res[k - 1] != nums1[i]) {
-                res[k++] = nums1[i];
+    qsort(nums1, nums1Size, sizeof(int), cmp);          /* 将数组1排序 */
+    qsort(nums2, nums2Size, sizeof(int), cmp);          /* 将数组2排序 */
+    int *res = (int *)malloc(sizeof(int) * (nums1Size < nums2Size ? nums1Size : nums2Size));    /* 创建一个新数组用于保存结果 */
+    int i = 0, j = 0, k = 0;                            /* 创建3个索引 */
+    while (i < nums1Size && j < nums2Size) {            /* 当i和j索引没有出界时，保持循环 */
+        if (nums1[i] == nums2[j]) {                         /* 如果 数组1索引i的值 等于 数组2索引j的值 */    
+            if (k == 0 || res[k - 1] != nums1[i]) {             /* 结果去除重复元素 */
+                res[k++] = nums1[i];                                /* 将相同元素添加至结果数组 */
             }
-            i++;
-            j++;
-        } else if (nums1[i] < nums2[j]) {
-            i++;
-        } else {
-            j++;
+            i++;                                                /* i向后移动一位 */
+            j++;                                                /* j向后移动一位 */
+        } else if (nums1[i] < nums2[j]) {                   /* 如果 数组1索引i的值 小于 数组2索引j的值*/
+            i++;                                                /* i向后移动一位 */
+        } else {                                            /* 反之 */
+            j++;                                                /* j向后移动一位 */
         }
     }
-    *returnSize = k;
-    return res;
+    *returnSize = k;                                    /* 返回数组大小为k */
+    return res;                                         /* 返回结果数组 */
 }
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
 /* NOTE:主函数 */
-/* FIXME: 用chatgpt生成更合适的答案 */
 int main(int argc, char const *argv[]) {
     // NOTE: Test case 1
     printf("======== Case 1 ======== \n");
