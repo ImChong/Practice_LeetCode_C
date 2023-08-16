66e3b1a99a9ad3315bb8ea063e9ff914e4a9ca79
Chong Liu <liuc12@miamioh.edu>
2023-08-16 12:21:15 +0800

    Update main_chong.c

--- programmercarl/03_Hash_Table/01_isAnagram/main_chong.c
+++ programmercarl/03_Hash_Table/01_isAnagram/main_chong.c
@@ -3,21 +3,46 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
 bool isAnagram(char *s, char *t) {
-    
-    return 0;
+    int sLen = strlen(s);
+    int tLen = strlen(t);
+    if (sLen != tLen) {
+        return false;
+    }
+
+    int count[26] = {0};
+    for (int i = 0; i < sLen; i++) {
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (count[i] != 0) {
+            return false;
+        }
+    }
+    return true;
 }
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
 /* 主函数 */
 int main(int argc, char const *argv[]) {
+    // NOTE: Test case 1
+    printf("======== Case 1 ======== \n");
+    char s1[] = "anagram";
+    char t1[] = "nagaram";
+    printf("Test case 1: %d\n", isAnagram(s1, t1));
 
-
+    // NOTE: Test case 2
+    printf("======== Case 2 ======== \n");
+    char s2[] = "rat";
+    char t2[] = "car";
+    printf("Test case 2: %d\n", isAnagram(s2, t2));
 
     return 0;
 }
