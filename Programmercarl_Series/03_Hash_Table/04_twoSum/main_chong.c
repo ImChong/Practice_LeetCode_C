51bf0826005f0eb8d7c9446230a2663fb556c2df
Chong Liu <liuc12@miamioh.edu>
2023-08-17 14:39:29 +0800

    Update main_chong.c

--- Programmercarl_Series/03_Hash_Table/04_twoSum/main_chong.c
+++ Programmercarl_Series/03_Hash_Table/04_twoSum/main_chong.c
@@ -1,5 +1,6 @@
 /* 1. 两数之和：https://leetcode.cn/problems/two-sum/ */
 /* https://programmercarl.com/0001.%E4%B8%A4%E6%95%B0%E4%B9%8B%E5%92%8C.html */
+https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0001.%E4%B8%A4%E6%95%B0%E4%B9%8B%E5%92%8C.md
 
 #include <stdio.h>
 
