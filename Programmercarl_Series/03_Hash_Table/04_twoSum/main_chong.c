b510568d18dc4d1a9c92ea9fdb6d13f49e4685af
Chong Liu <liuc12@miamioh.edu>
2023-08-19 06:45:13 +0800

    Update main_chong.c

--- Programmercarl_Series/03_Hash_Table/04_twoSum/main_chong.c
+++ Programmercarl_Series/03_Hash_Table/04_twoSum/main_chong.c
@@ -8,7 +8,18 @@
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
-
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int *result = (int *)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
     return NULL;
 }
 /* ==================================================================================================== */
