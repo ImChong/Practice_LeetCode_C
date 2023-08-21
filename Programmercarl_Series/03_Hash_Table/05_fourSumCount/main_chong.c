5a9ee57eae5fbc43d606d1758918d8047780ccdf
Chong Liu <liuc12@miamioh.edu>
2023-08-21 19:15:53 +0800

    Update main_chong.c

--- Programmercarl_Series/03_Hash_Table/05_fourSumCount/main_chong.c
+++ Programmercarl_Series/03_Hash_Table/05_fourSumCount/main_chong.c
@@ -4,11 +4,22 @@
 
 #include <stdio.h>
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 int fourSumCount(int* nums1, int nums1Size, int* nums2, int nums2Size, int* nums3, int nums3Size, int* nums4, int nums4Size) {
+    
     return 0;
 }
 
 int main(int argc, char const *argv[]) {
+    // NOTE: Test case 1
+    printf("======== Case 1 ======== \n");
+    int nums1[] = {1, 2};
+    int nums2[] = {-2, -1};
+    int nums3[] = {-1, 2};
+    int nums4[] = {0, 2};
+    int result = fourSumCount(nums1, ARRAY_SIZE(nums1), nums2, ARRAY_SIZE(nums2), nums3, ARRAY_SIZE(nums3), nums4, ARRAY_SIZE(nums4));
+    printf("result: %d\n", result);
 
     return 0;
 }
