7b553b7c530578b290397eaeb09a7bd9b33cbd49
Chong Liu <liuc12@miamioh.edu>
2023-08-21 20:35:17 +0800

    Update main_chong.c

--- Programmercarl_Series/03_Hash_Table/07_threeSum/main_chong.c
+++ Programmercarl_Series/03_Hash_Table/07_threeSum/main_chong.c
@@ -4,13 +4,43 @@
 
 #include <stdio.h>
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 /* TODO：目标函数 */
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
 
+    return NULL;
+}
+
+/* 打印结果 */
+void printAns(int **result, int returnSize, int* returnColumnSizes) {
+    for (int i = 0; i < returnSize; i++) {
+        printf("Ans[%d]: ", i);
+        for (int j = 0; j < returnColumnSizes[0]; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/* 释放结果空间 */
+void freeAns(int **result, int** returnColumnSizes) {
+    for (int i = 0; i < returnSize; i++) {
+        free(result[i]);
+    }
+    free(result);
+    free(returnColumnSizes);
 }
 
 /* 主函数 */
 int main(int argc, const char* argv[]) {
+    int nums[] = {-1, 0, 1, 2, -1, -4};
 
+    int returnSize = 0;
+    int *returnColumnSizes;
+
+    int **result = threeSum(nums, ARRAY_SIZE(nums), &returnSize, &returnColumnSizes);
+    printAns(result, returnSize, returnColumnSizes);
+    freeAns(result, returnColumnSizes)
     return 0;
 }
