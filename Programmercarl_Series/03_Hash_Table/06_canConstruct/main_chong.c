196a833937c6b397d5e6bf34657149a3a61e012b
Chong Liu <liuc12@miamioh.edu>
2023-08-21 19:59:11 +0800

    Update main_chong.c

--- Programmercarl_Series/03_Hash_Table/06_canConstruct/main_chong.c
+++ Programmercarl_Series/03_Hash_Table/06_canConstruct/main_chong.c
@@ -7,6 +7,18 @@
 
 bool canConstruct(char * ransomNote, char * magazine){
     int counter[26] = {0};
+
+    for (int i = 0; magazine[i] != '\0'; i++) {
+        counter[magazine[i] - 'a']++;
+    }
+
+    for (int i = 0; ransomNote[i] != '\0'; i++) {
+        counter[ransomNote[i] - 'a']--;
+
+        if (counter[ransomNote[i] - 'a'] < 0) {
+            return false;
+        }
+    }
     return true;
 }
 
