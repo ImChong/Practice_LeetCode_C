e68493c14cf253a4b2684d04a5fd28054c89908f
Chong Liu <liuc12@miamioh.edu>
2023-08-22 15:34:52 +0800

    update

--- Programmercarl_Series/06_Stack_Queue/01_myQueueCreate/main_chong.c
+++ Programmercarl_Series/06_Stack_Queue/01_myQueueCreate/main_chong.c
@@ -1,1 +1,10 @@
 /* 232. 用栈实现队列：https://leetcode.cn/problems/implement-queue-using-stacks/ */
+/* https://programmercarl.com/0232.%E7%94%A8%E6%A0%88%E5%AE%9E%E7%8E%B0%E9%98%9F%E5%88%97.html */
+/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0232.%E7%94%A8%E6%A0%88%E5%AE%9E%E7%8E%B0%E9%98%9F%E5%88%97.md */
+
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
--- Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong.c
+++ Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong.c
@@ -1,1 +1,10 @@
 /* 225. 用队列实现栈：https://leetcode.cn/problems/implement-stack-using-queues/ */
+
+/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.md */
+
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
