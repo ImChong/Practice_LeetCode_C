088a6bfd67fac2aedc9ef7d179a5a2affb8fa77c
Chong Liu <liuc12@miamioh.edu>
2023-08-22 15:35:20 +0800

    Update main_chong.c

--- Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong.c
+++ Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong.c
@@ -1,5 +1,5 @@
 /* 225. 用队列实现栈：https://leetcode.cn/problems/implement-stack-using-queues/ */
-
+/* https://programmercarl.com/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.html */
 /* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.md */
 
 #include <stdio.h>
