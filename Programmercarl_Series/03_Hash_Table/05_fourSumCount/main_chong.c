f08031c15f093a6faef885cd74643ed21fc28787
Chong Liu <liuc12@miamioh.edu>
2023-08-22 14:48:03 +0800

    Update main_chong.c

--- Programmercarl_Series/03_Hash_Table/05_fourSumCount/main_chong.c
+++ Programmercarl_Series/03_Hash_Table/05_fourSumCount/main_chong.c
@@ -3,12 +3,17 @@
 /* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0454.%E5%9B%9B%E6%95%B0%E7%9B%B8%E5%8A%A0II.md */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
-
+struct HashNode {
+    int key;
+    int val;
+    struct HashNode *next;
+}
 
 /* TODO：目标函数 */
 int fourSumCount(int* nums1, int nums1Size, int* nums2, int nums2Size, int* nums3, int nums3Size, int* nums4, int nums4Size) {
