1f70e802bb0347da1f578b38584e099f535024c3
Chong Liu <liuc12@miamioh.edu>
2023-08-23 23:22:40 +0800

    Update main_chong.c

--- Programmercarl_Series/05_Double_Pointer/09_threeSum/main_chong.c
+++ Programmercarl_Series/05_Double_Pointer/09_threeSum/main_chong.c
@@ -1,4 +1,4 @@
-/* 第15题. 三数之和: */
+/* 第15题. 三数之和: https://leetcode.cn/problems/3sum/ */
 /* https://programmercarl.com/0015.%E4%B8%89%E6%95%B0%E4%B9%8B%E5%92%8C.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE */
 
 #include <stdio.h>
