8cdad449dff5f5d87cb4aa5f0ab2681167405fec
Chong Liu <liuc12@miamioh.edu>
2023-08-23 23:10:11 +0800

    Update main_chong.c

--- Programmercarl_Series/04_String/06_strStr/main_chong.c
+++ Programmercarl_Series/04_String/06_strStr/main_chong.c
@@ -1,6 +1,21 @@
 /* 28. 实现 strStr()： https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/ */
 /* https://programmercarl.com/0028.%E5%AE%9E%E7%8E%B0strStr.html */
+/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0028.%E5%AE%9E%E7%8E%B0strStr.md */
 
+#include <stdio.h>
+
+/* ==================================================================================================== */
+/* ==================================================================================================== */
+/* TODO: 目标函数 */
 int strStr(char * haystack, char * needle){
 
+    return 0;
+}
+/* ==================================================================================================== */
+/* ==================================================================================================== */
+
+/* NOTE：主函数 */
+int main(int argc, const char* argv[]) {
+
+    return 0;
 }
