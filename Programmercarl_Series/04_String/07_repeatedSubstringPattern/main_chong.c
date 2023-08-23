28f7a6dd35bd87b4378317b7ed8168e18f679161
Chong Liu <liuc12@miamioh.edu>
2023-08-23 23:11:23 +0800

    Update main_chong.c

--- Programmercarl_Series/04_String/07_repeatedSubstringPattern/main_chong.c
+++ Programmercarl_Series/04_String/07_repeatedSubstringPattern/main_chong.c
@@ -1,8 +1,22 @@
 /* 459. 重复的子字符串： https://leetcode.cn/problems/repeated-substring-pattern/ */
 /* https://programmercarl.com/0459.%E9%87%8D%E5%A4%8D%E7%9A%84%E5%AD%90%E5%AD%97%E7%AC%A6%E4%B8%B2.html */
+/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0459.%E9%87%8D%E5%A4%8D%E7%9A%84%E5%AD%90%E5%AD%97%E7%AC%A6%E4%B8%B2.md */
 
+#include <stdio.h>
 #include <stdbool.h>
 
+/* ==================================================================================================== */
+/* ==================================================================================================== */
+/* TODO: 目标函数 */
 bool repeatedSubstringPattern(char * s){
 
+    return 0;
+}
+/* ==================================================================================================== */
+/* ==================================================================================================== */
+
+/* NOTE：主函数 */
+int main(int argc, const char* argv[]) {
+
+    return 0;
 }
