22289a37fce8343c5ff2a94db5237497afb463d9
Chong Liu <liuc12@miamioh.edu>
2023-08-24 19:34:46 +0800

    Update main_chong.c

--- Programmercarl_Series/07_Binary_Tree/01_preorderTraversal/main_chong.c
+++ Programmercarl_Series/07_Binary_Tree/01_preorderTraversal/main_chong.c
@@ -12,27 +12,47 @@ struct TreeNode {
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数: 递归遍历 */
-int* preorderTraversal(struct TreeNode* root, int* returnSize) {
-    return NULL;
+void preorder(struct TreeNode *root, int *res, int *resSize) {
+    if (root == NULL) {
+        return;
+    }
+    res[(*resSize)++] = root->val;
+    preorder(root->left, res, resSize);
+    preorder(root->right, res, resSize);
+}
+
+int* preorderTraversal_recursion(struct TreeNode* root, int* returnSize) {
+    int *res = (int *)malloc(sizeof(int) * 2000);
+    *returnSize = 0;
+    preorder(root, res, returnSize);
+    return res;
 }
 
 /* TODO: 目标函数：迭代遍历  */
-
+int* preorderTraversal_iteration(struct TreeNode* root, int* returnSize) {
+    return NULL;
+}
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
 /* NOTE：主函数 */
 int main(int argc, const char* argv[]) {
+    /* NOTE: Test case 1 */
+    printf("======== Case 1 ======== \n");
     struct TreeNode n1, n2, n3;
     n1.val = 1;
     n2.val = 2;
     n3.val = 3;
+    n1.left = NULL;
     n1.right = &n2;
     n2.left = &n3;
+    n2.right = NULL;
+    n3.left = NULL;
+    n3.right = NULL;
 
-    int returnSize;
-    int *result = preorderTraversal(&n1, &returnSize);
+    int returnSize = 0;
+    int *result = preorderTraversal_recursion(&n1, &returnSize);
 
     for (int i = 0; i < returnSize; i++) {
         printf("%d ", result[i]);
