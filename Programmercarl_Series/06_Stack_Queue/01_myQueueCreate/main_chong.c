6cdcc0d36eb438cf672b3b04c25f25fee14c38b5
Chong Liu <liuc12@miamioh.edu>
2023-08-25 00:18:13 +0800

    Update main_chong.c

--- Programmercarl_Series/06_Stack_Queue/01_myQueueCreate/main_chong.c
+++ Programmercarl_Series/06_Stack_Queue/01_myQueueCreate/main_chong.c
@@ -3,11 +3,39 @@
 /* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0232.%E7%94%A8%E6%A0%88%E5%AE%9E%E7%8E%B0%E9%98%9F%E5%88%97.md */
 
 #include <stdio.h>
+#include <stdbool.h>
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
+typedef struct {
 
+} MyQueue;
+
+
+MyQueue* myQueueCreate() {
+
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+
+}
+
+int myQueuePop(MyQueue* obj) {
+    return 0;
+}
+
+int myQueuePeek(MyQueue* obj) {
+    return 0;
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+    return false;
+}
+
+void myQueueFree(MyQueue* obj) {
+
+}
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
