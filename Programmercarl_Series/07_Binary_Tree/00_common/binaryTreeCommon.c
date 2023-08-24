d96d2eb43362a5f4883dfc72e9cc5da6199992c2
Chong Liu <liuc12@miamioh.edu>
2023-08-24 14:42:05 +0800

    update

--- Programmercarl_Series/07_Binary_Tree/00_common/binaryTreeCommon.c
+++ Programmercarl_Series/07_Binary_Tree/00_common/binaryTreeCommon.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/* NOTE: 树节点 */
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 /* NOTE: 主函数 */
 int main(int argc, const char* argv[]) {
 
--- Programmercarl_Series/07_Binary_Tree/01_preorderTraversal/main_chong.c
+++ Programmercarl_Series/07_Binary_Tree/01_preorderTraversal/main_chong.c
@@ -1,11 +1,20 @@
 /* 144.二叉树的前序遍历：https://leetcode.cn/problems/binary-tree-preorder-traversal/ */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数: 递归遍历 */
-
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    return NULL;
+}
 
 /* TODO: 目标函数：迭代遍历  */
 
@@ -15,6 +24,20 @@
 
 /* NOTE：主函数 */
 int main(int argc, const char* argv[]) {
+    struct TreeNode n1, n2, n3;
+    n1.val = 1;
+    n2.val = 2;
+    n3.val = 3;
+    n1.right = &n2;
+    n2.left = &n3;
 
+    int returnSize;
+    int *result = preorderTraversal(&n1, &returnSize);
+
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", result[i]);
+    }
+
+    free(result);
     return 0;
 }
