f5962164358a7cd478f3c34b44d22ac0649e2ad2
Chong Liu <liuc12@miamioh.edu>
2023-08-24 20:24:54 +0800

    Update binaryTreeCommon.c

--- Programmercarl_Series/07_Binary_Tree/00_common/binaryTreeCommon.c
+++ Programmercarl_Series/07_Binary_Tree/00_common/binaryTreeCommon.c
@@ -2,7 +2,11 @@
 /* 
  * 二叉树主要有两种遍历方式：
  *      深度优先遍历：先往深走，遇到叶子节点再往回走。
+ *          前序遍历（递归法，迭代法）
+ *          中序遍历（递归法，迭代法）
+ *          后序遍历（递归法，迭代法）
  *      广度优先遍历：一层一层的去遍历。
+ *          层次遍历（迭代法）
  */
 
 
@@ -14,6 +18,23 @@ struct TreeNode {
     struct TreeNode *left;
     struct TreeNode *right;
 };
+
+/* NOTE: 前序遍历 */
+void preOrder(struct TreeNode *root, int *ret, int *returnSize) {
+    if (root == NULL) {
+        return;
+    }
+    ret[(*returnSize)++] = root->val;
+    preOrder(root->left, ret, returnSize);
+    preOrder(root->right, ret, returnSize);
+}
+
+int *preorderTraversal(struct TreeNode *root, int *returnSize) {
+    int *ret = (int *)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    preOrder(root, ret, returnSize);
+    return ret;
+}
 
 /* NOTE: 主函数 */
 int main(int argc, const char* argv[]) {
