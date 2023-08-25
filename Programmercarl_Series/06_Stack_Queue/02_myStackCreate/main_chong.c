404bd77a4520eb96ec49966e2d94f3d7b5f44724
Chong Liu <liuc12@miamioh.edu>
2023-08-25 09:24:25 +0800

    Update main_chong.c

--- Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong.c
+++ Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong.c
@@ -3,11 +3,40 @@
 /* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.md */
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
+typedef struct {
 
+} MyStack;
+
+
+MyStack* myStackCreate() {
+
+}
+
+void myStackPush(MyStack* obj, int x) {
+
+}
+
+int myStackPop(MyStack* obj) {
+
+}
+
+int myStackTop(MyStack* obj) {
+
+}
+
+bool myStackEmpty(MyStack* obj) {
+
+}
+
+void myStackFree(MyStack* obj) {
+
+}
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
