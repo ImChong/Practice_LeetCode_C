c520edd3a6e3adc40d3c65eced7f9feb4fc145e5
Chong Liu <liuc12@miamioh.edu>
2023-08-25 09:35:11 +0800

    update

---
+++ Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong_single_queue.c
@@ -0,0 +1,55 @@
+/* 225. 用队列实现栈：https://leetcode.cn/problems/implement-stack-using-queues/ */
+/* https://programmercarl.com/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.html */
+/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.md */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/* ==================================================================================================== */
+/* ==================================================================================================== */
+/* TODO: 目标函数 */
+typedef struct {
+
+} MyStack;
+
+
+MyStack* myStackCreate() {
+
+}
+
+void myStackPush(MyStack* obj, int x) {
+
+}
+
+int myStackPop(MyStack* obj) {
+    return 0;
+}
+
+int myStackTop(MyStack* obj) {
+    return 0;
+}
+
+bool myStackEmpty(MyStack* obj) {
+    return false;
+}
+
+void myStackFree(MyStack* obj) {
+
+}
+/* ==================================================================================================== */
+/* ==================================================================================================== */
+
+/* NOTE：主函数 */
+int main(int argc, const char* argv[]) {
+    /* NOTE: Test case 1 */
+    printf("======== Case 1 ======== \n");
+    MyStack *myStack = myStackCreate();
+    myStackPush(myStack, 1);
+    myStackPush(myStack, 2);
+    printf("myStackTop val: %d \n", myStackTop(myStack));
+    printf("myStackPop val: %d \n", myStackPop(myStack));
+    printf("myStackEmpty: %d \n", myStackEmpty(myStack));
+    myStackFree(myStack);
+    return 0;
+}
