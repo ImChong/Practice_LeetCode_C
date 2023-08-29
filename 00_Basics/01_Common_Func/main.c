e41da2e3d66c8b7aa57a11324bf56f7be5c668cf
Chong Liu <liuc12@miamioh.edu>
2023-08-29 10:06:00 +0800

    update

---
+++ 00_Basics/01_Common_Func/main.c
@@ -0,0 +1,33 @@
+/* 本文件记录C语言常用宏定义技巧及常用函数 */
+#include <stdio.h>
+
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+/* int main(int argc, char *argv[])
+ *  Functionality: description...
+ *  Arguments: arguments...
+ *  Return: possibleReturnValues
+ *  References: document/website...
+ ***********************************************************************************
+ *  IMPORTANT NOTICE FOR READER
+ */
+int main(int argc, char *argv[])
+{
+    printf("Number of command-line arguments: %d\n", argc);
+
+    printf("Command-line arguments:\n");
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
+    return 0;
+}
+
+// $ ./program arg1 arg2 arg3
+// Number of command-line arguments: 4
+// ./program
+// arg1
+// arg2
+// arg3
--- 00_Basics/02_Common_Func/main.c
+++
@@ -1,3 +0,0 @@
-/* 本文件记录C语言常用宏定义技巧及常用函数 */
-
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
