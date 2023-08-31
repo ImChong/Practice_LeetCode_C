1846cccd0827ef259fb6cf92a7697be95a419fb0
Chong Liu <liuc12@miamioh.edu>
2023-08-31 20:32:11 +0800

    Update binaryTreeCommon.c

--- 01_Programmercarl_Series/07_Binary_Tree/00_common/binaryTreeCommon.c
+++ 01_Programmercarl_Series/07_Binary_Tree/00_common/binaryTreeCommon.c
@@ -6,7 +6,7 @@
  * =================================================================================
  * Copyright (c) 2023 Chong Liu
  * =================================================================================
- * Last Modified: Chong Liu - 2023-08-31 8:27:49 pm
+ * Last Modified: Chong Liu - 2023-08-31 8:32:10 pm
  */
 
 /* 用数组来存储二叉树: 如果父节点的数组下标是 i，那么它的左孩子就是 i * 2 + 1，右孩子就是 i * 2 + 2。*/
@@ -30,12 +30,12 @@ struct TreeNode {
 
 /* NOTE: 前序遍历: 递归 */
 void preOrder(struct TreeNode *node, int *ret, int *returnSize) {
-    if (node == NULL) {                         /* 如果当前节点为 NULL，直接返回 */
+    if (node == NULL) {                             /* 如果当前节点为 NULL，直接返回 */
         return;
     }
-    ret[(*returnSize)++] = node->val;           /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
-    preOrder(node->left, ret, returnSize);      /* 前序遍历左节点 */
-    preOrder(node->right, ret, returnSize);     /* 前序遍历右节点 */
+    ret[(*returnSize)++] = node->val;               /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
+    preOrder(node->left, ret, returnSize);          /* 前序遍历左节点 */
+    preOrder(node->right, ret, returnSize);         /* 前序遍历右节点 */
 }
 
 int *preorderTraversal(struct TreeNode *root, int *returnSize) {
@@ -47,36 +47,36 @@ int *preorderTraversal(struct TreeNode *root, int *returnSize) {
 
 /* NOTE: 中序遍历: 递归 */
 void inOrder(struct TreeNode *node, int *ret, int *returnSize) {
-    if (!node) {                                /* 如果当前节点为 NULL，直接返回 */
+    if (!node) {                                    /* 如果当前节点为 NULL，直接返回 */
         return;
     }
-    inOrder(node->left, ret, returnSize);       /* 中序遍历左节点 */
-    ret[(*returnSize)++] = node->val;           /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
-    inOrder(node->right, ret, returnSize);      /* 中序遍历右节点 */
+    inOrder(node->left, ret, returnSize);           /* 中序遍历左节点 */
+    ret[(*returnSize)++] = node->val;               /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
+    inOrder(node->right, ret, returnSize);          /* 中序遍历右节点 */
 }
 
 int *inorderTraversal(struct TreeNode *root, int *returnSize) {
-    int *ret = (int *)malloc(sizeof(int) * 100);
-    *returnSize = 0;
-    inOrder(root, ret, returnSize);
-    return ret;
+    int *ret = (int *)malloc(sizeof(int) * 100);    /* 初始化一个100长度的 ret 数组，用于储存遍历答案 */
+    *returnSize = 0;                                /* 初始化数的大小为 0 */
+    inOrder(root, ret, returnSize);                 /* 中序遍历根节点 */
+    return ret;                                     /* 返回结果 ret 数组 */
 }
 
 /* NOTE: 后序遍历: 递归 */
 void postOrder(struct TreeNode *node, int *ret, int *returnSize) {
-    if (!node) {
+    if (!node) {                                    /* 如果当前节点为 NULL，直接返回 */
         return;
     }
-    postOrder(node->left, ret, returnSize);
-    postOrder(node->right, ret, returnSize);
-    ret[(*returnSize)++] = node->val;
+    postOrder(node->left, ret, returnSize);         /* 后序遍历左节点 */
+    postOrder(node->right, ret, returnSize);        /* 后序遍历右节点 */
+    ret[(*returnSize)++] = node->val;               /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
 }
 
 int *postorderTraversal(struct TreeNode *root, int *returnSize) {
-    int *ret = (int *)malloc(sizeof(int) * 100);
-    *returnSize = 0;
-    postOrder(root, ret, returnSize);
-    return ret;
+    int *ret = (int *)malloc(sizeof(int) * 100);    /* 初始化一个100长度的 ret 数组，用于储存遍历答案 */
+    *returnSize = 0;                                /* 初始化数的大小为 0 */
+    postOrder(root, ret, returnSize);               /* 后序遍历根节点 */
+    return ret;                                     /* 返回结果 ret 数组 */
 }
 
 /* TODO: 前序遍历: 迭代 */
