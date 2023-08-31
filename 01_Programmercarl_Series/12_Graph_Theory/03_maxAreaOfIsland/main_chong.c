bd4a999eb81325d5e3f3324665cc73a2674629e7
Chong Liu <liuc12@miamioh.edu>
2023-08-31 20:08:08 +0800

    Create main_chong.c

---
+++ 01_Programmercarl_Series/12_Graph_Theory/03_maxAreaOfIsland/main_chong.c
@@ -0,0 +1,17 @@
+/*
+ * @File: main_chong.c
+ * @Created Date: 2023-08-31 8:07:32 pm
+ * @Author: Chong Liu
+ * @Brief: 695. 岛屿的最大面积: https://leetcode.cn/problems/max-area-of-island/
+ * https://programmercarl.com/0695.%E5%B2%9B%E5%B1%BF%E7%9A%84%E6%9C%80%E5%A4%A7%E9%9D%A2%E7%A7%AF.html
+ * =================================================================================
+ * Copyright (c) 2023 Chong Liu
+ * =================================================================================
+ * Last Modified: Chong Liu - 2023-08-31 8:08:06 pm
+ */
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
