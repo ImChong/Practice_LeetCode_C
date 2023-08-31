a4df0241df89a46a0a51abcbe319fb38208ed80a
Chong Liu <liuc12@miamioh.edu>
2023-08-31 20:08:47 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/12_Graph_Theory/02_numIslands/main_chong.c
+++ 01_Programmercarl_Series/12_Graph_Theory/02_numIslands/main_chong.c
@@ -2,11 +2,12 @@
  * @File: main_chong.c
  * @Created Date: 2023-08-31 8:06:22 pm
  * @Author: Chong Liu
- * @Brief: Description of what this file does
+ * @Brief: 200. 岛屿数量: https://leetcode.cn/problems/number-of-islands/
+ * https://programmercarl.com/%E5%9B%BE%E8%AE%BA%E5%B9%BF%E6%90%9C%E7%90%86%E8%AE%BA%E5%9F%BA%E7%A1%80.html
  * =================================================================================
  * Copyright (c) 2023 Chong Liu
  * =================================================================================
- * Last Modified: Chong Liu - 2023-08-31 8:06:30 pm
+ * Last Modified: Chong Liu - 2023-08-31 8:08:44 pm
  */
 #include <stdio.h>
 
