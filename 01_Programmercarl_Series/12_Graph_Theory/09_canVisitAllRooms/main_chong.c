9c86ed9325fc36245781a64885e277ca37cde896
Chong Liu <liuc12@miamioh.edu>
2023-08-31 20:18:15 +0800

    Create main_chong.c

---
+++ 01_Programmercarl_Series/12_Graph_Theory/09_canVisitAllRooms/main_chong.c
@@ -0,0 +1,21 @@
+/*
+ * @File: main_chong.c
+ * @Created Date: 2023-08-31 8:17:44 pm
+ * @Author: Chong Liu
+ * @Brief: 841. 钥匙和房间: https://leetcode.cn/problems/keys-and-rooms/
+ * https://programmercarl.com/0841.%E9%92%A5%E5%8C%99%E5%92%8C%E6%88%BF%E9%97%B4.html
+ * =================================================================================
+ * Copyright (c) 2023 Chong Liu
+ * =================================================================================
+ * Last Modified: Chong Liu - 2023-08-31 8:18:11 pm
+ */
+#include <stdio.h>
+
+bool canVisitAllRooms(int** rooms, int roomsSize, int* roomsColSize){
+
+}
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
