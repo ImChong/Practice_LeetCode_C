62f79a52fb791a0499668fe8dacbc5df66362300
Chong Liu <liuc12@miamioh.edu>
2023-08-31 20:09:53 +0800

    Create main_chong.c

---
+++ 01_Programmercarl_Series/12_Graph_Theory/04_numEnclaves/main_chong.c
@@ -0,0 +1,17 @@
+/*
+ * @File: main_chong.c
+ * @Created Date: 2023-08-31 8:09:25 pm
+ * @Author: Chong Liu
+ * @Brief: 1020. 飞地的数量: https://leetcode.cn/problems/number-of-enclaves/description/
+ * https://programmercarl.com/1020.%E9%A3%9E%E5%9C%B0%E7%9A%84%E6%95%B0%E9%87%8F.html
+ * =================================================================================
+ * Copyright (c) 2023 Chong Liu
+ * =================================================================================
+ * Last Modified: Chong Liu - 2023-08-31 8:09:50 pm
+ */
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
