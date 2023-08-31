e5f811a5f43ec5e260541271043267e23b2451fc
Chong Liu <liuc12@miamioh.edu>
2023-08-31 20:22:13 +0800

    Create main_chong.c

---
+++ 01_Programmercarl_Series/12_Graph_Theory/11_validPath/main_chong.c
@@ -0,0 +1,21 @@
+/*
+ * @File: main_chong.c
+ * @Created Date: 2023-08-31 8:21:41 pm
+ * @Author: Chong Liu
+ * @Brief: 1971. 寻找图中是否存在路径: https://leetcode.cn/problems/find-if-path-exists-in-graph/
+ * https://programmercarl.com/1971.%E5%AF%BB%E6%89%BE%E5%9B%BE%E4%B8%AD%E6%98%AF%E5%90%A6%E5%AD%98%E5%9C%A8%E8%B7%AF%E5%BE%84.html#%E6%80%9D%E8%B7%AF
+ * =================================================================================
+ * Copyright (c) 2023 Chong Liu
+ * =================================================================================
+ * Last Modified: Chong Liu - 2023-08-31 8:22:10 pm
+ */
+#include <stdio.h>
+
+bool validPath(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination){
+
+}
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
