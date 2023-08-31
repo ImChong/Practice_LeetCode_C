bdfd1affefff8cb5d08e5da01b83695d44046842
Chong Liu <liuc12@miamioh.edu>
2023-08-31 20:25:11 +0800

    Create main_chong.c

---
+++ 01_Programmercarl_Series/12_Graph_Theory/13_findRedundantDirectedConnection/main_chong.c
@@ -0,0 +1,21 @@
+/*
+ * @File: main_chong.c
+ * @Created Date: 2023-08-31 8:24:05 pm
+ * @Author: Chong Liu
+ * @Brief: 685. 冗余连接 II: https://leetcode.cn/problems/redundant-connection-ii/
+ * https://programmercarl.com/0685.%E5%86%97%E4%BD%99%E8%BF%9E%E6%8E%A5II.html
+ * =================================================================================
+ * Copyright (c) 2023 Chong Liu
+ * =================================================================================
+ * Last Modified: Chong Liu - 2023-08-31 8:24:55 pm
+ */
+#include <stdio.h>
+
+int* findRedundantDirectedConnection(int** edges, int edgesSize, int* edgesColSize, int* returnSize){
+
+}
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
