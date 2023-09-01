5482a70790d95e42411f84036653e79114d6eafe
Chong Liu <liuc12@miamioh.edu>
2023-09-01 22:59:36 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/01_preorderTraversal/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/01_preorderTraversal/main_chong.c
@@ -7,7 +7,7 @@
  * =================================================================================
  * Copyright (c) 2023 Chong Liu
  * =================================================================================
- * Last Modified: Chong Liu - 2023-09-01 10:56:16 pm
+ * Last Modified: Chong Liu - 2023-09-01 10:59:32 pm
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,42 +32,42 @@ struct TreeNode {
 /* ==================================================================================================== */
 /* DONE: 目标函数: 递归遍历 */
 void preOrder(struct TreeNode *node, int *ret, int *returnSize) {
-    if (node == NULL) {                             /* 如果当前节点为 NULL，直接返回 */
+    if (node == NULL) {                                 /* 如果当前节点为 NULL，直接返回 */
         return;
     }
-    ret[(*returnSize)++] = node->val;               /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
-    preOrder(node->left, ret, returnSize);          /* 前序遍历左节点 */
-    preOrder(node->right, ret, returnSize);         /* 前序遍历右节点 */
+    ret[(*returnSize)++] = node->val;                   /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
+    preOrder(node->left, ret, returnSize);              /* 前序遍历左节点 */
+    preOrder(node->right, ret, returnSize);             /* 前序遍历右节点 */
 }
 
 int *preorderTraversal_recursion(struct TreeNode *root, int *returnSize) {
-    int *ret = (int *)malloc(sizeof(int) * MAX_SIZE);    /* 初始化一个100长度的 ret 数组，用于储存遍历答案 */
-    *returnSize = 0;                                /* 初始化数的大小为 0 */
-    preOrder(root, ret, returnSize);                /* 前序遍历根节点 */
-    return ret;                                     /* 返回结果 ret 数组 */
+    int *ret = (int *)malloc(sizeof(int) * MAX_SIZE);   /* 初始化一个100长度的 ret 数组，用于储存遍历答案 */
+    *returnSize = 0;                                    /* 初始化数的大小为 0 */
+    preOrder(root, ret, returnSize);                    /* 前序遍历根节点 */
+    return ret;                                         /* 返回结果 ret 数组 */
 }
 
 /* DONE: 目标函数：迭代遍历 */
 int* preorderTraversal_iteration(struct TreeNode *root, int *returnSize) {
     int *res = (int *)malloc(sizeof(int) * MAX_SIZE);   /* 初始化一个2000长度的 ret 数组，用于储存遍历答案 */
-    *returnSize = 0;                                /* 初始化数的大小为 0 */
-    if (root == NULL) {                             /* 如果当前节点为 NULL，直接返回 */
+    *returnSize = 0;                                    /* 初始化数的大小为 0 */
+    if (root == NULL) {                                 /* 如果当前节点为 NULL，直接返回 */
         return res;
     }
 
     struct TreeNode *stk[MAX_SIZE];                     /* 用数组实现初始化一个2000容量的树节点栈 */
-    struct TreeNode *node = root;                   /* 获取根节点的指针 */
-    int stk_top = 0;                                /* 栈顶索引为 0 */
-    while (stk_top > 0 || node != NULL) {           /* 当栈顶索引大于0 或者 节点指针不指向NULL */
-        while (node != NULL) {                          /* 当结点指针不指向NULL */
-            res[(*returnSize)++] = node->val;               /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
-            stk[stk_top++] = node;                          /* 将当前节点指针放入栈顶，并将栈顶索引 + 1 */
-            node = node->left;                              /* 节点指针向左移动 */
+    struct TreeNode *node = root;                       /* 获取根节点的指针 */
+    int stk_top = 0;                                    /* 栈顶索引为 0 */
+    while (stk_top > 0 || node != NULL) {               /* 当栈顶索引大于0 或者 节点指针不指向NULL */
+        while (node != NULL) {                              /* 当结点指针不指向NULL */
+            res[(*returnSize)++] = node->val;                   /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
+            stk[stk_top++] = node;                              /* 将当前节点指针放入栈顶，并将栈顶索引 + 1 */
+            node = node->left;                                  /* 节点指针向左移动 */
         }
-        node = stk[--stk_top];                          /* 当节点指针指向NULL - 左节点指针已经到头：获取栈顶节点指针，并将栈顶索引 - 1 */
-        node = node->right;                             /* 节点指针向右移动 */
+        node = stk[--stk_top];                              /* 当节点指针指向NULL - 左节点指针已经到头：获取栈顶节点指针，并将栈顶索引 - 1 */
+        node = node->right;                                 /* 节点指针向右移动 */
     }
-    return res;                                     /* 返回结果数组 */
+    return res;                                         /* 返回结果数组 */
 }
 /* ==================================================================================================== */
 /* ==================================================================================================== */
