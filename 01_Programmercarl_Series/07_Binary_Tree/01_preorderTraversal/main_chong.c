d020568d83210bee040ae83bf55b7ccd181bc97c
Chong Liu <liuc12@miamioh.edu>
2023-09-01 10:34:55 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/01_preorderTraversal/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/01_preorderTraversal/main_chong.c
@@ -7,7 +7,7 @@
  * =================================================================================
  * Copyright (c) 2023 Chong Liu
  * =================================================================================
- * Last Modified: Chong Liu - 2023-09-01 10:23:39 am
+ * Last Modified: Chong Liu - 2023-09-01 10:34:45 am
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,8 +19,16 @@ struct TreeNode {
 };
 
 /* ==================================================================================================== */
+/* preorderTraversal()
+ *  Functionality: description...
+ *  Arguments: struct TreeNode *root, int *returnSize
+ *  Return: int *res
+ *  References: https://programmercarl.com/%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E8%BF%AD%E4%BB%A3%E9%81%8D%E5%8E%86.html
+ ***********************************************************************************
+ *  IMPORTANT NOTICE FOR READER
+ */
 /* ==================================================================================================== */
-/* TODO: 目标函数: 递归遍历 */
+/* DONE: 目标函数: 递归遍历 */
 void preOrder(struct TreeNode *node, int *ret, int *returnSize) {
     if (node == NULL) {                             /* 如果当前节点为 NULL，直接返回 */
         return;
@@ -37,7 +45,7 @@ int *preorderTraversal_recursion(struct TreeNode *root, int *returnSize) {
     return ret;                                     /* 返回结果 ret 数组 */
 }
 
-/* TODO: 目标函数：迭代遍历  */
+/* DONE: 目标函数：迭代遍历  */
 int* preorderTraversal_iteration(struct TreeNode *root, int *returnSize) {
     int *res = (int *)malloc(sizeof(int) * 2000);   /* 初始化一个2000长度的 ret 数组，用于储存遍历答案 */
     *returnSize = 0;                                /* 初始化数的大小为 0 */
@@ -47,26 +55,23 @@ int* preorderTraversal_iteration(struct TreeNode *root, int *returnSize) {
 
     struct TreeNode *stk[2000];                     /* 用数组实现初始化一个2000容量的树节点栈 */
     struct TreeNode *node = root;                   /* 获取根节点的指针 */
-    int stk_top = 0;                                /* 栈顶索引为0 */
+    int stk_top = 0;                                /* 栈顶索引为 0 */
     while (stk_top > 0 || node != NULL) {           /* 当栈顶索引大于0 或者 节点指针不指向NULL */
         while (node != NULL) {                          /* 当结点指针不指向NULL */
             res[(*returnSize)++] = node->val;               /* 把当前节点的值放入ret数组的 *returnSize 索引，并将 *returnSize 索引值 + 1 */
             stk[stk_top++] = node;                          /* 将当前节点指针放入栈顶，并将栈顶索引 + 1 */
             node = node->left;                              /* 节点指针向左移动 */
         }
-        node = stk[--stk_top];
-        node = node->right;
+        node = stk[--stk_top];                          /* 当节点指针指向NULL - 左节点指针已经到头：获取栈顶节点指针，并将栈顶索引 - 1 */
+        node = node->right;                             /* 节点指针向右移动 */
     }
-    return res;
+    return res;                                     /* 返回结果数组 */
 }
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
 /* NOTE：主函数 */
 int main(int argc, const char* argv[]) {
-    /* NOTE: Test case 1 */
-    printf("======== Case 1 ======== \n");
-
     /*
      * 创建以下树结构
      *
@@ -75,7 +80,6 @@ int main(int argc, const char* argv[]) {
      *  2   3
      *
      */
-
     struct TreeNode n1, n2, n3;     /* 初始化3个树节点 */
     n1.val = 1;     /* n1节点赋值为 1 */
     n2.val = 2;     /* n2节点赋值为 2 */
@@ -88,11 +92,24 @@ int main(int argc, const char* argv[]) {
     n3.right = NULL;    /* n3节点的右节点为 NULL */
 
     int returnSize = 0;
+
+    /* NOTE: Test case 1 */
+    printf("======== Case 1 ======== \n");
     int *result = preorderTraversal_recursion(&n1, &returnSize);
 
     for (int i = 0; i < returnSize; i++) {
         printf("%d ", result[i]);
     }
+    printf("\n");
+
+    /* NOTE: Test case 2 */
+    printf("======== Case 2 ======== \n");
+    result = preorderTraversal_iteration(&n1, &returnSize);
+
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", result[i]);
+    }
+    printf("\n");
 
     free(result);
     return 0;
