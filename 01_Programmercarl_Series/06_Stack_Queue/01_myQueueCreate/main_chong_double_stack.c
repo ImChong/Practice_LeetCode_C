07431e42606275539900b0ba9e4210012b871cf5
Chong Liu <liuc12@miamioh.edu>
2023-09-06 19:40:57 +0800

    Update main_chong_double_stack.c

--- 01_Programmercarl_Series/06_Stack_Queue/01_myQueueCreate/main_chong_double_stack.c
+++ 01_Programmercarl_Series/06_Stack_Queue/01_myQueueCreate/main_chong_double_stack.c
@@ -1,7 +1,16 @@
-/* 232. 用栈实现队列：https://leetcode.cn/problems/implement-queue-using-stacks/ */
-/* https://programmercarl.com/0232.%E7%94%A8%E6%A0%88%E5%AE%9E%E7%8E%B0%E9%98%9F%E5%88%97.html */
-/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0232.%E7%94%A8%E6%A0%88%E5%AE%9E%E7%8E%B0%E9%98%9F%E5%88%97.md */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\06_Stack_Queue\01_myQueueCreate\main_chong_double_stack.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-28 09:44:35
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-09-06 19:40:14
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 232. 用栈实现队列：https://leetcode.cn/problems/implement-queue-using-stacks/
+ * https://programmercarl.com/0232.%E7%94%A8%E6%A0%88%E5%AE%9E%E7%8E%B0%E9%98%9F%E5%88%97.html
+ * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0232.%E7%94%A8%E6%A0%88%E5%AE%9E%E7%8E%B0%E9%98%9F%E5%88%97.md
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -17,8 +26,13 @@ typedef struct {
     int stkCapacity;    /* 栈容量：记录栈大小的最大值 */
 } Stack;
 
-/* 创建栈 */
-Stack* stackCreate(int capacity) {
+/**
+ * @description: 创建栈
+ * =================================================================================
+ * @param {int} capacity
+ * @return {Stack *} ret
+ */
+Stack *stackCreate(int capacity) {
     Stack *ret = (Stack *)malloc(sizeof(Stack));        /* 为栈结构体分配空间 */
     ret->stk = (int *)malloc(sizeof(int) * capacity);   /* 为栈数组分配空间，大小为最大容量 */
     ret->stkSize = 0;                                   /* 当前栈大小为0 */
@@ -26,17 +40,33 @@ Stack* stackCreate(int capacity) {
     return ret;                                         /* 返回创建的栈结构体 */
 }
 
-/* 入栈 */
+/**
+ * @description: 入栈
+ * =================================================================================
+ * @param {Stack*} obj
+ * @param {int} x
+ * @return {void}
+ */
 void stackPush(Stack* obj, int x) {
     obj->stk[obj->stkSize++] = x;                       /* 将数据存入栈结构体内数组的【栈大小】索引位置，存入后将【栈大小】+ 1 */
 }
 
-/* 出栈 */
+/**
+ * @description: 出栈
+ * =================================================================================
+ * @param {Stack*} obj
+ * @return {void}
+ */
 void stackPop(Stack* obj) {
     obj->stkSize--;                                     /* 将【栈大小】- 1 */
 }
 
-/* 查看栈顶元素 */
+/**
+ * @description: 查看栈顶元素
+ * =================================================================================
+ * @param {Stack*} obj
+ * @return {int} 栈顶元素
+ */
 int stackTop(Stack* obj) {
     return obj->stk[obj->stkSize - 1];
 }
@@ -58,7 +88,13 @@ typedef struct {
 } MyQueue;
 
 /* 创建队列 */
-MyQueue* myQueueCreate() {
+/**
+ * @description:
+ * =================================================================================
+ * @param {MyQueue} *ret
+ * @return {*}
+ */
+MyQueue* myQueueCreate(void) {
     MyQueue *ret = (MyQueue *)malloc(sizeof(MyQueue));
     ret->inStack = stackCreate(100);
     ret->outStack = stackCreate(100);
@@ -109,7 +145,13 @@ void myQueueFree(MyQueue* obj) {
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
-/* NOTE：主函数 */
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc
+ * @param {char*} argv
+ * @return {int}
+ */
 int main(int argc, const char* argv[]) {
     /* NOTE: Test case 1 */
     printf("======== Case 1 ======== \n");
