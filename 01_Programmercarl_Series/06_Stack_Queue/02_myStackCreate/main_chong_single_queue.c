8c8e18e8bb93a3e2391c788d66cfc3a713b9143c
Chong Liu <liuc12@miamioh.edu>
2023-09-10 18:25:03 +0800

    Update main_chong_single_queue.c

--- 01_Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong_single_queue.c
+++ 01_Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_chong_single_queue.c
@@ -1,15 +1,23 @@
-/* 225. 用队列实现栈：https://leetcode.cn/problems/implement-stack-using-queues/ */
-/* https://programmercarl.com/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.html */
-/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.md */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\06_Stack_Queue\02_myStackCreate\main_chong_single_queue.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-25 20:30:05
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-09-10 18:24:18
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 225. 用队列实现栈：https://leetcode.cn/problems/implement-stack-using-queues/
+ * https://programmercarl.com/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.html
+ * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0225.%E7%94%A8%E9%98%9F%E5%88%97%E5%AE%9E%E7%8E%B0%E6%A0%88.md
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
 /* ==================================================================================================== */
-/* 方法二：一个队列 */
+/* 方法二：用一个队列实现栈 */
 /* ==================================================================================================== */
-/* TODO: 目标函数 */
 typedef struct tagListNode {
     struct tagListNode *next;
     int val;
@@ -19,11 +27,23 @@ typedef struct {
     ListNode *top;
 } MyStack;
 
+/**
+ * @description:
+ * =================================================================================
+ * @return {*}
+ */
 MyStack* myStackCreate() {
     MyStack *stk = (MyStack *)calloc(1, sizeof(MyStack));
     return stk;
 }
 
+/**
+ * @description:
+ * =================================================================================
+ * @param {MyStack*} obj
+ * @param {int} x
+ * @return {*}
+ */
 void myStackPush(MyStack* obj, int x) {
     ListNode *node = (ListNode *)malloc(sizeof(ListNode));
     node->val = x;
@@ -31,6 +51,12 @@ void myStackPush(MyStack* obj, int x) {
     obj->top = node;
 }
 
+/**
+ * @description:
+ * =================================================================================
+ * @param {MyStack*} obj
+ * @return {*}
+ */
 int myStackPop(MyStack* obj) {
     ListNode *node = obj->top;
     int val = node->val;
@@ -39,10 +65,22 @@ int myStackPop(MyStack* obj) {
     return val;
 }
 
+/**
+ * @description:
+ * =================================================================================
+ * @param {MyStack*} obj
+ * @return {*}
+ */
 int myStackTop(MyStack* obj) {
     return obj->top->val;
 }
 
+/**
+ * @description:
+ * =================================================================================
+ * @param {MyStack*} obj
+ * @return {*}
+ */
 bool myStackEmpty(MyStack* obj) {
     return (obj->top == NULL);
 }
@@ -58,8 +96,14 @@ void myStackFree(MyStack* obj) {
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
-/* NOTE：主函数 */
-int main(int argc, const char* argv[]) {
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc
+ * @param {char} *argv
+ * @return {*}
+ */
+int main(int argc, const char *argv[]) {
     /* NOTE: Test case 1 */
     printf("======== Case 1 ======== \n");
     MyStack *myStack = myStackCreate();
