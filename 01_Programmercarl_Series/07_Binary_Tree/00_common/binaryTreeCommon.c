a528244fae6d2cd271f8c03dcf0f7b9064afa2fc
Chong Liu <liuc12@miamioh.edu>
2023-09-11 23:36:07 +0800

    Update binaryTreeCommon.c

--- 01_Programmercarl_Series/07_Binary_Tree/00_common/binaryTreeCommon.c
+++ 01_Programmercarl_Series/07_Binary_Tree/00_common/binaryTreeCommon.c
@@ -1,24 +1,25 @@
 /*
- * @File: binaryTreeCommon.c
- * @Created Date: 2023-08-28 9:44:35 am
- * @Author: Chong Liu
- * @Brief: 二叉树的常用函数
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\07_Binary_Tree\00_common\binaryTreeCommon.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-09-01 20:07:37
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-09-11 23:35:50
  * =================================================================================
- * Copyright (c) 2023 Chong Liu
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * Last Modified: Chong Liu - 2023-09-02 12:13:52 pm
+ * @Description  : 二叉树的常用函数
+ *
+ * 用数组来存储二叉树: 如果父节点的数组下标是 i，那么它的左孩子就是 i * 2 + 1，右孩子就是 i * 2 + 2。
+ *
+ * 二叉树主要有两种遍历方式：
+ *   深度优先遍历：先往深走，遇到叶子节点再往回走。
+ *       前序遍历（递归法，迭代法）
+ *       中序遍历（递归法，迭代法）
+ *       后序遍历（递归法，迭代法）
+ *   广度优先遍历：一层一层的去遍历。
+ *       层次遍历（迭代法）
  */
 
-/* 用数组来存储二叉树: 如果父节点的数组下标是 i，那么它的左孩子就是 i * 2 + 1，右孩子就是 i * 2 + 2。*/
-/*
- * 二叉树主要有两种遍历方式：
- *      深度优先遍历：先往深走，遇到叶子节点再往回走。
- *          前序遍历（递归法，迭代法）
- *          中序遍历（递归法，迭代法）
- *          后序遍历（递归法，迭代法）
- *      广度优先遍历：一层一层的去遍历。
- *          层次遍历（迭代法）
- */
 #include <stdio.h>
 #include <stdlib.h>
 
