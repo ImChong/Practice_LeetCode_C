7acae1f2059311746ae23685e738e9ec29a0298f
Chong Liu <liuc12@miamioh.edu>
2023-09-13 17:54:30 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/05_levelOrderBottom/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/05_levelOrderBottom/main_chong.c
@@ -3,11 +3,12 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-28 09:44:35
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-13 17:46:38
+ * @LastEditTime : 2023-09-13 17:53:45
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 107. 二叉树的层序遍历 II: https://leetcode.cn/problems/binary-tree-level-order-traversal-ii/
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,15 +22,71 @@ struct TreeNode {
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
-/* 107. 二叉树的层序遍历 II */
+/* 107. 二叉树的层序遍历 II：从下往上层序遍历二叉树 */
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
+/**
+ * TODO: 目标函数
+ * @description: 二叉树的层序遍历
+ * =================================================================================
+ * @param {struct TreeNode} *root       二叉树的根节点
+ * @param {int} *returnSize             二叉树的层数
+ * @param {int} **returnColumnSizes     二叉树对应层级的节点数
+ * @return {int} **resultArray          结果二维数组
+ */
+int** levelOrderBottom(struct TreeNode* root, int* returnSize, int** returnColumnSizes){
+    /* TODO */
+
+
+
+
+
+
+    return NULL;
+}
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* ==================================================================================================== */
+
+/**
+ * @description: 创建树节点
+ * =================================================================================
+ * @param {int} value                   树节点的数值
+ * @return {struct TreeNode} *node      新树节点的指针
+ */
+struct TreeNode *newNode(int value) {
+    struct TreeNode *node = (struct TreeNode *)malloc(sizeof(struct TreeNode));     /* 初始化树节点：为树节点分配空间 */
+    node->val = value;                                                              /* 树节点的值为 value */
+    node->left = NULL;                                                              /* 树节点的左子节点为 NULL */
+    node->right = NULL;                                                             /* 树节点的右子节点为 NULL */
+    return node;                                                                    /* 返回树节点 */
+}
+
+/**
+ * @description: 打印二维数组
+ * =================================================================================
+ * @param {int} **array                 二维数组
+ * @param {int} size                    二维数组的行数
+ * @param {int} *columnSizes            二维数组行数对应的列数
+ * @return {void}
+ */
+void print2DArray(int **array, int size, int *columnSizes) {
+    printf("[\n");                                          /* 打印总边框 [ */
+    for (int i = 0; i < size; ++i) {                        /* 遍历二维数组的行 */
+        printf("  [");                                          /* 打印行边框 [ */
+        for (int j = 0; j < columnSizes[i]; ++j) {                  /* 遍历 i 行内的元素 */
+            printf("%d", array[i][j]);                                  /* 打印 i 行内的元素 */
+            if (j < columnSizes[i] - 1) {                               /* 打印分隔符：, */
+                printf(", ");
+            }
+        }
+        printf("]\n");                                          /* 打印行边框 ] */
+    }
+    printf("]\n");                                          /* 打印总边框 ] */
+}
 
 /**
  * @description: 主函数
@@ -39,6 +96,30 @@ struct TreeNode {
  * @return {int}            程序运行状态
  */
 int main(int argc, const char *argv[]) {
+    /*
+     * 创建以下树结构
+     *      1
+     *     / \
+     *    2   3
+     *   / \ / \
+     *  4  5 6  7
+     */
+    struct TreeNode *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+
+    int returnSize = 0;
+    int *returnColumnSizes;
+
+    /* NOTE: 函数调用 */
+    int** result = levelOrderBottom(root, &returnSize, &returnColumnSizes);
+    print2DArray(result, returnSize, returnColumnSizes);
 
     return 0;
 }
