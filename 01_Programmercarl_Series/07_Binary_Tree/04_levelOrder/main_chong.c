3abfd24cc53b3470310bac414f6b2d736aed03d5
Chong Liu <liuc12@miamioh.edu>
2023-09-13 15:38:37 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/04_levelOrder/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/04_levelOrder/main_chong.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-11 21:17:11
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-13 15:27:36
+ * @LastEditTime : 2023-09-13 15:31:08
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -57,10 +57,10 @@ void enQueue(struct Queue *queueHead, struct TreeNode *node) {
     while (queueIt->next != NULL) {             /* 当队列节点指针的下一元素不为 NULL 时 */
         queueIt = queueIt->next;                    /* 队列节点指针向后移动一位 */
     }
-    struct Queue *queueNew = (struct Queue *)malloc(sizeof(struct Queue));
-    queueNew->node = node;
-    queueNew->next = NULL;
-    queueIt->next = queueNew;
+    struct Queue *queueNew = (struct Queue *)malloc(sizeof(struct Queue));      /* 为新队列节点分配空间 */
+    queueNew->node = node;                      /* 将当前新队列节点的树节点指针指向目标树节点指针 node */
+    queueNew->next = NULL;                      /* 将当前新队列节点的下一节点指向 NULL */
+    queueIt->next = queueNew;                   /* 将队列尾节点的下一节点指向*/
 }
 
 /**
