bfb9917768c9609ef32d458764fe74e62e3ada87
Chong Liu <liuc12@miamioh.edu>
2023-09-14 16:31:26 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/06_rightSideView/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/06_rightSideView/main_chong.c
@@ -10,6 +10,7 @@
  * @Description  : 199. 二叉树的右视图: https://leetcode.cn/problems/binary-tree-right-side-view/
  * https://programmercarl.com/0102.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86.html#_199-%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%8F%B3%E8%A7%86%E5%9B%BE
  * 解法参考：https://leetcode.cn/problems/binary-tree-right-side-view/solutions/214060/199-er-cha-shu-de-you-shi-tu-by-hamiguaex163com/
+ * 解法参考：https://leetcode.cn/problems/binary-tree-right-side-view/solutions/1453989/by-goodgoodday-2kho/
  */
 #include <stdio.h>
 #include <stdlib.h>
