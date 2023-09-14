98beef4d2786c5282b72aec06315daf10c0b97ce
Chong Liu <liuc12@miamioh.edu>
2023-09-14 16:02:00 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/06_rightSideView/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/06_rightSideView/main_chong.c
@@ -1,6 +1,23 @@
-/* 199. 二叉树的右视图: https://leetcode.cn/problems/binary-tree-right-side-view/ */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\07_Binary_Tree\06_rightSideView\main_chong.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-28 09:44:35
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-09-14 16:01:33
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 199. 二叉树的右视图: https://leetcode.cn/problems/binary-tree-right-side-view/
+ */
 #include <stdio.h>
+#include <stdlib.h>
+
+/* NOTE: 树节点 */
+struct TreeNode {
+    int val;                    /* 节点的值 */
+    struct TreeNode *left;      /* 左节点 */
+    struct TreeNode *right;     /* 右节点 */
+};
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
