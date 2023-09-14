f97fbbfd94610e446eff1b65a3a68bf2e3d2b17c
Chong Liu <liuc12@miamioh.edu>
2023-09-14 16:29:22 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/06_rightSideView/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/06_rightSideView/main_chong.c
@@ -3,13 +3,13 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-28 09:44:35
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-14 16:01:33
+ * @LastEditTime : 2023-09-14 16:29:03
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 199. 二叉树的右视图: https://leetcode.cn/problems/binary-tree-right-side-view/
  * https://programmercarl.com/0102.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86.html#_199-%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%8F%B3%E8%A7%86%E5%9B%BE
- * 解法参考：
+ * 解法参考：https://leetcode.cn/problems/binary-tree-right-side-view/solutions/214060/199-er-cha-shu-de-you-shi-tu-by-hamiguaex163com/
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +24,17 @@ struct TreeNode {
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
+/**
+ * @description: 二叉树的右视图
+ * =================================================================================
+ * @param {TreeNode} *root
+ * @param {int} *returnSize
+ * @return {int} *resultArray
+ */
+int *rightSideView(struct TreeNode *root, int *returnSize){
 
+    return NULL;
+}
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
@@ -57,6 +67,6 @@ int main(int argc, const char *argv[]) {
     int returnSize = 0;
     int *returnColumnSizes;
 
-    
+
     return 0;
 }
