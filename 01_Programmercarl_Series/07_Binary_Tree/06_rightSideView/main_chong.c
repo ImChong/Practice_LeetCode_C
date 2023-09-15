dc635367cf4a76b4cf7419d44c0700c6c58eb5b0
Chong Liu <liuc12@miamioh.edu>
2023-09-15 09:03:09 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/06_rightSideView/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/06_rightSideView/main_chong.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-28 09:44:35
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-15 08:55:37
+ * @LastEditTime : 2023-09-15 09:00:47
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -35,17 +35,18 @@ struct TreeNode {
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
 /* ==================================================================================================== */
+#define MAX_SIZE 2000           /* 初始化大小 */
 
 /**
  * @description: 二叉树的前序遍历
  * =================================================================================
  * @param {struct TreeNode} *root   二叉树的根节点
- * @param {int} *returnArr          结果数组
+ * @param {int} *returnArray          结果一维数组
  * @param {int} height              二叉树的高度
  * @param {int} *returnSize         结果数组的大小
  * @return {*}
  */
-void preorder(struct TreeNode *root, int *returnArr, int height, int *returnSize) {
+void preorder(struct TreeNode *root, int *returnArray, int height, int *returnSize) {
     if (root == NULL) {
         return;
     }
@@ -54,9 +55,9 @@ void preorder(struct TreeNode *root, int *returnArr, int height, int *returnSize
         *returnSize = height + 1;
     }
 
-    returnArr[height] = root->val;
-    preorder(root->left, returnArr, height + 1, returnSize);
-    preorder(root->right, returnArr, height + 1, returnSize);
+    returnArray[height] = root->val;
+    preorder(root->left, returnArray, height + 1, returnSize);
+    preorder(root->right, returnArray, height + 1, returnSize);
     return;
 }
 
@@ -65,10 +66,10 @@ void preorder(struct TreeNode *root, int *returnArr, int height, int *returnSize
  * =================================================================================
  * @param {TreeNode} *root          二叉树的根节点
  * @param {int} *returnSize         返回数组大小
- * @return {int} *resultArray       结果一维数组
+ * @return {int} *returnArray         结果一维数组
  */
 int *rightSideView(struct TreeNode *root, int *returnSize){
-
+    int *returnArray = (int *)calloc(MAX_SIZE, sizeof(int));
     return NULL;
 }
 /* ==================================================================================================== */
