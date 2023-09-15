5ba32d29e939ad41c24fdf311cb66220d8d8a3a5
Chong Liu <liuc12@miamioh.edu>
2023-09-15 15:02:59 +0800

    update

--- 01_Programmercarl_Series/07_Binary_Tree/07_averageOfLevels/main_chong_DFS.c
+++ 01_Programmercarl_Series/07_Binary_Tree/07_averageOfLevels/main_chong_DFS.c
@@ -1,21 +1,23 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\07_Binary_Tree\07_averageOfLevels\main_chong.c
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\07_Binary_Tree\07_averageOfLevels\main_chong_DFS.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-28 09:44:35
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-15 15:00:29
+ * @LastEditTime : 2023-09-15 15:02:12
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 637. 二叉树的层平均值: https://leetcode.cn/problems/average-of-levels-in-binary-tree/
  * https://programmercarl.com/0102.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86.html#_637-%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%B9%B3%E5%9D%87%E5%80%BC
  */
-
 #include <stdio.h>
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
+/*
+    Depth-First-Search (DFS) - 深度优先搜索算法
+*/
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
