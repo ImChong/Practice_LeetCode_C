d060d81f732f5251cf0ce918c62e49d7ef5d8bfe
Chong Liu <liuc12@miamioh.edu>
2023-09-15 08:46:26 +0800

    update

--- 01_Programmercarl_Series/07_Binary_Tree/04_levelOrder/main_chong_list.c
+++ 01_Programmercarl_Series/07_Binary_Tree/04_levelOrder/main_chong_list.c
@@ -11,6 +11,14 @@
  * 102. 二叉树的层序遍历: https://leetcode.cn/problems/binary-tree-level-order-traversal/
  * https://programmercarl.com/0102.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86.html
  * 解法参考：https://leetcode.cn/problems/binary-tree-level-order-traversal/solutions/1286876/er-cha-shu-de-ceng-xu-bian-li-cyu-yan-xi-jdk8/
+ *
+ * 层序遍历：
+ * [
+ *   [1]
+ *   [2, 3]
+ *   [4, 5, 6, 7]
+ * ]
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
