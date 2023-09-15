bbb4ce1d9b93f69cd4a85ea3897f82341066de86
Chong Liu <liuc12@miamioh.edu>
2023-09-15 14:56:09 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/07_averageOfLevels/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/07_averageOfLevels/main_chong.c
@@ -1,16 +1,44 @@
-/* 637. 二叉树的层平均值: https://leetcode.cn/problems/average-of-levels-in-binary-tree/ */
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\07_Binary_Tree\07_averageOfLevels\main_chong.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-28 09:44:35
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-09-15 14:54:37
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 637. 二叉树的层平均值: https://leetcode.cn/problems/average-of-levels-in-binary-tree/
+ * https://programmercarl.com/0102.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86.html#_637-%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%B9%B3%E5%9D%87%E5%80%BC
+ */
 
 #include <stdio.h>
 
 /* ==================================================================================================== */
-/* ==================================================================================================== */
 /* TODO: 目标函数 */
 
 /* ==================================================================================================== */
+/**
+ * @description:
+ * =================================================================================
+ * @param {TreeNode} *root
+ * @param {int} *returnSize
+ * @return {double} *averages
+ */
+double *averageOfLevels(struct TreeNode *root, int *returnSize){
+    /* TODO */
+    
+}
+/* ==================================================================================================== */
 /* ==================================================================================================== */
 
-/* NOTE：主函数 */
-int main(int argc, const char* argv[]) {
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc
+ * @param {char} *argv
+ * @return {*}
+ */
+int main(int argc, const char *argv[]) {
 
     return 0;
 }
