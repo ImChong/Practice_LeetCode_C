1e77eb5f4c5a7f82ad0701f6e8ef7914ccb0b0f2
Chong Liu <liuc12@miamioh.edu>
2023-09-16 15:05:03 +0800

    Update main_chong_cut.c

--- 01_Programmercarl_Series/08_Backtracking/01_combine/main_chong_cut.c
+++ 01_Programmercarl_Series/08_Backtracking/01_combine/main_chong_cut.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-28 09:44:35
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-16 15:02:16
+ * @LastEditTime : 2023-09-16 15:05:02
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -35,7 +35,17 @@ int pathTop;        /* 记录当前数的数量 */
 int **ans;          /* 结果二维数组：记录所有 k 个数的组合 */
 int ansTop;         /* 记录当前的组数 */
 
+/**
+ * @description: 回溯算法
+ * =================================================================================
+ * @param {int} n
+ * @param {int} k
+ * @param {int} startNum
+ * @return {*}
+ */
+void backtracking(int n, int k, int startNum) {
 
+}
 
 /**
  * @description: 目标函数：组合
@@ -58,18 +68,26 @@ int **combine(int n, int k, int *returnSize, int **returnColumnSizes){
 /**
  * @description: 打印组合
  * =================================================================================
- * @param {int} **result
- * @param {int} returnSize
- * @param {int} k
+ * @param {int} **result        二维数组
+ * @param {int} returnSize      二维数组的行数
+ * @param {int} k               k 个数的组合
  * @return {void}
  */
 void printCombinations(int **result, int returnSize, int k) {
+    printf("[\n");
     for (int i = 0; i < returnSize; ++i) {
+        printf("  [");
         for (int j = 0; j < k; ++j) {
-            printf("%d ", result[i][j]);
+            printf("%d", result[i][j]);
+            if (j < k - 1) {
+                printf(", ");
+            }
         }
-        printf("\n");
+        printf("]\n");
     }
+    printf("]\n");
+
+    printf("nums of combination: %d\n", returnSize);
 }
 
 /**
