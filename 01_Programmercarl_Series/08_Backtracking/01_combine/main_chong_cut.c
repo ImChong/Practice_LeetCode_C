fc22442bd17e97e66948aa4729c1244589c7cf70
Chong Liu <liuc12@miamioh.edu>
2023-09-16 15:16:29 +0800

    Update main_chong_cut.c

--- 01_Programmercarl_Series/08_Backtracking/01_combine/main_chong_cut.c
+++ 01_Programmercarl_Series/08_Backtracking/01_combine/main_chong_cut.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-28 09:44:35
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-16 15:11:38
+ * @LastEditTime : 2023-09-16 15:12:56
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -19,7 +19,6 @@
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
-/* TODO: 目标函数 */
 /*
     回溯算法理论基础：https://programmercarl.com/%E5%9B%9E%E6%BA%AF%E7%AE%97%E6%B3%95%E7%90%86%E8%AE%BA%E5%9F%BA%E7%A1%80.html
         回溯法解决的问题都可以抽象为树形结构（N叉树），用树形结构来理解回溯
@@ -56,8 +55,20 @@ void backtracking(int n, int k, int startNum) {
  * @param {int} **returnColumnSizes     数组表示二维数组每行的列数
  * @return {int} **ans                  结果二维数组
  */
-int **combine(int n, int k, int *returnSize, int **returnColumnSizes){
-    return NULL;
+int **combine(int n, int k, int *returnSize, int **returnColumnSizes) {
+    path = (int *)malloc(sizeof(int) * k);                              /* path数组存储符合条件的结果 */
+    ans = (int **)malloc(sizeof(int *) * MAX_SIZE);                     /* ans二维数组存储符合条件的结果数组的集合。（数组足够大，避免极端情况） */
+    pathTop = ansTop = 0;                                               /* 初始化当前数的数量 pathTop 和 当前的组数 ansTop 为 0 */
+
+    backtracking(n, k, 1);                                              /* 回溯算法，回溯初始值为 1 */
+
+    *returnSize = ansTop;                                               /* 最后的返回大小为当前的组数 */
+    *returnColumnSizes = (int *)malloc(sizeof(int) * (*returnSize));    /* returnColumnSizes数组存储ans二维数组对应下标中一维数组的长度（都为k） */
+    for(int i = 0; i < *returnSize; i++) {
+        (*returnColumnSizes)[i] = k;
+    }
+
+    return ans;                                                         /* 返回ans二维数组 */
 }
 
 /* ==================================================================================================== */
@@ -98,8 +109,8 @@ void printCombinations(int **result, int returnSize, int k) {
  * @return {int}            程序运行状态
  */
 int main(int argc, const char *argv[]) {
-    int n = 4;
-    int k = 2;
+    int n = 20;
+    int k = 10;
     int returnSize;
     int *returnColumnSizes;
     int **result = combine(n, k, &returnSize, &returnColumnSizes);
