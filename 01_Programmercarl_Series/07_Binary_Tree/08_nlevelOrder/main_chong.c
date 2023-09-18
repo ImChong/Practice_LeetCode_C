a11e17bcbfd7b732d73a0998dd6405493f712fa1
Chong Liu <liuc12@miamioh.edu>
2023-09-18 19:53:52 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/07_Binary_Tree/08_nlevelOrder/main_chong.c
+++ 01_Programmercarl_Series/07_Binary_Tree/08_nlevelOrder/main_chong.c
@@ -1,11 +1,25 @@
-/* 429. N 叉树的层序遍历: https://leetcode.cn/problems/n-ary-tree-level-order-traversal/ */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\07_Binary_Tree\08_nlevelOrder\main_chong.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-28 09:44:35
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-09-18 19:53:07
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 429. N 叉树的层序遍历: https://leetcode.cn/problems/n-ary-tree-level-order-traversal/
+ * https://programmercarl.com/0102.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86.html#_429-n%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86
+ * 解法参考：https://leetcode.cn/problems/n-ary-tree-level-order-traversal/solutions/1400623/n-cha-shu-de-ceng-xu-bian-li-by-leetcode-lxdr/
+ */
 #include <stdio.h>
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /* TODO: 目标函数 */
-
+int** levelOrder(struct Node* root, int* returnSize, int** returnColumnSizes) {
+    /* TODO */
+    return NULL;
+}
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 
