3438e07518ae53fed550da3472da7f8a7e814046
Chong Liu <liuc12@miamioh.edu>
2023-09-18 18:59:57 +0800

    Update main_chong_DFS.c

--- 01_Programmercarl_Series/07_Binary_Tree/07_averageOfLevels/main_chong_DFS.c
+++ 01_Programmercarl_Series/07_Binary_Tree/07_averageOfLevels/main_chong_DFS.c
@@ -10,6 +10,9 @@
  * @Description  : 637. 二叉树的层平均值: https://leetcode.cn/problems/average-of-levels-in-binary-tree/
  * https://programmercarl.com/0102.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86.html#_637-%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%B9%B3%E5%9D%87%E5%80%BC
  * 解法参考：https://leetcode.cn/problems/average-of-levels-in-binary-tree/solutions/410522/er-cha-shu-de-ceng-ping-jun-zhi-by-leetcode-soluti/
+ *
+ * 方法一：深度优先搜索
+ *
  */
 #include <stdio.h>
 
