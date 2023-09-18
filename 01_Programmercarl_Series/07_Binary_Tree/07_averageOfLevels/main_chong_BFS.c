193863a0e496f635842974fb7f72dc58aad40b64
Chong Liu <liuc12@miamioh.edu>
2023-09-18 14:08:43 +0800

    Update main_chong_BFS.c

--- 01_Programmercarl_Series/07_Binary_Tree/07_averageOfLevels/main_chong_BFS.c
+++ 01_Programmercarl_Series/07_Binary_Tree/07_averageOfLevels/main_chong_BFS.c
@@ -3,15 +3,31 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-28 09:44:35
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-15 15:01:44
+ * @LastEditTime : 2023-09-18 14:06:19
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 637. 二叉树的层平均值: https://leetcode.cn/problems/average-of-levels-in-binary-tree/
  * https://programmercarl.com/0102.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%BA%8F%E9%81%8D%E5%8E%86.html#_637-%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E5%B1%82%E5%B9%B3%E5%9D%87%E5%80%BC
  * 解法参考：https://leetcode.cn/problems/average-of-levels-in-binary-tree/solutions/410522/er-cha-shu-de-ceng-ping-jun-zhi-by-leetcode-soluti/
+ *
+ *       1
+ *      / \
+ *     2   3
+ *    / \ / \
+ *   4  5 6  7
+ *
+ *
  */
 #include <stdio.h>
+#include <stdlib.h>
+
+/* 树节点 */
+struct TreeNode {
+    int val;                    /* 节点的值 */
+    struct TreeNode *left;      /* 左节点 */
+    struct TreeNode *right;     /* 右节点 */
+};
 
 /* ==================================================================================================== */
 /* ==================================================================================================== */
@@ -19,7 +35,6 @@
 /*
     Breadth-first search (BFS) - 广度优先搜索算法
 */
-
 /* ==================================================================================================== */
 /* ==================================================================================================== */
 /**
