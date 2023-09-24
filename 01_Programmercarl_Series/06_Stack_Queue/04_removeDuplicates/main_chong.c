77ae2fe5b74d2fb1564dd424311f06b21bd70010
Chong Liu <liuc12@miamioh.edu>
2023-09-24 23:08:08 +0800

    Update main_chong.c

--- 01_Programmercarl_Series/06_Stack_Queue/04_removeDuplicates/main_chong.c
+++ 01_Programmercarl_Series/06_Stack_Queue/04_removeDuplicates/main_chong.c
@@ -3,15 +3,13 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-03 10:29:02
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-24 23:07:35
+ * @LastEditTime : 2023-09-24 23:07:55
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : 1047. 删除字符串中的所有相邻重复项：https://leetcode.cn/problems/remove-all-adjacent-duplicates-in-string/
+ * https://programmercarl.com/1047.%E5%88%A0%E9%99%A4%E5%AD%97%E7%AC%A6%E4%B8%B2%E4%B8%AD%E7%9A%84%E6%89%80%E6%9C%89%E7%9B%B8%E9%82%BB%E9%87%8D%E5%A4%8D%E9%A1%B9.html
  */
-/* 1047. 删除字符串中的所有相邻重复项：https://leetcode.cn/problems/remove-all-adjacent-duplicates-in-string/ */
-/* https://programmercarl.com/1047.%E5%88%A0%E9%99%A4%E5%AD%97%E7%AC%A6%E4%B8%B2%E4%B8%AD%E7%9A%84%E6%89%80%E6%9C%89%E7%9B%B8%E9%82%BB%E9%87%8D%E5%A4%8D%E9%A1%B9.html */
-
 #include <stdio.h>
 
 /**********************************************************************************/
