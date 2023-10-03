3da60f3c8985dd87149c7cae8aa361503bb6f939
Chong Liu <liuc12@miamioh.edu>
2023-10-03 11:56:25 +0800

    Update main.c

--- 01_Programmercarl_Series/01_Array/02_removeElement/main.c
+++ 01_Programmercarl_Series/01_Array/02_removeElement/main.c
@@ -1,8 +1,32 @@
-/* 27. 移除元素：https://leetcode.cn/problems/remove-element/ */
-/* https://programmercarl.com/0027.%E7%A7%BB%E9%99%A4%E5%85%83%E7%B4%A0.html */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\01_Array\02_removeElement\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-09-16 08:57:10
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-03 11:56:22
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 27. 移除元素：https://leetcode.cn/problems/remove-element/
+ * https://programmercarl.com/0027.%E7%A7%BB%E9%99%A4%E5%85%83%E7%B4%A0.html
+ */
 #include <stdio.h>
 
+/**********************************************************************************/
+/**********************************************************************************/
+/***                                                                            ***/
+/***                               TARGET FUNCTION                              ***/
+/***                                                                            ***/
+/**********************************************************************************/
+/**********************************************************************************/
+/**
+ * @description:
+ * =================================================================================
+ * @param {int} *nums
+ * @param {int} numsSize
+ * @param {int} val
+ * @return {*}
+ */
 int removeElement(int *nums, int numsSize, int val) {
     int quick, slow;            /* 初始化快慢指针 */
     for (quick = 0, slow = 0; quick < numsSize; quick++) {  /* 快指针遍历数组 */
@@ -14,7 +38,19 @@ int removeElement(int *nums, int numsSize, int val) {
     return slow;    /* 返回慢指针  */
 }
 
-int main() {
+/**********************************************************************************/
+/*                                                                                */
+/*                                  MAIN FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
     int nums[] = {3, 2, 2, 3};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
     int val = 3;
