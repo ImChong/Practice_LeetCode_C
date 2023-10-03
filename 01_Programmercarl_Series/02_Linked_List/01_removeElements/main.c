50733d67e94d970108f6a112203a7cc6ab575141
Chong Liu <liuc12@miamioh.edu>
2023-10-03 12:03:58 +0800

    Update main.c

--- 01_Programmercarl_Series/02_Linked_List/01_removeElements/main.c
+++ 01_Programmercarl_Series/02_Linked_List/01_removeElements/main.c
@@ -1,16 +1,41 @@
-/* 203. 移除链表元素：https://leetcode.cn/problems/remove-linked-list-elements/ */
-/* https://programmercarl.com/0203.%E7%A7%BB%E9%99%A4%E9%93%BE%E8%A1%A8%E5%85%83%E7%B4%A0.html */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\02_Linked_List\01_removeElements\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-09-16 08:57:10
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-03 12:03:26
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 203. 移除链表元素：https://leetcode.cn/problems/remove-linked-list-elements/
+ * https://programmercarl.com/0203.%E7%A7%BB%E9%99%A4%E9%93%BE%E8%A1%A8%E5%85%83%E7%B4%A0.html
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
+/**********************************************************************************/
+/*                                                                                */
+/*                                 GLOBAL VARIABLES                               */
+/*                                                                                */
+/**********************************************************************************/
 /* 节点定义 */
 struct ListNode {
     int val;
     struct ListNode *next;
 };
 
-/* 创建链表 */
+/**********************************************************************************/
+/*                                                                                */
+/*                                 UTILITY FUNCTIONS                              */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 创建链表
+ * =================================================================================
+ * @param {int} *array
+ * @param {int} size
+ * @return {*}
+ */
 struct ListNode *createList(int *array, int size) {
     struct ListNode *head = NULL;       /* 头节点指针 */
     struct ListNode *current = NULL;    /* 当前节点指针 */
@@ -33,7 +58,12 @@ struct ListNode *createList(int *array, int size) {
     return head;
 }
 
-/* 打印链表 */
+/**
+ * @description: 打印链表
+ * =================================================================================
+ * @param {ListNode} *listPtr
+ * @return {*}
+ */
 void printList(struct ListNode *listPtr) {
     while (listPtr != NULL) {
         printf("%d ", listPtr->val);
@@ -42,10 +72,24 @@ void printList(struct ListNode *listPtr) {
     printf("\n");
 }
 
-/* ==================================================================================================== */
-/* ==================================================================================================== */
+/**********************************************************************************/
+/**********************************************************************************/
+/***                                                                            ***/
+/***                               TARGET FUNCTION                              ***/
+/***                                                                            ***/
+/**********************************************************************************/
+/**********************************************************************************/
 /* TODO: 运行时长和内存占用可提升*/
-struct ListNode* removeElements(struct ListNode* head, int val){
+/* ============================================================================== */
+/* ============================================================================== */
+/**
+ * @description:
+ * =================================================================================
+ * @param {ListNode} *head
+ * @param {int} val
+ * @return {*}
+ */
+struct ListNode *removeElements(struct ListNode *head, int val){
     struct ListNode dummy;      /* 创建虚拟头节点 */
     dummy.next = head;          /* 将传入链表接在虚拟头节点后面 */
 
@@ -61,10 +105,19 @@ struct ListNode* removeElements(struct ListNode* head, int val){
     }
     return dummy.next;          /* 返回虚拟头节点后挂载的结果链表 */
 }
-/* ==================================================================================================== */
-/* ==================================================================================================== */
 
-/* 主函数 */
+/**********************************************************************************/
+/*                                                                                */
+/*                                  MAIN FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
 int main(int argc, const char* argv[]) {
     int arr[7] = {1, 2, 6, 3, 4, 5, 6};
     struct ListNode *head = createList(arr, 7);
