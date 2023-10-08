d8b181524b546ba0b27e534282fdfebedde8ffc8
Chong Liu <liuc12@miamioh.edu>
2023-10-08 14:23:25 +0800

    Update main.c

--- 01_Programmercarl_Series/07_Binary_Tree/24_constructMaximumBinaryTree/main.c
+++ 01_Programmercarl_Series/07_Binary_Tree/24_constructMaximumBinaryTree/main.c
@@ -3,16 +3,13 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-05 14:47:07
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-08 14:22:42
+ * @LastEditTime : 2023-10-08 14:23:22
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 654.最大二叉树: https://leetcode.cn/problems/maximum-binary-tree/
  * https://programmercarl.com/0654.%E6%9C%80%E5%A4%A7%E4%BA%8C%E5%8F%89%E6%A0%91.html
  */
-/*  */
-/*  */
-
 #include <stdio.h>
 
 /**********************************************************************************/
