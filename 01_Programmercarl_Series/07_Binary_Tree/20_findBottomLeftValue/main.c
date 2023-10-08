9da2802c4b6bc7c803c2d8f697fd7d422353a7f5
Chong Liu <liuc12@miamioh.edu>
2023-10-08 14:19:57 +0800

    Update main.c

--- 01_Programmercarl_Series/07_Binary_Tree/20_findBottomLeftValue/main.c
+++ 01_Programmercarl_Series/07_Binary_Tree/20_findBottomLeftValue/main.c
@@ -3,11 +3,11 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-05 14:47:07
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-08 14:19:39
+ * @LastEditTime : 2023-10-08 14:19:53
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  : 513.找树左下角的值: https://leetcode.cn/problems/find-bottom-left-tree-value/
+ * @Description  : 513. 找树左下角的值: https://leetcode.cn/problems/find-bottom-left-tree-value/
  * https://programmercarl.com/0513.%E6%89%BE%E6%A0%91%E5%B7%A6%E4%B8%8B%E8%A7%92%E7%9A%84%E5%80%BC.html
  */
 #include <stdio.h>
