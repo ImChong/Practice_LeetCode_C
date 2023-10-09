d4473ece933d2f73f7a8829a0d9c66c83dd7f29f
Chong Liu <liuc12@miamioh.edu>
2023-10-09 12:11:26 +0800

    Update main.c

--- 01_Programmercarl_Series/08_Backtracking/04_combinationSum/main.c
+++ 01_Programmercarl_Series/08_Backtracking/04_combinationSum/main.c
@@ -1,18 +1,53 @@
-/* 39. 组合总和: https://leetcode.cn/problems/combination-sum/ */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\08_Backtracking\04_combinationSum\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-05 14:47:07
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-09 12:11:07
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 39. 组合总和: https://leetcode.cn/problems/combination-sum/
+ */
 #include <stdio.h>
 
-/* ==================================================================================================== */
-/* ==================================================================================================== */
+/**********************************************************************************/
+/*                                                                                */
+/*                                 TARGET FUNCTION                                */
+/*                                                                                */
+/**********************************************************************************/
 /* TODO: 目标函数 */
-int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
 
+/* ============================================================================== */
+/* ============================================================================== */
+/**
+ * @description:
+ * =================================================================================
+ * @param {int} *candidates
+ * @param {int} candidatesSize
+ * @param {int} target
+ * @param {int} *returnSize
+ * @param {int} **returnColumnSizes
+ * @return {int} **ans
+ */
+int **combinationSum(int *candidates, int candidatesSize, int target, int *returnSize, int **returnColumnSizes){
+
+    return NULL
 }
-/* ==================================================================================================== */
-/* ==================================================================================================== */
 
-/* NOTE：主函数 */
-int main(int argc, const char* argv[]) {
+/**********************************************************************************/
+/*                                                                                */
+/*                                  MAIN FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
 
     return 0;
 }
