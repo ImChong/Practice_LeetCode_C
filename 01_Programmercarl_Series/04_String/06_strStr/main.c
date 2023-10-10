1a81337cd2e2714e6739a44afd0e1f90f0db0932
Chong Liu <liuc12@miamioh.edu>
2023-10-11 00:07:29 +0800

    Update main.c

--- 01_Programmercarl_Series/04_String/06_strStr/main.c
+++ 01_Programmercarl_Series/04_String/06_strStr/main.c
@@ -1,21 +1,53 @@
-/* 28. 实现 strStr()： https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/ */
-/* https://programmercarl.com/0028.%E5%AE%9E%E7%8E%B0strStr.html */
-/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0028.%E5%AE%9E%E7%8E%B0strStr.md */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\04_String\06_strStr\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-18 23:18:36
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-11 00:07:27
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 28. 实现 strStr()： https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/
+ * https://programmercarl.com/0028.%E5%AE%9E%E7%8E%B0strStr.html
+ * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0028.%E5%AE%9E%E7%8E%B0strStr.md
+ */
 #include <stdio.h>
 
-/* ==================================================================================================== */
-/* ==================================================================================================== */
+/**********************************************************************************/
+/*                                                                                */
+/*                                 TARGET FUNCTION                                */
+/*                                                                                */
+/**********************************************************************************/
 /* TODO: 目标函数 */
-int strStr(char * haystack, char * needle){
+
+/* ============================================================================== */
+/* ============================================================================== */
+/**
+ * @description:
+ * =================================================================================
+ * @param {char} *haystack
+ * @param {char} *needle
+ * @return {int}
+ */
+int strStr(char *haystack, char *needle){
 
     return 0;
 }
-/* ==================================================================================================== */
-/* ==================================================================================================== */
 
-/* NOTE：主函数 */
-int main(int argc, const char* argv[]) {
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                  MAIN FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
 
     return 0;
 }
