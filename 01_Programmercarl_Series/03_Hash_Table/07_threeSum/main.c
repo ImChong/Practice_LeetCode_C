86456ac62dd2c1ee9aa7e53a8329d02438149729
Chong Liu <liuc12@miamioh.edu>
2023-10-10 23:58:40 +0800

    Update main.c

--- 01_Programmercarl_Series/03_Hash_Table/07_threeSum/main.c
+++ 01_Programmercarl_Series/03_Hash_Table/07_threeSum/main.c
@@ -1,21 +1,57 @@
-/* 15. 三数之和：https://leetcode.cn/problems/3sum/ */
-/* https://programmercarl.com/0015.%E4%B8%89%E6%95%B0%E4%B9%8B%E5%92%8C.html */
-/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0015.%E4%B8%89%E6%95%B0%E4%B9%8B%E5%92%8C.md */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\07_threeSum\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-23 22:54:19
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-10 23:58:31
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 15. 三数之和：https://leetcode.cn/problems/3sum/
+ * https://programmercarl.com/0015.%E4%B8%89%E6%95%B0%E4%B9%8B%E5%92%8C.html
+ * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0015.%E4%B8%89%E6%95%B0%E4%B9%8B%E5%92%8C.md
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
+/**********************************************************************************/
+/*                                                                                */
+/*                                MACRO FUNCTIONS                                 */
+/*                                                                                */
+/**********************************************************************************/
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-/* 比较函数 */
+/**********************************************************************************/
+/**********************************************************************************/
+/***                                                                            ***/
+/***                               TARGET FUNCTION                              ***/
+/***                                                                            ***/
+/**********************************************************************************/
+/**********************************************************************************/
+
+/* ============================================================================== */
+/* ============================================================================== */
+/**
+ * @description: 比较函数
+ * =================================================================================
+ * @param {void} *a
+ * @param {void} *b
+ * @return {*}
+ */
 int cmp(const void *a, const void *b) {
     return *(int *)a - *(int *)b;
 }
 
-/* ==================================================================================================== */
-/* ==================================================================================================== */
-/* TODO：目标函数 */
-int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+/**
+ * @description: 三数之和
+ * =================================================================================
+ * @param {int} *nums
+ * @param {int} numsSize
+ * @param {int} *returnSize
+ * @param {int} **returnColumnSizes
+ * @return {int} **ans
+ */
+int **threeSum(int *nums, int numsSize, int *returnSize, int **returnColumnSizes){
     *returnSize = 0;
     if (numsSize < 3) {
         return NULL;
@@ -60,10 +96,20 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
     }
     return ans;
 }
-/* ==================================================================================================== */
-/* ==================================================================================================== */
 
-/* 打印结果 */
+/**********************************************************************************/
+/*                                                                                */
+/*                                 HELPER FUNCTIONS                               */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 打印结果
+ * =================================================================================
+ * @param {int} **result
+ * @param {int} returnSize
+ * @param {int} *returnColumnSizes
+ * @return {*}
+ */
 void printAns(int **result, int returnSize, int *returnColumnSizes) {
     for (int i = 0; i < returnSize; i++) {
         printf("Ans[%d]: ", i);
@@ -74,7 +120,14 @@ void printAns(int **result, int returnSize, int *returnColumnSizes) {
     }
 }
 
-/* 释放结果空间 */
+/**
+ * @description: 释放结果空间
+ * =================================================================================
+ * @param {int} ***result
+ * @param {int} *returnSize
+ * @param {int} **returnColumnSizes
+ * @return {*}
+ */
 void freeAns(int ***result, int *returnSize, int **returnColumnSizes) {
     for (int i = 0; i < *returnSize; i++) {
         free((*result)[i]);
@@ -83,8 +136,19 @@ void freeAns(int ***result, int *returnSize, int **returnColumnSizes) {
     free(*returnColumnSizes);
 }
 
-/* 主函数 */
-int main(int argc, const char* argv[]) {
+/**********************************************************************************/
+/*                                                                                */
+/*                                  MAIN FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
     int nums[] = {-1, 0, 1, 2, -1, -4};
 
     int returnSize = 0;
