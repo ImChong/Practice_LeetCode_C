5eb818058cccb69999cf0730eff27442d1407898
Chong Liu <liuc12@miamioh.edu>
2023-10-11 00:08:37 +0800

    Update main.c

--- 01_Programmercarl_Series/04_String/07_repeatedSubstringPattern/main.c
+++ 01_Programmercarl_Series/04_String/07_repeatedSubstringPattern/main.c
@@ -1,22 +1,52 @@
-/* 459. 重复的子字符串： https://leetcode.cn/problems/repeated-substring-pattern/ */
-/* https://programmercarl.com/0459.%E9%87%8D%E5%A4%8D%E7%9A%84%E5%AD%90%E5%AD%97%E7%AC%A6%E4%B8%B2.html */
-/* https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0459.%E9%87%8D%E5%A4%8D%E7%9A%84%E5%AD%90%E5%AD%97%E7%AC%A6%E4%B8%B2.md */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\04_String\07_repeatedSubstringPattern\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-18 23:18:36
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-11 00:08:32
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 459. 重复的子字符串： https://leetcode.cn/problems/repeated-substring-pattern/
+ * https://programmercarl.com/0459.%E9%87%8D%E5%A4%8D%E7%9A%84%E5%AD%90%E5%AD%97%E7%AC%A6%E4%B8%B2.html
+ * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0459.%E9%87%8D%E5%A4%8D%E7%9A%84%E5%AD%90%E5%AD%97%E7%AC%A6%E4%B8%B2.md
+ */
 #include <stdio.h>
 #include <stdbool.h>
 
-/* ==================================================================================================== */
-/* ==================================================================================================== */
+/**********************************************************************************/
+/*                                                                                */
+/*                                 TARGET FUNCTION                                */
+/*                                                                                */
+/**********************************************************************************/
 /* TODO: 目标函数 */
-bool repeatedSubstringPattern(char * s){
+
+/* ============================================================================== */
+/* ============================================================================== */
+/**
+ * @description:
+ * =================================================================================
+ * @param {char} *s
+ * @return {bool}
+ */
+bool repeatedSubstringPattern(char *s){
 
     return 0;
 }
-/* ==================================================================================================== */
-/* ==================================================================================================== */
 
-/* NOTE：主函数 */
-int main(int argc, const char* argv[]) {
+/**********************************************************************************/
+/*                                                                                */
+/*                                  MAIN FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
 
     return 0;
 }
