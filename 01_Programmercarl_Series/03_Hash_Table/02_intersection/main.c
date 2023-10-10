38cb6a10fbe7d637b1557635a163f4cee09bfafb
Chong Liu <liuc12@miamioh.edu>
2023-10-10 10:15:05 +0800

    Update main.c

--- 01_Programmercarl_Series/03_Hash_Table/02_intersection/main.c
+++ 01_Programmercarl_Series/03_Hash_Table/02_intersection/main.c
@@ -1,26 +1,47 @@
-/* 349. 两个数组的交集：https://leetcode.cn/problems/intersection-of-two-arrays/ */
-/* https://programmercarl.com/0349.%E4%B8%A4%E4%B8%AA%E6%95%B0%E7%BB%84%E7%9A%84%E4%BA%A4%E9%9B%86.html */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\02_intersection\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-05 14:47:06
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-10 10:14:42
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 349. 两个数组的交集：https://leetcode.cn/problems/intersection-of-two-arrays/
+ * https://programmercarl.com/0349.%E4%B8%A4%E4%B8%AA%E6%95%B0%E7%BB%84%E7%9A%84%E4%BA%A4%E9%9B%86.html
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 打印数组 */
-void printArray(int* arr, int arrSize) {
-    printf("Arr: ");
-    for (int i = 0; i < arrSize; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-/* ==================================================================================================== */
-/* ==================================================================================================== */
-/* 比较函数 */
+/**********************************************************************************/
+/**********************************************************************************/
+/***                                                                            ***/
+/***                               TARGET FUNCTION                              ***/
+/***                                                                            ***/
+/**********************************************************************************/
+/**********************************************************************************/
+/**
+ * @description: 比较函数
+ * =================================================================================
+ * @param {void} *a
+ * @param {void} *b
+ * @return {int}
+ */
 int cmp(const void *a, const void *b) {
     return *(int *)a - *(int *)b;
 }
 
-int* intersection(int* numsA, int numsASize, int* numsB, int numsBSize, int* returnSize){
+/**
+ * @description:
+ * =================================================================================
+ * @param {int} *numsA
+ * @param {int} numsASize
+ * @param {int} *numsB
+ * @param {int} numsBSize
+ * @param {int} *returnSize
+ * @return {int} *res
+ */
+int *intersection(int *numsA, int numsASize, int *numsB, int numsBSize, int *returnSize){
     qsort(numsA, numsASize, sizeof(int), cmp);          /* 将数组1排序 */
     qsort(numsB, numsBSize, sizeof(int), cmp);          /* 将数组2排序 */
     int *res = (int *)malloc(sizeof(int) * (numsASize < numsBSize ? numsASize : numsBSize));    /* 创建一个新数组用于保存结果 */
@@ -41,12 +62,41 @@ int* intersection(int* numsA, int numsASize, int* numsB, int numsBSize, int* ret
     *returnSize = k;                                    /* 返回数组大小为k */
     return res;                                         /* 返回结果数组 */
 }
-/* ==================================================================================================== */
-/* ==================================================================================================== */
 
-/* 主函数 */
+/**********************************************************************************/
+/*                                                                                */
+/*                                 HELPER FUNCTIONS                               */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 打印数组
+ * =================================================================================
+ * @param {int} *arr
+ * @param {int} arrSize
+ * @return {void}
+ */
+void printArray(int *arr, int arrSize) {
+    printf("Arr: ");
+    for (int i = 0; i < arrSize; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                  MAIN FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
 int main(int argc, char const *argv[]) {
-    // NOTE: Test case 1
+    /* Test case 1 */
     printf("======== Case 1 ======== \n");
     int nums1[] = {1, 2, 2, 1};
     int nums1Size = sizeof(nums1) / sizeof(nums1[0]);
@@ -56,7 +106,7 @@ int main(int argc, char const *argv[]) {
     int *ans = intersection(nums1, nums1Size, nums2, nums2Size, &returnSize);
     printArray(ans, returnSize);
 
-    // NOTE: Test case 2
+    /* Test case 2 */
     printf("======== Case 2 ======== \n");
     int nums3[] = {4, 9, 5};
     int nums4[] = {9, 4, 9, 8, 4};
