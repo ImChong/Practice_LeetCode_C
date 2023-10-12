aac919c5a3c41ded0933a411908588c6a7debebc
Chong Liu <liuc12@miamioh.edu>
2023-10-12 23:37:25 +0800

    Update main.c

--- 01_Programmercarl_Series/01_Array/03_sortedSquares/main.c
+++ 01_Programmercarl_Series/01_Array/03_sortedSquares/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-16 08:57:10
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-12 23:28:14
+ * @LastEditTime : 2023-10-12 23:37:20
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -12,6 +12,13 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                MACRO FUNCTIONS                                 */
+/*                                                                                */
+/**********************************************************************************/
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))        /* 获取数组长度 */
 
 /**********************************************************************************/
 /**********************************************************************************/
@@ -58,18 +65,33 @@ int *sortedSquares(int *nums, int numsSize, int *returnSize) {
 /*                                                                                */
 /**********************************************************************************/
 /**
- * @description: 验证答案
+ * @description:
  * =================================================================================
  * @param {char} testNum    测试编号
  * @param {int} expect      预期
  * @param {int} actual      实际
  * @return {void}
  */
-void validateAnswer(char testNum, int expect, int actual) {
-    if (expect == actual) {
-        printf("✅ Test %c Passed\n", testNum);
-    } else {
+/**
+ * @description: 验证答案
+ * =================================================================================
+ * @param {char} testNum        测试编号
+ * @param {int} *expectNums     预期结果
+ * @param {int} expectLen       预期结果长度
+ * @param {int} *actualNums     实际结果
+ * @param {int} actualLen       实际结果长度
+ * @return {*}
+ */
+void validateAnswer(char testNum, int *expectNums, int expectLen, int *actualNums, int actualLen) {
+    if (expectLen != actualLen) {
         printf("❌ Test %c Failed\n", testNum);
+    } else {
+        for (int i = 0; i < expectLen; i++) {
+            if (expectNums[i] != actualNums[i]) {
+                printf("❌ Test %c Failed\n", testNum);
+            }
+        }
+        printf("✅ Test %c Passed\n", testNum);
     }
 }
 
@@ -84,7 +106,20 @@ void validateAnswer(char testNum, int expect, int actual) {
  * @return {void}
  */
 void test_1(void) {
+    /* 实际结果 */
+    int nums[] = {-4, -1, 0, 3, 10};
+    int numsSize = ARRAY_SIZE(nums);
+    int returnSize;
+    int *returnNums = sortedSquares(nums, numsSize, &returnSize);
 
+    /* 预期结果 */
+    int expectNums[] = {0, 1, 9, 16, 100};
+    int expectSize = ARRAY_SIZE(expectNums);
+
+    /* 比较结果 */
+
+    /* 释放内存 */
+    free(returnNums);
 }
 
 /**
