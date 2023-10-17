5b528696dbde15218b7e5afdefaa9ba1a814b25c
Chong Liu <liuc12@miamioh.edu>
2023-10-17 23:20:09 +0800

    Update main.c

--- 01_Programmercarl_Series/05_Double_Pointer/02_reverseString/main.c
+++ 01_Programmercarl_Series/05_Double_Pointer/02_reverseString/main.c
@@ -59,6 +59,11 @@ void validateAnswer(char testNum, int expect, int actual) {
  * @return {void}
  */
 void test_1(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
 
 }
 
@@ -68,6 +73,11 @@ void test_1(void) {
  * @return {void}
  */
 void test_2(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
 
 }
 
@@ -77,6 +87,11 @@ void test_2(void) {
  * @return {void}
  */
 void test_3(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
 
 }
 
