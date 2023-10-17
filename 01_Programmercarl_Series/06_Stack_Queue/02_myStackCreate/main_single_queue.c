99a8ffe2aff2146b27729428d4e7aa58f4f2c01b
Chong Liu <liuc12@miamioh.edu>
2023-10-17 23:26:10 +0800

    Update main_single_queue.c

--- 01_Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_single_queue.c
+++ 01_Programmercarl_Series/06_Stack_Queue/02_myStackCreate/main_single_queue.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-25 20:30:05
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-17 23:25:01
+ * @LastEditTime : 2023-10-17 23:26:00
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -110,6 +110,27 @@ void myStackFree(MyStack *obj) {
         free(node);                         /* 释放当前元素指针所指空间 */
     }
     free(obj);                          /* 释放栈空间 */
+}
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                 HELPER FUNCTIONS                               */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 验证答案
+ * =================================================================================
+ * @param {char} testNum    测试编号
+ * @param {int} expect      预期
+ * @param {int} actual      实际
+ * @return {void}
+ */
+void validateAnswer(char testNum, int expect, int actual) {
+    if (expect == actual) {
+        printf("✅ Test %c Passed\n", testNum);
+    } else {
+        printf("❌ Test %c Failed\n", testNum);
+    }
 }
 
 /**********************************************************************************/
