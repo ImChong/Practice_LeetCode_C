09ba80b8ebcb45f6e34597113e487ed56822e646
Chong Liu <liuc12@miamioh.edu>
2023-10-17 23:41:51 +0800

    Update main.c

--- 01_Programmercarl_Series/10_Dynamic_Programming/03_minCostClimbingStairs/main.c
+++ 01_Programmercarl_Series/10_Dynamic_Programming/03_minCostClimbingStairs/main.c
@@ -1,6 +1,15 @@
-/* 746. 使用最小花费爬楼梯: https://leetcode.cn/problems/min-cost-climbing-stairs/ */
-/* https://programmercarl.com/0746.%E4%BD%BF%E7%94%A8%E6%9C%80%E5%B0%8F%E8%8A%B1%E8%B4%B9%E7%88%AC%E6%A5%BC%E6%A2%AF.html */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\10_Dynamic_Programming\03_minCostClimbingStairs\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-25 00:02:34
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-17 23:41:50
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 746. 使用最小花费爬楼梯: https://leetcode.cn/problems/min-cost-climbing-stairs/
+ * https://programmercarl.com/0746.%E4%BD%BF%E7%94%A8%E6%9C%80%E5%B0%8F%E8%8A%B1%E8%B4%B9%E7%88%AC%E6%A5%BC%E6%A2%AF.html
+ */
 #include <stdio.h>
 
 /* ==================================================================================================== */
@@ -14,6 +23,6 @@ int minCostClimbingStairs(int* cost, int costSize){
 
 /* NOTE：主函数 */
 int main(int argc, const char* argv[]) {
-    
+
     return 0;
 }
