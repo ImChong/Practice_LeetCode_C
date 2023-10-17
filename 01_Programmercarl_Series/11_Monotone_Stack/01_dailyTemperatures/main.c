1de02683a2f370463c7a8fa04db9145876b5902b
Chong Liu <liuc12@miamioh.edu>
2023-10-17 23:42:41 +0800

    Update main.c

--- 01_Programmercarl_Series/11_Monotone_Stack/01_dailyTemperatures/main.c
+++ 01_Programmercarl_Series/11_Monotone_Stack/01_dailyTemperatures/main.c
@@ -1,5 +1,14 @@
-/* 739. 每日温度: https://leetcode.cn/problems/daily-temperatures/ */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\11_Monotone_Stack\01_dailyTemperatures\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-24 00:26:57
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-17 23:42:35
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 739. 每日温度: https://leetcode.cn/problems/daily-temperatures/
+ */
 #include <stdio.h>
 
 /* ==================================================================================================== */
