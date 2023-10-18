3f4097d788b2ee5be9adc385b56dbc4149070a31
Chong Liu <liuc12@miamioh.edu>
2023-10-18 18:57:28 +0800

    Update main.c

--- 01_Programmercarl_Series/04_String/07_repeatedSubstringPattern/main.c
+++ 01_Programmercarl_Series/04_String/07_repeatedSubstringPattern/main.c
@@ -68,6 +68,11 @@ void validateAnswer(char testNum, int expect, int actual) {
  * @return {void}
  */
 void test_1(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
 
 }
 
@@ -77,6 +82,11 @@ void test_1(void) {
  * @return {void}
  */
 void test_2(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
 
 }
 
@@ -86,6 +96,11 @@ void test_2(void) {
  * @return {void}
  */
 void test_3(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
 
 }
 
