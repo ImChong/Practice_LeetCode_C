1c4181bcd4888a2c8c268f8281722c6a4dcd50ac
Chong Liu <liuc12@miamioh.edu>
2023-10-21 09:29:31 +0800

    update

---
+++ 01_Programmercarl_Series/03_Hash_Table/01_isAnagram/solutionFunc.h
@@ -0,0 +1,16 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\01_isAnagram\solutionFunc.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-21 08:58:40
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-21 09:10:40
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 242. 有效的字母异位词：https://leetcode.cn/problems/valid-anagram/
+ */
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isAnagram(char *s, char *t);
