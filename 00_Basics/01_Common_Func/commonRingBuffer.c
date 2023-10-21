5d5124cb4ffc1bb92d7740622fbf04e8dfe801a6
Chong Liu <liuc12@miamioh.edu>
2023-10-21 19:19:51 +0800

    Update commonRingBuffer.c

--- 00_Basics/01_Common_Func/commonRingBuffer.c
+++ 00_Basics/01_Common_Func/commonRingBuffer.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu | truegrit rainaftermath@qq.com
  * @CreateDate   : 2023-09-18 22:40:08
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-21 18:36:54
+ * @LastEditTime : 2023-10-21 19:19:34
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -11,6 +11,7 @@
  */
 #include <stdio.h>
 #include <stdint.h>
+#include "commonArray.h"
 #include "commonHeader.h"
 #include "commonRingBuffer.h"
 
