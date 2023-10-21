3df1522d06e4e4cf7a612a74b0c164bdec439fc1
Chong Liu <liuc12@miamioh.edu>
2023-10-21 19:27:32 +0800

    update

---
+++ 00_Basics/01_Common_Func/inc/commonBinaryTree.h
@@ -0,0 +1,17 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\inc\commonBinaryTree.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-21 19:27:05
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-21 19:27:17
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 二叉树相关的宏定义及宏函数
+ */
+#ifndef __COMMON_BINARY_TREE_H
+#define __COMMON_BINARY_TREE_H
+
+
+
+#endif  /* __COMMON_BINARY_TREE_H */
---
+++ 00_Basics/01_Common_Func/inc/commonLinkedList.h
@@ -0,0 +1,17 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\inc\commonLinkedList.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-21 19:26:13
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-21 19:26:27
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 链表相关的宏定义及宏函数
+ */
+#ifndef __COMMON_LINKED_LIST_H
+#define __COMMON_LINKED_LIST_H
+
+
+
+#endif  /* __COMMON_LINKED_LIST_H */
