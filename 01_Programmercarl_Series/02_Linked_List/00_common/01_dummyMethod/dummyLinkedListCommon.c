3640bb27288d0115a091eb5e84fe3ae40a2a144b
Chong Liu <liuc12@miamioh.edu>
2023-10-21 10:36:44 +0800

    update

---
+++ 01_Programmercarl_Series/02_Linked_List/00_common/01_dummyMethod/dummyLinkedListCommon.c
@@ -0,0 +1,11 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\02_Linked_List\00_common\01_dummyMethod\dummyLinkedListCommon.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-21 10:33:34
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-21 10:36:19
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 链表公共函数 - 使用虚拟头节点
+ */
