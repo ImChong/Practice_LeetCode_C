842452423bfb11be8b9e6eef8bfd228589a18030
Chong Liu <liuc12@miamioh.edu>
2023-10-22 13:06:32 +0800

    Update main.c

--- 01_Programmercarl_Series/01_Array/01_binarySearch/main.c
+++ 01_Programmercarl_Series/01_Array/01_binarySearch/main.c
@@ -11,7 +11,7 @@
  * https://programmercarl.com/0704.%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE
  */
 #include <stdio.h>
-#include "commonHelper.h"       /* 00_Basics\01_Common_Func\inc\commonHeader.h */
+#include "commonHelper.h"       /* 00_Basics\01_Common_Func\inc\commonHelper.h */
 #include "commonArray.h"        /* 00_Basics\01_Common_Func\inc\commonArray.h */
 
 /**********************************************************************************/
