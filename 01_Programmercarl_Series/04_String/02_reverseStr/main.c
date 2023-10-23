a355db06ae8878a66a4b1e6ac0207d0201c9917d
Chong Liu <liuc12@miamioh.edu>
2023-10-24 00:55:34 +0800

    update

--- 01_Programmercarl_Series/04_String/02_reverseStr/main.c
+++ 01_Programmercarl_Series/04_String/02_reverseStr/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-21 22:14:49
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-23 23:34:14
+ * @LastEditTime : 2023-10-24 00:54:21
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -12,6 +12,7 @@
  * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0541.%E5%8F%8D%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2II.md
  */
 #include <stdio.h>
+#include <string.h>
 
 /**********************************************************************************/
 /**********************************************************************************/
@@ -29,7 +30,7 @@
  */
 char *reverseStr(char *s, int k){
 
-    return 0;
+    return NULL;
 }
 
 /**********************************************************************************/
@@ -38,15 +39,22 @@ char *reverseStr(char *s, int k){
 /*                                                                                */
 /**********************************************************************************/
 /**
- * @description: 验证答案
+ * @description: 验证字符串
  * =================================================================================
  * @param {char} testNum    测试编号
  * @param {int} expect      预期
  * @param {int} actual      实际
  * @return {void}
  */
-void validateSingleValue(char testNum, int expect, int actual) {
-    if (expect == actual) {
+void validateString(char testNum, char *expect, char *actual) {
+    if (expect == NULL && actual == NULL) {
+        printf("✅ Test %c Passed\n", testNum);
+        return;
+    } else if (expect == NULL || actual == NULL) {
+        printf("❌ Test %c Failed\n", testNum);
+        return;
+    }
+    if (strcmp(expect, actual) == 0) {
         printf("✅ Test %c Passed\n", testNum);
     } else {
         printf("❌ Test %c Failed\n", testNum);
@@ -67,11 +75,15 @@ void validateSingleValue(char testNum, int expect, int actual) {
  */
 void test_1(void) {
     /* 实际结果 */
+    char *s = "abcdefg";
+    int k = 2;
+    char *actual = reverseStr(s, k);
 
     /* 预期结果 */
+    char *expect = "bacdfeg";
 
     /* 比较结果 */
-
+    validateString('1', expect, actual);
 }
 
 /**
@@ -83,11 +95,15 @@ void test_1(void) {
  */
 void test_2(void) {
     /* 实际结果 */
+    char *s = "abcd";
+    int k = 2;
+    char *actual = reverseStr(s, k);
 
     /* 预期结果 */
+    char *expect = "bacd";
 
     /* 比较结果 */
-
+    validateString('2', expect, actual);
 }
 
 /**
@@ -99,11 +115,15 @@ void test_2(void) {
  */
 void test_3(void) {
     /* 实际结果 */
+    char *s = "abcdefg";
+    int k = 4;
+    char *actual = reverseStr(s, k);
 
     /* 预期结果 */
+    char *expect = "dcbaefg";
 
     /* 比较结果 */
-
+    validateString('3', expect, actual);
 }
 
 /**********************************************************************************/
@@ -120,5 +140,7 @@ void test_3(void) {
  */
 int main(int argc, const char *argv[]) {
     test_1();
+    test_2();
+    test_3();
     return 0;
 }
