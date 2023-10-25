f509ff6b6f55924037d4098e7101e4495d9d1ac5
Chong Liu <liuc12@miamioh.edu>
2023-10-26 01:44:37 +0800

    Update main.c

--- 01_Programmercarl_Series/04_String/04_reverseWords/main.c
+++ 01_Programmercarl_Series/04_String/04_reverseWords/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-18 23:18:36
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-24 01:06:22
+ * @LastEditTime : 2023-10-26 01:44:04
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -12,6 +12,8 @@
  * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0151.%E7%BF%BB%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2%E9%87%8C%E7%9A%84%E5%8D%95%E8%AF%8D.md
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "commonString.h"
 
 /**********************************************************************************/
@@ -22,14 +24,16 @@
 /**********************************************************************************/
 /**********************************************************************************/
 /**
- * @description:
+ * @description: 反转字符串中的单词
  * =================================================================================
- * @param {char} *s
- * @return {char} *ans
+ * @param {char} *s         字符串地址
+ * @return {char} *ans      修改后的字符串
  */
 char *reverseWords(char *s) {
+    int sLen = strlen(s);
+    char *ans = (char *)malloc(sizeof(char) * sLen + 1);
 
-    return 0;
+    return ans;
 }
 
 /**********************************************************************************/
@@ -39,20 +43,30 @@ char *reverseWords(char *s) {
 /**********************************************************************************/
 /**
  * @description: 测试 1
+ * 输入：s = "the sky is blue"
+ * 输出："blue is sky the"
  * =================================================================================
  * @return {void}
  */
 void test_1(void) {
     /* 实际结果 */
+    char s[] = "the sky is blue";
+    char *actual = reverseWords(s);
 
     /* 预期结果 */
+    char *expect = "blue is sky the";
 
     /* 比较结果 */
+    validateString('1', expect, actual);
 
+    /* 释放内存 */
+    freeString(actual);
 }
 
 /**
  * @description: 测试 2
+ * 输入：s = "  hello world  "
+ * 输出："world hello"
  * =================================================================================
  * @return {void}
  */
@@ -67,6 +81,8 @@ void test_2(void) {
 
 /**
  * @description: 测试 3
+ * 输入：s = "a good   example"
+ * 输出："example good a"
  * =================================================================================
  * @return {void}
  */
