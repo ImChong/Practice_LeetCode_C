14f4da1bbf652b537ab5907644429de07b0c8f28
Chong Liu <liuc12@miamioh.edu>
2023-10-26 01:51:41 +0800

    Update main.c

--- 01_Programmercarl_Series/04_String/05_reverseLeftWords/main.c
+++ 01_Programmercarl_Series/04_String/05_reverseLeftWords/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-18 23:18:36
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-26 01:48:16
+ * @LastEditTime : 2023-10-26 01:50:43
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -12,6 +12,8 @@
  * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/%E5%89%91%E6%8C%87Offer58-II.%E5%B7%A6%E6%97%8B%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2.md
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "commonString.h"
 
 /**********************************************************************************/
@@ -29,7 +31,17 @@
  * @return {char} *ans          修改后的字符串
  */
 char *dynamicPassword(char *password, int target) {
-
+    int sLen = strlen(password);
+    char *ans = (char *)malloc(sizeof(char) * sLen + 1);
+    int index = 0;
+    for (int i = target; i < sLen; i++) {
+        ans[index++] = password[i];
+    }
+    for (int i = 0; i < target; i++) {
+        ans[index++] = password[i];
+    }
+    ans[sLen] = '\0';
+    return ans;
 }
 
 /**********************************************************************************/
@@ -46,11 +58,18 @@ char *dynamicPassword(char *password, int target) {
  */
 void test_1(void) {
     /* 实际结果 */
+    char s[] = "s3cur1tyC0d3";
+    int target = 4;
+    char *actual = dynamicPassword(s, target);
 
     /* 预期结果 */
+    char *expect = "r1tyC0d3s3cu";
 
     /* 比较结果 */
+    validateString('1', expect, actual);
 
+    /* 释放内存 */
+    freeString(actual);
 }
 
 /**
@@ -62,11 +81,18 @@ void test_1(void) {
  */
 void test_2(void) {
     /* 实际结果 */
+    char s[] = "lrloseumgh";
+    int target = 6;
+    char *actual = dynamicPassword(s, target);
 
     /* 预期结果 */
+    char *expect = "umghlrlose";
 
     /* 比较结果 */
+    validateString('2', expect, actual);
 
+    /* 释放内存 */
+    freeString(actual);
 }
 
 /**
@@ -78,11 +104,18 @@ void test_2(void) {
  */
 void test_3(void) {
     /* 实际结果 */
+    char s[] = "leetcode";
+    int target = 4;
+    char *actual = dynamicPassword(s, target);
 
     /* 预期结果 */
+    char *expect = "codeleet";
 
     /* 比较结果 */
+    validateString('3', expect, actual);
 
+    /* 释放内存 */
+    freeString(actual);
 }
 
 /**********************************************************************************/
@@ -99,5 +132,7 @@ void test_3(void) {
  */
 int main(int argc, const char *argv[]) {
     test_1();
+    test_2();
+    test_3();
     return 0;
 }
