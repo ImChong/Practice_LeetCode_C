267228206b11e8837d3ce11db38bf72243c44955
Chong Liu <liuc12@miamioh.edu>
2023-10-27 02:31:31 +0800

    Update compileCfg.h

--- 01_Programmercarl_Series/06_Stack_Queue/01_myQueueCreate/cfg/compileCfg.h
+++ 01_Programmercarl_Series/06_Stack_Queue/01_myQueueCreate/cfg/compileCfg.h
@@ -3,11 +3,11 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-27 02:14:55
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-27 02:26:23
+ * @LastEditTime : 2023-10-27 02:31:28
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : 通用模块编译配置文件
  */
 #ifndef __MODULE_CFG_H
 #define __MODULE_CFG_H
