79ea8f1b851f9a70a246a0099146fccd5964d1d7
Chong Liu <liuc12@miamioh.edu>
2023-10-27 01:39:45 +0800

    Create commonModuleDef.h

---
+++ 00_Basics/01_Common_Func/inc/commonDef/commonModuleDef.h
@@ -0,0 +1,28 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\inc\commonDef\commonModuleDef.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-27 01:30:54
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-27 01:39:13
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 通用模块定义头文件
+ */
+#ifndef __COMMON_MODULE_DEF_H
+#define __COMMON_MODULE_DEF_H
+
+#define CMD_MODULE_EN                   0
+#define HELPER_MODULE_EN                0
+#define RING_BUFFER_MODULE_EN           0
+#define ARRAY_MODULE_EN                 0
+#define LINKED_LIST_MODULE_EN           0
+#define LINKED_LIST_DUMMY_MODULE_EN     0
+#define HASH_TABLE_MODULE_EN            0
+#define STRING_MODULE_EN                0
+#define STACK_MODULE_EN                 0
+#define QUEUE_MODULE_EN                 0
+#define BINARY_TREE_MODULE_EN           0
+
+
+#endif  /* __COMMON_MODULE_DEF_H */
