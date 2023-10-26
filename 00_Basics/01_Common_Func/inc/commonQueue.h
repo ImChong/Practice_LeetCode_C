2d68def189f8f51d5b5879fb66fef5523b1e671c
Chong Liu <liuc12@miamioh.edu>
2023-10-26 13:40:46 +0800

    update

---
+++ 00_Basics/01_Common_Func/inc/commonQueue.h
@@ -0,0 +1,22 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\inc\commonQueue.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-26 13:38:47
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-26 13:39:19
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 通用队列头文件
+ */
+#ifndef __COMMON_QUEUE_H
+#define __COMMON_QUEUE_H
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                UTILITY FUNCTIONS                               */
+/*                                                                                */
+/**********************************************************************************/
+
+
+#endif  /* __COMMON_QUEUE_H */
