7f1888f20c9a8d648fe46269ff713a84c9d425c4
Chong Liu <liuc12@miamioh.edu>
2023-10-28 00:29:45 +0800

    Update main.c

--- 01_Programmercarl_Series/05_Double_Pointer/07_getIntersectionNode/main.c
+++ 01_Programmercarl_Series/05_Double_Pointer/07_getIntersectionNode/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-18 23:18:36
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-12 20:03:28
+ * @LastEditTime : 2023-10-28 00:29:19
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -17,34 +17,15 @@
 /*                                 TARGET FUNCTION                                */
 /*                                                                                */
 /**********************************************************************************/
-/* TODO: 目标函数 */
-
-/* ============================================================================== */
-/* ============================================================================== */
+/**
+ * @description:
+ * =================================================================================
+ * @param {struct ListNode} *headA
+ * @param {struct ListNode} *headB
+ * @return {struct ListNode} *ans
+ */
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
 
-}
-
-
-/**********************************************************************************/
-/*                                                                                */
-/*                                 HELPER FUNCTIONS                               */
-/*                                                                                */
-/**********************************************************************************/
-/**
- * @description: 验证答案
- * =================================================================================
- * @param {char} testNum    测试编号
- * @param {int} expect      预期
- * @param {int} actual      实际
- * @return {void}
- */
-void validateSingleValue(char testNum, int expect, int actual) {
-    if (expect == actual) {
-        printf("✅ Test %c Passed\n", testNum);
-    } else {
-        printf("❌ Test %c Failed\n", testNum);
-    }
 }
 
 /**********************************************************************************/
