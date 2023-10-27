ba5d5ee69e12dabb9dc91923f2c0ec1aaf7c869a
Chong Liu <liuc12@miamioh.edu>
2023-10-28 00:16:44 +0800

    update

--- 01_Programmercarl_Series/05_Double_Pointer/01_removeElement/main.c
+++ 01_Programmercarl_Series/05_Double_Pointer/01_removeElement/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-18 23:18:36
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-28 00:14:13
+ * @LastEditTime : 2023-10-28 00:16:27
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -11,8 +11,6 @@
  * https://programmercarl.com/0027.%E7%A7%BB%E9%99%A4%E5%85%83%E7%B4%A0.html
  * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0027.%E7%A7%BB%E9%99%A4%E5%85%83%E7%B4%A0.md
  */
-#include <stdio.h>
-#include "commonHelper.h"
 #include "commonArray.h"
 
 /**********************************************************************************/
