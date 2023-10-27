c4021b6ed0bc7669f83b142203dc5be4c149ed63
Chong Liu <liuc12@miamioh.edu>
2023-10-27 23:45:48 +0800

    Update main.c

--- 01_Programmercarl_Series/04_String/06_strStr/main.c
+++ 01_Programmercarl_Series/04_String/06_strStr/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-18 23:18:36
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-26 01:57:43
+ * @LastEditTime : 2023-10-27 23:45:47
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -34,6 +34,8 @@
 int strStr(char *haystack, char *needle){
     int hLen = strlen(haystack);        /* haystack长度 */
     int nLen = strlen(needle);          /* needle长度 */
+
+    /* TODO */
 
     return 0;
 }
