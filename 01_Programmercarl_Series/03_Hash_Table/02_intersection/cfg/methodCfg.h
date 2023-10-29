3f99fd22a79573617f53dac1d6b7c082d2e5a917
Chong Liu <liuc12@miamioh.edu>
2023-10-29 11:35:45 +0800

    update

---
+++ 01_Programmercarl_Series/03_Hash_Table/02_intersection/cfg/methodCfg.h
@@ -0,0 +1,18 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\02_intersection\cfg\methodCfg.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-29 11:32:41
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-29 11:35:42
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  :
+ */
+#ifndef __METHOD_CFG_H
+#define __METHOD_CFG_H
+
+#define DOUBLE_POINTER_METHOD_EN    1      /* 双指针法求解 - 1: enable, 0: disable */
+#define HASH_TABLE_METHOD_EN        1      /* 哈希表法求解 - 1: enable, 0: disable */
+
+#endif  /* __METHOD_CFG_H */
