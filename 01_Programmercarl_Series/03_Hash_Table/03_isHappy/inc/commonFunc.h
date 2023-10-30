6837cb6c7df8c7e15200c1e37b4e1bfb6d2e72a8
Chong Liu <liuc12@miamioh.edu>
2023-10-30 23:53:42 +0800

    Update commonFunc.h

--- 01_Programmercarl_Series/03_Hash_Table/03_isHappy/inc/commonFunc.h
+++ 01_Programmercarl_Series/03_Hash_Table/03_isHappy/inc/commonFunc.h
@@ -3,9 +3,15 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-30 23:52:16
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-30 23:52:31
+ * @LastEditTime : 2023-10-30 23:53:20
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : 快乐数 - 公共函数头文件
  */
+#ifndef __COMMON_FUNC_H
+#define __COMMON_FUNC_H
+
+int getNext(int n);     /* 计算下一个数 */
+
+#endif  /* __COMMON_FUNC_H */
