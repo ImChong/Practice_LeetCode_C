990c09908ec52368fc9321a79b3ba7bf8e8f6728
Chong Liu <liuc12@miamioh.edu>
2023-10-31 00:03:48 +0800

    update

--- 01_Programmercarl_Series/03_Hash_Table/03_isHappy/inc/isHappy_hash_table.h
+++ 01_Programmercarl_Series/03_Hash_Table/03_isHappy/inc/isHappy_hash_table.h
@@ -3,9 +3,15 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-30 23:46:16
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-30 23:58:09
+ * @LastEditTime : 2023-10-31 00:01:10
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 快乐数 - 哈希表法头文件
  */
+#ifndef __IS_HAPPY_HASH_TABLE_H
+#define __IS_HAPPY_HASH_TABLE_H
+
+bool isHappy(int n);
+
+#endif  /* __IS_HAPPY_HASH_TABLE_H */
