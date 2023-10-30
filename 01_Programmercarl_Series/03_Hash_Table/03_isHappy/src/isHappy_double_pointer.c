0d1884c0080b537cfa8b26eccf34954e8ed32bd6
Chong Liu <liuc12@miamioh.edu>
2023-10-30 23:59:59 +0800

    update

--- 01_Programmercarl_Series/03_Hash_Table/03_isHappy/src/isHappy_double_pointer.c
+++ 01_Programmercarl_Series/03_Hash_Table/03_isHappy/src/isHappy_double_pointer.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-30 23:45:33
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-30 23:58:58
+ * @LastEditTime : 2023-10-30 23:59:40
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -14,5 +14,27 @@
 #if FAST_SLOW_POINTER_METHOD_EN
 #include "isHappy_double_pointer.h"
 
+/**
+ * @description: 快乐数
+ * isHappy函数用于判断一个数字是否为快乐数。
+ *
+ * 这段代码使用了一个类似于快慢指针的算法来判断一个数字是否为快乐数。
+ *
+ * 该算法使用两个指针，一个慢指针和一个快指针，每次慢指针移动一位，快指针移动两位，
+ *      直到快指针指向的值为1或者快慢指针指向的值相同时，判断该数是否为快乐数。
+ *      如果是快乐数，则返回true，否则返回false。
+ * =================================================================================
+ * @param {int} n       当前数字
+ * @return {bool}       是否为快乐数
+ */
+bool isHappy(int n) {
+    int slow = n;                           /* 初始化慢指针：指向当前数值 */
+    int fast = getNext(n);                  /* 初始化快指针：指向下一数值 */
+    while (fast != 1 && slow != fast) {     /* 当快指针的值不为 1 或快指针与慢指针指向的值不相同时，保持循环 */
+        slow = getNext(slow);               /* 慢指针一次移动一位 */
+        fast = getNext(getNext(fast));      /* 快指针一次移动两位 */
+    }
+    return fast == 1;                       /* 当开始循环且快慢指针指向的值都为 1 时，返回 true */
+}
 
 #endif  /* FAST_SLOW_POINTER_METHOD_EN */
