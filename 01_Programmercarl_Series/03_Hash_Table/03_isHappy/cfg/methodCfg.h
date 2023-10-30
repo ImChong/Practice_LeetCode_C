8edb601a41be927197b246dc522c3815e9ef57ed
Chong Liu <liuc12@miamioh.edu>
2023-10-30 23:45:40 +0800

    update

--- 01_Programmercarl_Series/03_Hash_Table/03_isHappy/cfg/methodCfg.h
+++ 01_Programmercarl_Series/03_Hash_Table/03_isHappy/cfg/methodCfg.h
@@ -3,9 +3,16 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-30 23:42:48
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-30 23:42:51
+ * @LastEditTime : 2023-10-30 23:44:19
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 解题方法配置文件
  */
+#ifndef __METHOD_CFG_H
+#define __METHOD_CFG_H
+
+#define HASH_TABLE_METHOD_EN            1   /* 哈希表法 */
+#define FAST_SLOW_POINTER_METHOD_EN     0   /* 快慢指针法 */
+
+#endif  /* __METHOD_CFG_H */
