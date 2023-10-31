a735766c9363805aa69133982a5242b095203a65
Chong Liu <liuc12@miamioh.edu>
2023-10-31 12:18:09 +0800

    update

---
+++ 01_Programmercarl_Series/03_Hash_Table/05_fourSumCount/inc/fourSumCount_hash_table.h
@@ -0,0 +1,18 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\05_fourSumCount\inc\fourSumCount_hash_table.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-31 12:15:03
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-31 12:17:59
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 四数相加 - 哈希表法头文件
+ */
+#ifndef __FOUR_SUM_COUNT_HASH_TABLE_H
+#define __FOUR_SUM_COUNT_HASH_TABLE_H
+
+int fourSumCount(int *nums1, int nums1Size, int *nums2, int nums2Size,
+                    int *nums3, int nums3Size, int *nums4, int nums4Size);
+
+#endif  /* __FOUR_SUM_COUNT_HASH_TABLE_H */
---
+++ 01_Programmercarl_Series/03_Hash_Table/05_fourSumCount/src/fourSumCount_hash_table.c
@@ -0,0 +1,12 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\05_fourSumCount\src\fourSumCount_hash_table.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-31 12:15:07
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-31 12:16:29
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 四数相加 - 哈希表法源文件
+ */
+#include "methodCfg.h"
