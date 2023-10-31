3cf93c11330f9030296a89f95726f52f4516c669
Chong Liu <liuc12@miamioh.edu>
2023-10-31 12:14:08 +0800

    update

---
+++ 01_Programmercarl_Series/03_Hash_Table/05_fourSumCount/cfg/methodCfg.h
@@ -0,0 +1,23 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\04_twoSum\cfg\methodCfg.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-31 00:22:36
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-10-31 00:26:28
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 解题方法配置文件
+ */
+#ifndef __METHOD_CFG_H
+#define __METHOD_CFG_H
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                MACRO CONSTANTS                                 */
+/*                                                                                */
+/**********************************************************************************/
+#define DOUBLE_POINTER_METHOD_EN    1      /* 双指针法求解  1: enable, 0: disable */
+#define HASH_TABLE_METHOD_EN        0      /* 哈希表法求解  1: enable, 0: disable */
+
+#endif  /* __METHOD_CFG_H */
