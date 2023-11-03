e8859f7914ceeee25da6d96cdee0dc3d22b4d2fd
Chong Liu <liuc12@miamioh.edu>
2023-11-04 00:53:21 +0800

    Update build.py

--- .vscode/build.py
+++ .vscode/build.py
@@ -3,12 +3,13 @@ FilePath     : \Practice_LeetCode_C\.vscode\build.py
 Author       : Chong Liu
 CreateDate   : 2023-11-04 00:23:08
 LastEditors  : Chong Liu
-LastEditTime : 2023-11-04 00:41:19
+LastEditTime : 2023-11-04 00:53:20
 =================================================================================
 Copyright (c) 2023 by Chong Liu, All Rights Reserved.
 =================================================================================
 Description  : python 编译指令
 '''
+import subprocess
 
 if __name__ == "__main__":
     print("Hello World!")
