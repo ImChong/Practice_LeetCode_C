dd3619a6f06ddf17d6b17e65323d234113568154
Chong Liu <liuc12@miamioh.edu>
2023-11-04 01:09:14 +0800

    Update build.py

--- .vscode/build.py
+++ .vscode/build.py
@@ -3,7 +3,7 @@ FilePath     : \Practice_LeetCode_C\.vscode\build.py
 Author       : Chong Liu
 CreateDate   : 2023-11-04 00:23:08
 LastEditors  : Chong Liu
-LastEditTime : 2023-11-04 01:03:54
+LastEditTime : 2023-11-04 01:07:58
 =================================================================================
 Copyright (c) 2023 by Chong Liu, All Rights Reserved.
 =================================================================================
@@ -20,25 +20,25 @@ if __name__ == "__main__":
 
     # 如果有 ${fileDirname}/src 文件夹则编译
         # gcc
-        #     -I${workspaceFolder}/00_Basics/01_Common_Func/inc/
-        #     -I${workspaceFolder}/00_Basics/01_Common_Func/inc/commonDef/
-        #     -I${fileDirname}/cfg/
-        #     -I${fileDirname}/inc/
-        #     ${workspaceFolder}/00_Basics/01_Common_Func/src/*.c
-        #     ${fileDirname}/src/*.c
-        #     main.c
-        #     -o
-        #     main.exe
-        #     &&
-        #     $dirmain.exe
+        #     "-fdiagnostics-color=always",
+        #     "-I${workspaceFolder}/00_Basics/01_Common_Func/inc/",
+        #     "-I${workspaceFolder}/00_Basics/01_Common_Func/inc/commonDef/",
+        #     "-I${fileDirname}/cfg/",
+        #     "-I${fileDirname}/inc/",
+        #     "-g",
+        #     "${workspaceFolder}/00_Basics/01_Common_Func/src/*.c",
+        #     "${fileDirname}/src/*.c",
+        #     "main.c",
+        #     "-o",
+        #     "${fileDirname}/main.exe"
 
     # 如果没有 ${fileDirname}/src 文件夹则编译
         # gcc
-        #     -I${workspaceFolder}/00_Basics/01_Common_Func/inc/
-        #     -I${workspaceFolder}/00_Basics/01_Common_Func/inc/commonDef/
-        #     ${workspaceFolder}/00_Basics/01_Common_Func/src/*.c
-        #     main.c
-        #     -o
-        #     main.exe
-        #     &&
-        #     $dirmain.exe
+        #     "-fdiagnostics-color=always",
+        #     "-I${workspaceFolder}/00_Basics/01_Common_Func/inc/",
+        #     "-I${workspaceFolder}/00_Basics/01_Common_Func/inc/commonDef/",
+        #     "-g",
+        #     "${fileDirname}/src/*.c",
+        #     "main.c",
+        #     "-o",
+        #     "${fileDirname}/main.exe"
