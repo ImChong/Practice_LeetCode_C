207ad86f9c5284f8b4fc67a80a7a97cc7e166e41
Chong Liu <liuc12@miamioh.edu>
2023-11-04 20:20:27 +0800

    Update build.py

--- .vscode/build.py
+++ .vscode/build.py
@@ -3,7 +3,7 @@ FilePath     : \Practice_LeetCode_C\.vscode\build.py
 Author       : Chong Liu
 CreateDate   : 2023-11-04 00:23:08
 LastEditors  : Chong Liu
-LastEditTime : 2023-11-04 20:20:09
+LastEditTime : 2023-11-04 20:20:24
 =================================================================================
 Copyright (c) 2023 by Chong Liu, All Rights Reserved.
 =================================================================================
