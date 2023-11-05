ded450d214a2a15d3e4fa163755cf0d86ded25ce
Chong Liu <liuc12@miamioh.edu>
2023-11-05 13:17:11 +0800

    update

---
+++ 01_Programmercarl_Series/01_Array/05_generateMatrix/cfg/methodCfg.h
@@ -0,0 +1,11 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\01_Array\05_generateMatrix\cfg\methodCfg.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-11-05 13:03:05
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-11-05 13:03:08
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  :
+ */
