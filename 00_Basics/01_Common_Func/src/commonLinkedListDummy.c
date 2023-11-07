bb98a69f4fdc9f0389f1364c2effce084c201a20
Chong Liu <liuc12@miamioh.edu>
2023-11-07 23:01:50 +0800

    Update commonLinkedListDummy.c

--- 00_Basics/01_Common_Func/src/commonLinkedListDummy.c
+++ 00_Basics/01_Common_Func/src/commonLinkedListDummy.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-21 10:33:34
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-07 22:59:21
+ * @LastEditTime : 2023-11-07 23:01:41
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -41,14 +41,14 @@ DummyLinkedList *myLinkedListCreate(void) {
  * @return {int} ans                        返回指定索引的节点数值
  */
 int dummyLinkedListGet(DummyLinkedList *dummyHead, int index) {
-    if (index < 0 || index >= dummyHead->size) {      /* 输入校验 */
-        return -1;                                  /* 返回 -1 */
+    if (index < 0 || index >= dummyHead->size) {        /* 输入校验 */
+        return -1;                                          /* 返回 -1 */
     }
-    struct ListNode *curr = dummyHead->head;          /* 获取当前链表头节点的指针 */
-    while (index-- > 0) {                       /* 循环索引次 */
-        curr = curr->next;                          /* 指针向后移动一位 */
+    struct ListNode *curr = dummyHead->head;            /* 获取当前链表头节点的指针 */
+    while (index-- > 0) {                               /* 循环索引次 */
+        curr = curr->next;                                  /* 指针向后移动一位 */
     }
-    return curr->val;                           /* 返回指定索引的节点数值 */
+    return curr->val;                                   /* 返回指定索引的节点数值 */
 }
 
 /**
@@ -60,10 +60,10 @@ int dummyLinkedListGet(DummyLinkedList *dummyHead, int index) {
  */
 void dummyLinkedListAddAtHead(DummyLinkedList *dummyHead, int val) {
     struct ListNode *newHead = (struct ListNode*)malloc(sizeof(struct ListNode));    /* 为链表新头节点分配空间*/
-    newHead->val = val;             /* 为链表新头节点赋值 */
-    newHead->next = dummyHead->head;      /* 将链表新头节点的下一节点设置为链表旧头节点 */
-    dummyHead->head = newHead;            /* 将虚拟头节点指向的链表头节点设置为链表新头节点 */
-    dummyHead->size++;                    /* 链表长度 + 1 */
+    newHead->val = val;                     /* 为链表新头节点赋值 */
+    newHead->next = dummyHead->head;        /* 将链表新头节点的下一节点设置为链表旧头节点 */
+    dummyHead->head = newHead;              /* 将虚拟头节点指向的链表头节点设置为链表新头节点 */
+    dummyHead->size++;                      /* 链表长度 + 1 */
 }
 
 /**
@@ -75,19 +75,19 @@ void dummyLinkedListAddAtHead(DummyLinkedList *dummyHead, int val) {
  */
 void dummyLinkedListAddAtTail(DummyLinkedList *dummyHead, int val) {
     struct ListNode *newTail = (struct ListNode*)malloc(sizeof(struct ListNode));    /* 为链表新尾节点分配空间*/
-    newTail->val = val;                         /* 为链表新尾节点赋值 */
-    newTail->next = NULL;                       /* 将链表新尾节点的下一节点设置为NULL */
+    newTail->val = val;                                 /* 为链表新尾节点赋值 */
+    newTail->next = NULL;                               /* 将链表新尾节点的下一节点设置为NULL */
 
-    if (dummyHead->head == NULL) {                    /* 如果链表为空 */
-        dummyHead->head = newTail;                        /* 将虚拟头节点指向的链表头节点设置为链表新尾节点 */
-    } else {                                    /* 如果链表不为空 */
-        struct ListNode *curr = dummyHead->head;          /* 获取当前链表头节点的指针 */
-        while (curr->next != NULL) {                /* 当指针所指的节点不为NULL时 */
-            curr = curr->next;                          /* 指针向后移动一位 */
+    if (dummyHead->head == NULL) {                      /* 如果链表为空 */
+        dummyHead->head = newTail;                          /* 将虚拟头节点指向的链表头节点设置为链表新尾节点 */
+    } else {                                            /* 如果链表不为空 */
+        struct ListNode *curr = dummyHead->head;            /* 获取当前链表头节点的指针 */
+        while (curr->next != NULL) {                        /* 当指针所指的节点不为NULL时 */
+            curr = curr->next;                                  /* 指针向后移动一位 */
         }
-        curr->next = newTail;                       /* 将当前指针所指节点的下一节点指向新节点 */
+        curr->next = newTail;                               /* 将当前指针所指节点的下一节点指向新节点 */
     }
-    dummyHead->size++;                                /* 链表长度 + 1 */
+    dummyHead->size++;                                  /* 链表长度 + 1 */
 }
 
 /**
@@ -99,31 +99,31 @@ void dummyLinkedListAddAtTail(DummyLinkedList *dummyHead, int val) {
  * @return {void}
  */
 void dummyLinkedListAddAtIndex(DummyLinkedList *dummyHead, int index, int val) {
-    if (index < 0 || index > dummyHead->size) {       /* 输入校验 */
+    if (index < 0 || index > dummyHead->size) {         /* 输入校验 */
         return;
     }
 
-    if (index == 0) {                           /* 插入新头节点 */
+    if (index == 0) {                                   /* 插入新头节点 */
         dummyLinkedListAddAtHead(dummyHead, val);
         return;
     }
 
-    if (index == dummyHead->size) {                   /* 插入新尾节点 */
+    if (index == dummyHead->size) {                     /* 插入新尾节点 */
         dummyLinkedListAddAtTail(dummyHead, val);
         return;
     }
 
     struct ListNode *newNode = (struct ListNode*)malloc(sizeof(struct ListNode));    /* 为链表新节点分配空间 */
-    newNode->val = val;                         /* 为链表新节点赋值 */
+    newNode->val = val;                                 /* 为链表新节点赋值 */
 
-    struct ListNode *curr = dummyHead->head;          /* 获取当前链表头节点的指针 */
-    for (int i = 0; i < index - 1; i++) {       /* 因为新节点自己本身要占据一个节点位置，所以循环索引 - 1 次 */
-        curr = curr->next;                          /* 指针向后移动一位 */
+    struct ListNode *curr = dummyHead->head;            /* 获取当前链表头节点的指针 */
+    for (int i = 0; i < index - 1; i++) {               /* 因为新节点自己本身要占据一个节点位置，所以循环索引 - 1 次 */
+        curr = curr->next;                                  /* 指针向后移动一位 */
     }
 
-    newNode->next = curr->next;                 /* 将新节点的下一节点指向当前指针所指节点的下一节点*/
-    curr->next = newNode;                       /* 将当前指针所指节点的下一节点指向新节点 */
-    dummyHead->size++;                                /* 链表长度 + 1 */
+    newNode->next = curr->next;                         /* 将新节点的下一节点指向当前指针所指节点的下一节点*/
+    curr->next = newNode;                               /* 将当前指针所指节点的下一节点指向新节点 */
+    dummyHead->size++;                                  /* 链表长度 + 1 */
 }
 
 /**
@@ -134,24 +134,24 @@ void dummyLinkedListAddAtIndex(DummyLinkedList *dummyHead, int index, int val) {
  * @return {void}
  */
 void dummyLinkedListDeleteAtIndex(DummyLinkedList *dummyHead, int index) {
-    if (index < 0 || index >= dummyHead->size) {      /* 输入校验 */
+    if (index < 0 || index >= dummyHead->size) {        /* 输入校验 */
         return;
     }
 
-    struct ListNode *curr = dummyHead->head;          /* 获取当前链表头节点的指针 */
-    if (index == 0) {                           /* 如果删除头节点 */
-        dummyHead->head = curr->next;                    /* 将虚拟头节点所指向的链表头节点指向当前指针的下一节点 */
-    } else {                                    /* 如果删除中间节点 */
-        for (int i = 0; i < index - 1; i++) {       /* 因为目标节点自己本身要占据一个节点位置，所以循环索引 - 1 次 */
-            curr = curr->next;                          /* 指针向后移动一位 */
+    struct ListNode *curr = dummyHead->head;            /* 获取当前链表头节点的指针 */
+    if (index == 0) {                                   /* 如果删除头节点 */
+        dummyHead->head = curr->next;                       /* 将虚拟头节点所指向的链表头节点指向当前指针的下一节点 */
+    } else {                                            /* 如果删除中间节点 */
+        for (int i = 0; i < index - 1; i++) {               /* 因为目标节点自己本身要占据一个节点位置，所以循环索引 - 1 次 */
+            curr = curr->next;                                  /* 指针向后移动一位 */
         }
-        if (curr->next) {                           /* 如果当前指针所指节点的下一节点存在*/
-            struct ListNode* tmp = curr->next;          /* 获取待删除节点的指针 */
-            curr->next = curr->next->next;              /* 将待删除节点上一节点的下一节点改为待删除节点的下一节点*/
-            free(tmp);                                  /* 释放待删除节点内存空间 */
+        if (curr->next) {                                   /* 如果当前指针所指节点的下一节点存在*/
+            struct ListNode* tmp = curr->next;                  /* 获取待删除节点的指针 */
+            curr->next = curr->next->next;                      /* 将待删除节点上一节点的下一节点改为待删除节点的下一节点*/
+            free(tmp);                                          /* 释放待删除节点内存空间 */
         }
     }
-    dummyHead->size--;                                /* 链表长度 - 1 */
+    dummyHead->size--;                                  /* 链表长度 - 1 */
 }
 
 /**
@@ -231,13 +231,13 @@ void dummyLinkedListFromArray(DummyLinkedList *list, int *nums, int numsSize) {
 void dummyLinkedListPrint(DummyLinkedList *list) {
     printf("linked list (size: %d): Dummy -> ", list->size);    /* 打印链表长度 */
 
-    for (int i = 0; i < list->size; i++) {                              /* 循环打印链表节点数值 */
-        printf("%d ", dummyLinkedListGet(list, i));                            /* 打印链表节点数值 */
-        if (i < list->size - 1) {                                           /* 如果不是最后一个节点 */
-            printf("-> ");                                                      /* 打印箭头 */
+    for (int i = 0; i < list->size; i++) {                      /* 循环打印链表节点数值 */
+        printf("%d ", dummyLinkedListGet(list, i));                 /* 打印链表节点数值 */
+        if (i < list->size - 1) {                                   /* 如果不是最后一个节点 */
+            printf("-> ");                                              /* 打印箭头 */
         }
     }
-    printf("\n");                                                       /* 换行 */
+    printf("\n");                                               /* 换行 */
 }
 
 /**
