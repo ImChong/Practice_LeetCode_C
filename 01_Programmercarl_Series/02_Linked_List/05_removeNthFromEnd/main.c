b27675e513cb9a52432df2451066a794d98efed5
Chong Liu <liuc12@miamioh.edu>
2023-11-18 14:44:24 +0800

    Update main.c

--- 01_Programmercarl_Series/02_Linked_List/05_removeNthFromEnd/main.c
+++ 01_Programmercarl_Series/02_Linked_List/05_removeNthFromEnd/main.c
@@ -10,10 +10,12 @@
  * @Description  : 19. 删除链表的倒数第 N 个结点：https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
  * https://programmercarl.com/0019.%E5%88%A0%E9%99%A4%E9%93%BE%E8%A1%A8%E7%9A%84%E5%80%92%E6%95%B0%E7%AC%ACN%E4%B8%AA%E8%8A%82%E7%82%B9.html
  */
+/* 标准头文件 */
 #include <stdio.h>
 #include <stdlib.h>
-#include "commonArray.h"            /* 00_Basics\01_Common_Func\inc\commonArray.h */
-#include "commonLinkedList.h"       /* 00_Basics\01_Common_Func\inc\commonLinkedList.h */
+/* 通用头文件 */
+#include "commonArray.h"
+#include "commonLinkedList.h"
 
 /**********************************************************************************/
 /**********************************************************************************/
