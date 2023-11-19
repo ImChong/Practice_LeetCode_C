6a1fb26d59856c879a40f9ee2fa182ecba3eb442
Chong Liu <liuc12@miamioh.edu>
2023-11-19 21:04:52 +0800

    update

--- 01_Programmercarl_Series/03_Hash_Table/04_twoSum/src/twoSum_hash_table.c
+++ 01_Programmercarl_Series/03_Hash_Table/04_twoSum/src/twoSum_hash_table.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-31 00:23:15
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-18 19:30:52
+ * @LastEditTime : 2023-11-19 21:02:14
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -12,6 +12,11 @@
 /* 解题方法配置文件 */
 #include "methodCfg.h"
 #if HASH_TABLE_METHOD_EN
+/* 标准头文件 */
+#include <stdio.h>
+#include <stdlib.h>
+/* 通用头文件 */
+#include "commonHashTable.h"
 /* 本文件头文件 */
 #include "twoSum.h"
 
@@ -32,8 +37,25 @@
  * @return {int} *result        返回数组
  */
 int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
-    /* TODO */
-    return 0;                                                    /* 返回 NULL*/
+    struct HashTable *table = createHashTable(numsSize);
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+        int slot = getHashSlot(table, complement);
+        struct HashNode *currentNode = table->hashSlots[slot];
+        while (currentNode) {
+            if (currentNode->val == complement) {
+                int *result = (int *)malloc(sizeof(int) * 2);
+                result[0] = currentNode->index;
+                result[1] = i;
+                *returnSize = 2;
+                return result;
+            }
+            currentNode = currentNode->next;
+        }
+        insertHashTable(table, nums[i], i);
+    }
+    *returnSize = 0;
+    return NULL;
 }
 
 #endif  /* HASH_TABLE_METHOD_EN */
