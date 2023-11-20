ec0356651c1f4bf42e771103ea836fc7e37a2004
Chong Liu <liuc12@miamioh.edu>
2023-11-20 10:57:18 +0800

    Update commonHashTable.h

--- 00_Basics/01_Common_Func/inc/commonHashTable.h
+++ 00_Basics/01_Common_Func/inc/commonHashTable.h
@@ -20,8 +20,8 @@
 /* 哈希节点 */
 struct HashNode {
     int val;                    /* 元素值 */
-    int counter;                /* 元素个数（如果需要索引，此项则默认为 1） */
     int index;                  /* 元素索引（如果不需要索引，此项可以设置为 -1） */
+    int counter;                /* 元素个数（如果需要索引，此项则默认为 1） */
     struct HashNode *next;      /* 下一个节点 */
 };
 
