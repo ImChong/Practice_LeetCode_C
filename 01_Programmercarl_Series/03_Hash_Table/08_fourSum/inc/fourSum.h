6bfd86f0bdb8e8deac0b3196e5b84a18138012d1
Chong Liu <liuc12@miamioh.edu>
2023-11-22 23:33:41 +0800

    update

--- 01_Programmercarl_Series/03_Hash_Table/08_fourSum/inc/fourSum.h
+++ 01_Programmercarl_Series/03_Hash_Table/08_fourSum/inc/fourSum.h
@@ -1,11 +1,11 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_hashtable\08_fourSum\inc\fourSum.h
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\08_fourSum\inc\fourSum.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-11-22 10:12:48
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-22 18:30:02
+ * @LastEditTime : 2023-11-22 23:33:38
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : 四数之和头文件
  */
--- 01_Programmercarl_Series/03_Hash_Table/08_fourSum/src/fourSum_hashTable.c
+++ 01_Programmercarl_Series/03_Hash_Table/08_fourSum/src/fourSum_hashTable.c
@@ -1,11 +1,11 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_hashtable\08_fourSum\src\fourSum.c
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\08_fourSum\src\fourSum_hashTable.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-11-22 10:12:48
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-22 18:29:52
+ * @LastEditTime : 2023-11-22 23:33:12
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : 四数之和 - 哈希表
  */
