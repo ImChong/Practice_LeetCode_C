f50b9034fbda31c4667eb6a16b324485bd27940e
Chong Liu <liuc12@miamioh.edu>
2023-11-23 00:22:02 +0800

    Update fourSum_doublePointer.c

--- 01_Programmercarl_Series/03_Hash_Table/08_fourSum/src/fourSum_doublePointer.c
+++ 01_Programmercarl_Series/03_Hash_Table/08_fourSum/src/fourSum_doublePointer.c
@@ -1,23 +1,23 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\08_fourSum\src\fourSum_hashTable.c
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\08_fourSum\src\fourSum_doublePointer.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-11-22 10:12:48
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-23 00:16:45
+ * @LastEditTime : 2023-11-23 00:21:59
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  : 四数之和 - 哈希表
+ * @Description  : 四数之和 - 双指针
  */
 #include "methodCfg.h"
-#if HASH_TABLE_METHOD_EN
+#if DOUBLE_POINTER_METHOD_EN
 
 /**********************************************************************************/
 /*                                                                                */
 /*                                MACRO CONSTANTS                                 */
 /*                                                                                */
 /**********************************************************************************/
-#define HASH_SIZE     100
+#define LIST_SIZE     100
 
 /**********************************************************************************/
 /**********************************************************************************/
@@ -42,4 +42,4 @@ int **fourSum(int *nums, int numsSize, int target, int *returnSize, int **return
 }
 
 
-#endif /* HASH_TABLE_METHOD_EN */
+#endif /* DOUBLE_POINTER_METHOD_EN */
