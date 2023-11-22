cf16b0b6516600cb9bab746882f793b7127aa75f
Chong Liu <liuc12@miamioh.edu>
2023-11-23 01:32:26 +0800

    Create compileCfg.h

---
+++ 01_Programmercarl_Series/03_Hash_Table/06_canConstruct/cfg/compileCfg.h
@@ -0,0 +1,26 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\06_canConstruct\cfg\compileCfg.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-11-23 01:23:40
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-11-23 01:31:49
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 编译配置文件
+ */
+#ifndef __COMPILE_CFG_H
+#define __COMPILE_CFG_H
+
+/* 解题方法配置文件 */
+#include "methodCfg.h"
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                MACRO CONSTANTS                                 */
+/*                                                                                */
+/**********************************************************************************/
+#define COMMON_HASH_TABLE_EN        0    /* 通用哈希表法求解  1: enable, 0: disable */
+
+
+#endif  /* __COMPILE_CFG_H */
