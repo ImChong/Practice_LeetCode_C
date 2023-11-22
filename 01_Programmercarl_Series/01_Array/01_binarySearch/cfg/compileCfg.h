864b8c69194dce38ddb4606a7a586e989f8785fe
Chong Liu <liuc12@miamioh.edu>
2023-11-23 01:54:37 +0800

    Update compileCfg.h

--- 01_Programmercarl_Series/01_Array/01_binarySearch/cfg/compileCfg.h
+++ 01_Programmercarl_Series/01_Array/01_binarySearch/cfg/compileCfg.h
@@ -17,18 +17,18 @@
 /*                                MACRO CONSTANTS                                 */
 /*                                                                                */
 /**********************************************************************************/
-#define COMMON_ARRAY_EN             0                       /* 通用数组编译使能  1: enable, 0: disable */
-#define COMMON_BINARY_TREE_EN       0                       /* 通用二叉树编译使能  1: enable, 0: disable */
-#define COMMON_CMD_EN               0                       /* 通用命令行编译使能  1: enable, 0: disable */
-#define COMMON_HASH_TABLE_EN        0                       /* 通用哈希表编译使能  1: enable, 0: disable */
-#define COMMON_HELPER_EN            0                       /* 通用辅助编译使能  1: enable, 0: disable */
-#define COMMON_HEAP_EN              0                       /* 通用堆编译使能  1: enable, 0: disable */
-#define COMMON_LINKED_LIST_EN       0                       /* 通用链表编译使能  1: enable, 0: disable */
-#define COMMON_LINKED_LIST_DUMMY_EN 0                       /* 通用虚拟头节点链表编译使能  1: enable, 0: disable */
-#define COMMON_QUEUE_EN             0                       /* 通用队列编译使能  1: enable, 0: disable */
-#define COMMON_RING_BUFFER_EN       0                       /* 通用环形缓冲区编译使能  1: enable, 0: disable */
-#define COMMON_STACK_EN             0                       /* 通用栈编译使能  1: enable, 0: disable */
-#define COMMON_STRING_EN            0                       /* 通用字符串编译使能  1: enable, 0: disable */
+#define COMMON_ARRAY_EN                 0           /* 通用数组编译使能  1: enable, 0: disable */
+#define COMMON_BINARY_TREE_EN           0           /* 通用二叉树编译使能  1: enable, 0: disable */
+#define COMMON_CMD_EN                   0           /* 通用命令行编译使能  1: enable, 0: disable */
+#define COMMON_HASH_TABLE_EN            0           /* 通用哈希表编译使能  1: enable, 0: disable */
+#define COMMON_HELPER_EN                0           /* 通用辅助编译使能  1: enable, 0: disable */
+#define COMMON_HEAP_EN                  0           /* 通用堆编译使能  1: enable, 0: disable */
+#define COMMON_LINKED_LIST_EN           0           /* 通用链表编译使能  1: enable, 0: disable */
+#define COMMON_LINKED_LIST_DUMMY_EN     0           /* 通用虚拟头节点链表编译使能  1: enable, 0: disable */
+#define COMMON_QUEUE_EN                 0           /* 通用队列编译使能  1: enable, 0: disable */
+#define COMMON_RING_BUFFER_EN           0           /* 通用环形缓冲区编译使能  1: enable, 0: disable */
+#define COMMON_STACK_EN                 0           /* 通用栈编译使能  1: enable, 0: disable */
+#define COMMON_STRING_EN                0           /* 通用字符串编译使能  1: enable, 0: disable */
 
 
 #endif  /* __COMPILE_CFG_H */
