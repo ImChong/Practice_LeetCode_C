0b892e4a70f3396ad8f94d76b816eebac1065f04
Chong Liu <liuc12@miamioh.edu>
2023-11-23 02:00:36 +0800

    Update compileCfg.h

--- 01_Programmercarl_Series/01_Array/01_binarySearch/cfg/compileCfg.h
+++ 01_Programmercarl_Series/01_Array/01_binarySearch/cfg/compileCfg.h
@@ -21,11 +21,11 @@
 #define COMMON_BINARY_TREE_EN           0           /* 通用二叉树编译使能  1: enable, 0: disable */
 #define COMMON_CMD_EN                   0           /* 通用命令行编译使能  1: enable, 0: disable */
 #define COMMON_HASH_TABLE_EN            0           /* 通用哈希表编译使能  1: enable, 0: disable */
-#define COMMON_SINGLE_VALUE_EN          0           /* 通用单值函数编译使能  1: enable, 0: disable */
 #define COMMON_LINKED_LIST_EN           0           /* 通用链表编译使能  1: enable, 0: disable */
 #define COMMON_LINKED_LIST_DUMMY_EN     0           /* 通用虚拟头节点链表编译使能  1: enable, 0: disable */
 #define COMMON_QUEUE_EN                 0           /* 通用队列编译使能  1: enable, 0: disable */
 #define COMMON_RING_BUFFER_EN           0           /* 通用环形缓冲区编译使能  1: enable, 0: disable */
+#define COMMON_SINGLE_VALUE_EN          0           /* 通用单值函数编译使能  1: enable, 0: disable */
 #define COMMON_STACK_EN                 0           /* 通用栈编译使能  1: enable, 0: disable */
 #define COMMON_STRING_EN                0           /* 通用字符串编译使能  1: enable, 0: disable */
 
