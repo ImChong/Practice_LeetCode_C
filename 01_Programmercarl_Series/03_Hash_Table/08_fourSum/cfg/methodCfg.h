84f3f2871c22cf270c69aac726d8af3e5743b124
Chong Liu <liuc12@miamioh.edu>
2023-11-23 23:43:46 +0800

    update

--- 01_Programmercarl_Series/03_Hash_Table/08_fourSum/cfg/methodCfg.h
+++ 01_Programmercarl_Series/03_Hash_Table/08_fourSum/cfg/methodCfg.h
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-31 00:22:36
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-23 01:19:27
+ * @LastEditTime : 2023-11-23 23:43:20
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -17,7 +17,7 @@
 /*                                MACRO CONSTANTS                                 */
 /*                                                                                */
 /**********************************************************************************/
-#define HASH_TABLE_METHOD_EN        1                       /* 哈希表法求解  1: enable, 0: disable */
+#define HASH_TABLE_METHOD_EN        0                       /* 哈希表法求解  1: enable, 0: disable */
 #define DOUBLE_POINTER_METHOD_EN    !HASH_TABLE_METHOD_EN   /* 双指针法求解  1: enable, 0: disable */
 
 
