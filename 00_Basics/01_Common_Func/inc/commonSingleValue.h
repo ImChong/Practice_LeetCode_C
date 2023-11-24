9d25aa729cde33d3dad5baddb015cdbce6ff46bc
Chong Liu <liuc12@miamioh.edu>
2023-11-24 19:27:10 +0800

    Update commonSingleValue.h

--- 00_Basics/01_Common_Func/inc/commonSingleValue.h
+++ 00_Basics/01_Common_Func/inc/commonSingleValue.h
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-21 13:42:27
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-24 15:24:20
+ * @LastEditTime : 2023-11-24 18:58:18
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -17,11 +17,17 @@
 /*                                MACRO FUNCTIONS                                 */
 /*                                                                                */
 /**********************************************************************************/
+#define type_of_ptr(x)      _Generic((x), \
+                                int*: "int*", \
+                                float*: "float*", \
+                                double*: "double*", \
+                                default: "other")
+
 #define type_of(x)          _Generic((x), \
                                 int: "int", \
                                 float: "float", \
                                 double: "double", \
-                                default: "other")
+                                default: type_of_ptr(x))
 
 #define MAX(a, b)           ((a) > (b) ? (a) : (b))     /* 取最大值 */
 #define MIN(a, b)           ((a) < (b) ? (a) : (b))     /* 取最小值 */
