af31fda72776cc84a177fae4fd75d522fcd4cb57
Chong Liu <liuc12@miamioh.edu>
2023-11-25 23:00:00 +0800

    Update commonQueue.h

--- 00_Basics/01_Common_Func/inc/commonQueue.h
+++ 00_Basics/01_Common_Func/inc/commonQueue.h
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-26 13:38:47
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-25 22:54:48
+ * @LastEditTime : 2023-11-25 22:59:42
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -29,8 +29,8 @@ typedef struct {
 /*                                UTILITY FUNCTIONS                               */
 /*                                                                                */
 /**********************************************************************************/
-Queue *initQueue(int queueSize);            /* 初始化队列 */
-void enQueue(Queue *queue, int val);            /* 入队 */
+Queue *initQueue(int queueSize);        /* 初始化队列 */
+void enQueue(Queue *queue, int val);    /* 入队 */
 int deQueue(Queue *queue);            /* 出队 */
 int isQueueEmpty(Queue *queue);       /* 判断队列是否为空 */
 int isQueueFull(Queue *queue);        /* 判断队列是否为满 */
