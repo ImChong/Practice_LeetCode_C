34afab504972d792eccf1c4e3e477934d622f3df
Chong Liu <liuc12@miamioh.edu>
2023-11-26 11:22:29 +0800

    Update main.c

--- 01_Programming_Topics/02_Linked_List/11_middleNode/main.c
+++ 01_Programming_Topics/02_Linked_List/11_middleNode/main.c
@@ -1,4 +1,73 @@
-/* 876. 链表的中间结点：https://leetcode.cn/problems/middle-of-the-linked-list/ */
-/* https://labuladong.github.io/algo/di-ling-zh-bfe1b/shuang-zhi-0f7cc/ */
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\02_Linked_List\11_middleNode\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-18 23:18:36
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-11-26 11:22:28
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 876. 链表的中间结点：https://leetcode.cn/problems/middle-of-the-linked-list/
+ * https://labuladong.github.io/algo/di-ling-zh-bfe1b/shuang-zhi-0f7cc/
+ */
 
 /* TODO: 重构 */
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                  TEST FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 测试 1
+ * =================================================================================
+ * @return {void}
+ */
+void test_1(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
+
+}
+
+/**
+ * @description: 测试 2
+ * =================================================================================
+ * @return {void}
+ */
+void test_2(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
+
+}
+
+/**
+ * @description: 测试 3
+ * =================================================================================
+ * @return {void}
+ */
+void test_3(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
+
+}
+
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
+
+    return 0;
+}
