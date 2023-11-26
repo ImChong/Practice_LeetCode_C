469636663aea97cec06c6dc9ff40d725dede4693
Chong Liu <liuc12@miamioh.edu>
2023-11-26 11:12:37 +0800

    Update main.c

--- 01_Programming_Topics/02_Linked_List/07_hasCycle/main.c
+++ 01_Programming_Topics/02_Linked_List/07_hasCycle/main.c
@@ -1,4 +1,44 @@
-/* 141. 环形链表：https://leetcode.cn/problems/linked-list-cycle/ */
-/* https://labuladong.github.io/algo/di-ling-zh-bfe1b/shuang-zhi-0f7cc/ */
-
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\02_Linked_List\07_hasCycle\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-08-18 23:18:36
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-11-26 11:12:36
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 141. 环形链表：https://leetcode.cn/problems/linked-list-cycle/
+ * https://labuladong.github.io/algo/di-ling-zh-bfe1b/shuang-zhi-0f7cc/
+ */
 /* TODO: 重构 */
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                  TEST FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 测试 1
+ * =================================================================================
+ * @return {void}
+ */
+void test_1(void) {
+    /* 实际结果 */
+
+    /* 预期结果 */
+
+    /* 比较结果 */
+
+}
+
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
+
+    return 0;
+}
