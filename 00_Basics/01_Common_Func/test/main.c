0cdd5af795f1eb88d7dc6221f1dc2d7b37ed475b
Chong Liu <liuc12@miamioh.edu>
2023-12-01 02:09:44 +0800

    update

---
+++ 00_Basics/01_Common_Func/test/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
+
+    return 0;
+}
