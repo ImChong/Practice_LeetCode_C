2e2572fa12b9dcde01bdc2df429c445a5fe6fb0f
Chong Liu <liuc12@miamioh.edu>
2023-12-01 02:25:12 +0800

    update

--- 00_Basics/01_Common_Func/tests/commonBinaryTreeTest.cc
+++ 00_Basics/01_Common_Func/tests/commonBinaryTreeTest.cc
@@ -1,13 +1,13 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\tests\commonArrayTest.cc
+ * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\tests\commonBinaryTreeTest.cc
  * @Author       : Chong Liu
- * @CreateDate   : 2023-12-01 02:17:51
+ * @CreateDate   : 2023-12-01 02:24:32
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-01 02:23:16
+ * @LastEditTime : 2023-12-01 02:25:09
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  : 通用数组单元测试
+ * @Description  : 通用二叉树单元测试
  */
 
 main() {
