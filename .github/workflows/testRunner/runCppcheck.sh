8b10d09db898109d76889780a32f48516364000e
Chong Liu <liuc12@miamioh.edu>
2023-12-02 18:08:45 +0800

    Update runCppcheck.sh

--- .github/workflows/testRunner/runCppcheck.sh
+++ .github/workflows/testRunner/runCppcheck.sh
@@ -10,5 +10,9 @@ TARGET_DIR="./00_Basics/01_Common_Func"
 for file in $(find $TARGET_DIR -name '*.c' -or -name '*.h')
 do
     echo "=============================="
-    cppcheck $file
+    output=$(cppcheck $file)
+    echo "$output"
+    if echo "$output" | grep -q "error"; then   # if output contains "error"
+        exit 1                                      # exit with error
+    fi
 done
