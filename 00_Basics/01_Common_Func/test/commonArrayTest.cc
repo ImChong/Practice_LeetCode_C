8768cdfcb1b46bcc24f037f359129520953f7aaf
Chong Liu <liuc12@miamioh.edu>
2023-12-02 10:17:20 +0800

    update

--- 00_Basics/01_Common_Func/test/commonArrayTest.cc
+++ 00_Basics/01_Common_Func/test/commonArrayTest.cc
@@ -1,14 +1,15 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\tests\commonArrayTest.cc
+ * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\test\commonArrayTest.cc
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-01 02:17:51
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-01 03:03:22
+ * @LastEditTime : 2023-12-02 10:10:57
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 通用数组单元测试
  */
+/* 标准头文件 */
 #include <stdio.h>
 
 int main() {
