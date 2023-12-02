e4ddc69ecfc8a2ecaf99425da20c6ed50483a08a
Chong Liu <liuc12@miamioh.edu>
2023-12-02 18:15:30 +0800

    Update runCppcheck.sh

--- .github/workflows/testRunner/runCppcheck.sh
+++ .github/workflows/testRunner/runCppcheck.sh
@@ -10,12 +10,13 @@ TARGET_DIR="./00_Basics/01_Common_Func"
 for file in $(find $TARGET_DIR -name '*.c' -or -name '*.h')
 do
     echo "=============================="
-    output=$(cppcheck $file)
+    cppcheck $file --output-file=cppcheck_output.txt
+    output=$(cat cppcheck_output.txt)
     echo "$output"
     echo "test point 1"
-    if echo "$output" | grep -q "error"; then   # if output contains "error"
+    if echo "$output" | grep -q "error"; then
         echo "test point 2"
         echo "cppcheck found errors!"
-        exit 1                                      # exit with error
+        exit 1
     fi
 done
