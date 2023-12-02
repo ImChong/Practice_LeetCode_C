a2b684a06f649b46f16e3fbf46e5fbc758ab887d
Chong Liu <liuc12@miamioh.edu>
2023-12-02 23:36:06 +0800

    Update commonStringTest.cc

--- 00_Basics/01_Common_Func/test/commonStringTest.cc
+++ 00_Basics/01_Common_Func/test/commonStringTest.cc
@@ -4,7 +4,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-01 02:24:32
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-02 23:32:58
+ * @LastEditTime : 2023-12-02 23:35:21
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -65,7 +65,7 @@ TEST_F(TestCommonString, TestSwapChar) {
 TEST_F(TestCommonString, TestReverseStringSection) {
     /* 准备数据 */
     char *s = (char *)malloc(sizeof(char) * 8);     /* NOLINT(readability/casting) */
-    strcpy(s, "abcdefg");
+    snprintf(s, 8, "%s", "abcdefg");
 
     /* 运行结果 */
     reverseStringSection(s, 2, 5);
