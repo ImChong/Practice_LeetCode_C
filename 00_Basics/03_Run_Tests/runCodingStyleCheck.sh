3832416f8ac662caa8c48d2639c1f38c1bf49433
Chong Liu <liuc12@miamioh.edu>
2023-12-02 14:37:18 +0800

    Update runCodingStyleCheck.sh

--- 00_Basics/03_Run_Tests/runCodingStyleCheck.sh
+++ 00_Basics/03_Run_Tests/runCodingStyleCheck.sh
@@ -5,6 +5,7 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 TARGET_DIR="./00_Basics/01_Common_Func"
 
 for file in "$TARGET_DIR"/*.{c,h}; do
+    echo "Checking $file..."
     if [ -f "$file" ]; then
         cppcheck "$file" || { echo "cppcheck failed for $file"; exit 1; }
         echo "===> cppcheck passed for $file."
