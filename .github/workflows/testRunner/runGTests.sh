b8dc3faa602816a4099d3bca3d9c5e153cfe4041
Chong Liu <liuc12@miamioh.edu>
2023-12-02 17:32:46 +0800

    update

--- .github/workflows/testRunner/runGTests.sh
+++ .github/workflows/testRunner/runGTests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status.
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# run gtest executables in the build/bin directory
 for test in $(find ./build/bin -executable -type f); do
+    echo "=============================="
     $test
 done
+
+echo "=============================="
+echo "All executables have been run!"
