04f3932cbf2d9b5b652ffe1b42a2630a74f17c28
Chong Liu <liuc12@miamioh.edu>
2023-12-02 14:33:03 +0800

    update

--- 00_Basics/03_Run_Tests/runCodingStyleCheck.sh
+++ 00_Basics/03_Run_Tests/runCodingStyleCheck.sh
@@ -4,7 +4,8 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 
 TARGET_DIR="./00_Basics/01_Common_Func"
 
-for file in "$TARGET_DIR"/*; do
-    clang-format -i "$file" || { echo "clang-format check failed for $file"; exit 1; }
-    cppcheck "$file" || { echo "cppcheck failed for $file"; exit 1; }
+for file in "$TARGET_DIR"/*.{c,h}; do
+    if [ -f "$file" ]; then
+        cppcheck "$file" || { echo "cppcheck failed for $file"; exit 1; }
+    fi
 done
