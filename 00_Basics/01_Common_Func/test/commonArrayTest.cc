a91d6342d78cd511bfcffd08afcb0f1694fdfe82
Chong Liu <liuc12@miamioh.edu>
2023-12-02 23:12:02 +0800

    update

--- 00_Basics/01_Common_Func/test/commonArrayTest.cc
+++ 00_Basics/01_Common_Func/test/commonArrayTest.cc
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-01 02:17:51
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-02 22:37:31
+ * @LastEditTime : 2023-12-02 23:06:28
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -46,24 +46,36 @@ class TestCommonArray : public ::testing::Test {
 /* ARR_SIZE */
 /* ============================================================================== */
 TEST_F(TestCommonArray, TestArrSize) {
+    /* 准备数据 */
     int arr[5] = {1, 2, 3, 4, 5};
-    EXPECT_EQ((int)ARR_SIZE(arr), 5);
+
+    /* 运行结果 */
+    int arrSize = ARR_SIZE(arr);
+
+    /* 比较结果 */
+    EXPECT_EQ(arrSize, 5);
 }
 
 /* ============================================================================== */
 /* compareElements */
 /* ============================================================================== */
 TEST_F(TestCommonArray, TestCompareElements) {
+    /* 准备数据 */
     int a = 5;
     int b = 3;
+    /* 运行结果 & 比较结果 */
     EXPECT_EQ(compareElements(&a, &b), 2);
 
+    /* 准备数据 */
     a = 3;
     b = 5;
+    /* 运行结果 & 比较结果 */
     EXPECT_EQ(compareElements(&a, &b), -2);
 
+    /* 准备数据 */
     a = 5;
     b = 5;
+    /* 运行结果 & 比较结果 */
     EXPECT_EQ(compareElements(&a, &b), 0);
 }
 
@@ -71,8 +83,13 @@ TEST_F(TestCommonArray, TestCompareElements) {
 /* sortArray */
 /* ============================================================================== */
 TEST_F(TestCommonArray, TestSortArray) {
+    /* 准备数据 */
     int arr[5] = {5, 4, 3, 2, 1};
+
+    /* 运行结果 */
     sortArray(arr, 5);
+
+    /* 比较结果 */
     EXPECT_EQ(arr[0], 1);
     EXPECT_EQ(arr[1], 2);
     EXPECT_EQ(arr[2], 3);
@@ -84,18 +101,23 @@ TEST_F(TestCommonArray, TestSortArray) {
 /* sort2DArray */
 /* ============================================================================== */
 TEST_F(TestCommonArray, TestSort2DArray) {
-    int arr[2][3] = {{5, 4, 3}, {2, 1, 0}};
-    int *arrPtr[2] = {arr[0], arr[1]};
+    /* 准备数据 */
+    int **arrPtr = (int **)malloc(sizeof(int *) * 2);
+    for (int i = 0; i < 2; i++) {
+        arrPtr[i] = (int *)malloc(sizeof(int) * 3);
+    }
+    arrPtr[0][0] = 5;
+    arrPtr[0][1] = 4;
+    arrPtr[0][2] = 3;
+    arrPtr[1][0] = 2;
+    arrPtr[1][1] = 1;
+    arrPtr[1][2] = 0;
     int arrColSize[2] = {3, 3};
+
+    /* 运行结果 */
     sort2DArray(arrPtr, 2, arrColSize);
 
-    EXPECT_EQ(arr[0][0], 3);
-    EXPECT_EQ(arr[0][1], 4);
-    EXPECT_EQ(arr[0][2], 5);
-    EXPECT_EQ(arr[1][0], 0);
-    EXPECT_EQ(arr[1][1], 1);
-    EXPECT_EQ(arr[1][2], 2);
-
+    /* 比较结果 */
     EXPECT_EQ(arrPtr[0][0], 0);
     EXPECT_EQ(arrPtr[0][1], 1);
     EXPECT_EQ(arrPtr[0][2], 2);
