86c29aaf03b2592f2509bc6e1cd614bff31cfdab
Chong Liu <liuc12@miamioh.edu>
2023-12-02 14:45:36 +0800

    Update runCodingStyleCheck.sh

--- 00_Basics/03_Run_Tests/runCodingStyleCheck.sh
+++ 00_Basics/03_Run_Tests/runCodingStyleCheck.sh
@@ -4,12 +4,12 @@ echo "Running cppcheck..."
 set -e  # Exit immediately if a command exits with a non-zero status.
 TARGET_DIR="./00_Basics/01_Common_Func"
 
-find $TARGET_DIR -name "*.c" -o -name "*.h" -print
+find "$TARGET_DIR" -type f \( -name "*.c" -o -name "*.h" \) -print
 
 echo " =============== "
 
-find $TARGET_DIR \
-    -name "*.c" -o -name "*.h" \
-    -exec echo "Checking file: {} ..." \; \
-    -exec cppcheck {} \; \
-    || { echo "cppcheck failed for {}"; exit 1; }
+# find $TARGET_DIR \
+#     -name "*.c" -o -name "*.h" \
+#     -exec echo "Checking file: {} ..." \; \
+#     -exec cppcheck {} \; \
+#     || { echo "cppcheck failed for {}"; exit 1; }
