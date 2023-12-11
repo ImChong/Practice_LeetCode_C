67ce1f36ddb6b3819bc186e7ad0c56becec505f5
Chong Liu <liuc12@miamioh.edu>
2023-12-11 18:45:57 +0800

    Create methodCfg.h

---
+++ 01_Programming_Topics/07_Binary_Tree/05_levelOrderBottom/cfg/methodCfg.h
@@ -0,0 +1,22 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\07_Binary_Tree\04_levelOrder\cfg\methodCfg.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-10-29 11:32:41
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-12-08 20:19:18
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 解题方法配置文件
+ */
+#ifndef __METHOD_CFG_H
+#define __METHOD_CFG_H
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                MACRO CONSTANTS                                 */
+/*                                                                                */
+/**********************************************************************************/
+#define ARRAY_METHOD_EN         0                       /* 数组方法使能，1：使能，0：关闭 */
+
+#endif  /* __METHOD_CFG_H */
