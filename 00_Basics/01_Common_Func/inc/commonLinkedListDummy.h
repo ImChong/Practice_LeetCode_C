b28444868c4a9e0681c391d786dc1c68afbd5e4e
Chong Liu <liuc12@miamioh.edu>
2023-12-13 22:34:14 +0800

    update

--- 00_Basics/01_Common_Func/inc/commonLinkedListDummy.h
+++ 00_Basics/01_Common_Func/inc/commonLinkedListDummy.h
@@ -32,7 +32,7 @@ typedef struct {
 /*                                UTILITY FUNCTIONS                               */
 /*                                                                                */
 /**********************************************************************************/
-DummyLinkedList *myLinkedListCreate(void);     /* 创建使用虚拟头节点的链表 */
+DummyLinkedList *dummyLinkedListCreate(void);     /* 创建使用虚拟头节点的链表 */
 int dummyLinkedListGet(DummyLinkedList *dummyHead, int index);        /* 获取链表中第index个节点的值 */
 void dummyLinkedListAddAtHead(DummyLinkedList *dummyHead, int val);       /* 在链表头部添加一个节点 */
 void dummyLinkedListAddAtTail(DummyLinkedList *dummyHead, int val);       /* 在链表尾部添加一个节点 */
---
+++ 00_Basics/01_Common_Func/test/commonLinkedListDummyTest.cc
@@ -0,0 +1,120 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\test\commonLinkedListDummyTest.cc
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-12-13 22:14:22
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-12-13 22:32:39
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 通用链表头单元测试 - 未使用虚拟头节点
+ */
+/* gtest 头文件 */
+#include "gtest/gtest.h"
+/* 测试目标头文件 */
+extern "C" {
+    #include "commonTypeDef.h"
+    #include "commonLinkedListDummy.h"
+}
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                   TEST SUITE                                   */
+/*                                                                                */
+/**********************************************************************************/
+class TestCommonLinkedListDummy : public ::testing::Test {
+ public:
+    static void SetUpTestCase() {
+        // Code here will be called once before all tests
+    }
+    void SetUp() override {
+        // Code here will be called before each test
+    }
+    void TearDown() override {
+        // Code here will be called after each test
+    }
+    static void TearDownTestCase() {
+        // Code here will be called once after all tests
+    }
+};
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                   TEST CASES                                   */
+/*                                                                                */
+/**********************************************************************************/
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                 UTILITY FUNCTIONS                              */
+/*                                                                                */
+/**********************************************************************************/
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListCreate) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListGet) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListAddAtHead) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListAddAtTail) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListAddAtIndex) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListDeleteAtIndex) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListDeleteElement) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListGetIndexes) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                HELPER FUNCTIONS                                */
+/*                                                                                */
+/**********************************************************************************/
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListFromArray) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListPrint) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestCommonLinkedListDummy, TestDummyLinkedListFree) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                  TEST FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+TEST_F(TestCommonLinkedListDummy, TestValidateDummyLinkedList) {
+    /* TODO */
+    EXPECT_EQ(0, 0);
+}
