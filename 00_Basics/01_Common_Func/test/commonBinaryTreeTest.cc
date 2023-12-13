2d995e7dc27bac476065e1da62de3c02630cf772
Chong Liu <liuc12@miamioh.edu>
2023-12-13 10:56:49 +0800

    update

---
+++ 00_Basics/01_Common_Func/test/commonBinaryTreeTest.cc
@@ -0,0 +1,54 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\test\commonCmdTest.cc
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-12-13 00:47:48
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-12-13 01:06:43
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 通用命令行指令单元测试
+ */
+/* gtest 头文件 */
+#include "gtest/gtest.h"
+/* 测试目标头文件 */
+extern "C" {
+    #include "commonTypeDef.h"
+    #include "commonBinaryTree.h"
+}
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                   TEST SUITE                                   */
+/*                                                                                */
+/**********************************************************************************/
+class TestCommonBinaryTree : public ::testing::Test {
+ public:
+    static void SetUpTestCase() {
+        // Code here will be called once before all tests
+    }
+    void SetUp() override {
+        // Code here will be called before each test
+    }
+    void TearDown() override {
+        // Code here will be called after each test
+    }
+    static void TearDownTestCase() {
+        // Code here will be called once after all tests
+    }
+};
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                   TEST CASES                                   */
+/*                                                                                */
+/**********************************************************************************/
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                UTILITY FUNCTIONS                               */
+/*                                                                                */
+/**********************************************************************************/
+TEST_F(TestCommonBinaryTree, TestPreOrderTraversal_recursion) {
+    EXPECT_EQ(0, 0);
+}
