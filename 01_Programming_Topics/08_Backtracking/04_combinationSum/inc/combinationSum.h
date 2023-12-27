e7adf58deffc888085843e8e4fc0eb180330b020
Chong Liu <liuc12@miamioh.edu>
2023-12-28 00:43:32 +0800

    update

---
+++ 01_Programming_Topics/08_Backtracking/04_combinationSum/inc/combinationSum.h
@@ -0,0 +1,11 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\08_Backtracking\04_combinationSum\inc\combinationSum.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-12-26 13:19:53
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-12-28 00:43:08
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  :
+ */
--- 01_Programming_Topics/08_Backtracking/04_combinationSum/main.c
+++ 01_Programming_Topics/08_Backtracking/04_combinationSum/main.c
@@ -1,9 +1,9 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\08_Backtracking\04_combinationSum\main.c
+ * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\08_Backtracking\04_combinationSum\main.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-05 14:47:07
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-09 12:24:31
+ * @LastEditTime : 2023-12-28 00:43:25
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -34,7 +34,7 @@
  */
 int **combinationSum(int *candidates, int candidatesSize, int target, int *returnSize, int **returnColumnSizes){
 
-    return NULL
+    return NULL;
 }
 
 /**********************************************************************************/
