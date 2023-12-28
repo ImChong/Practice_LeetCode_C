21872e2c333e8f6a5a5a6bd55c24b7674a15a699
Chong Liu <liuc12@miamioh.edu>
2023-12-29 00:47:55 +0800

    update

---
+++ 01_Programming_Topics/02_Linked_List/02_myLinkedListCreate/src/dummyLinkedList.c
@@ -0,0 +1,11 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\02_Linked_List\00_common\01_dummyMethod\src\dummyLinkedList.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-12-29 00:21:33
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-12-29 00:21:50
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  :
+ */
