4283dceeabe025b89910edb0c5d33b633eb1df8b
Chong Liu <liuc12@miamioh.edu>
2023-12-28 14:54:47 +0800

    Create commonUnitTest.h

---
+++ 00_Basics/01_Common_Func/inc/commonUnitTest.h
@@ -0,0 +1,20 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\01_Array\02_removeElement\inc\unitTest.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-12-28 11:47:29
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-12-28 11:48:02
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  :
+ */
+#ifndef __UNIT_TEST_H
+#define __UNIT_TEST_H
+
+void test_1(void);
+void test_2(void);
+void test_3(void);
+void test_4(void);
+
+#endif  /* __UNIT_TEST_H */
