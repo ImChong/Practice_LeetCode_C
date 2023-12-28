583e5f964c1c6632a8164c4949c14cf60b5fefcd
Chong Liu <liuc12@miamioh.edu>
2023-12-29 00:19:43 +0800

    update

---
+++ 01_Programming_Topics/02_Linked_List/01_removeElements/inc/removeElements.h
@@ -0,0 +1,11 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\02_Linked_List\01_removeElements\inc\removeElements.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-12-29 00:19:25
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-12-29 00:19:30
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  :
+ */
