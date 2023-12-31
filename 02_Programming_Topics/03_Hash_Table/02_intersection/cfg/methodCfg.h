714daaead0173d0cfffb14988d1ca7c1f8d7be43
Chong Liu <liuc12@miamioh.edu>
2023-12-31 20:44:57 +0800

    Update methodCfg.h

--- 02_Programming_Topics/03_Hash_Table/02_intersection/cfg/methodCfg.h
+++ 02_Programming_Topics/03_Hash_Table/02_intersection/cfg/methodCfg.h
@@ -1,9 +1,9 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\03_Hash_Table\02_intersection\cfg\methodCfg.h
+ * @FilePath     : \Practice_LeetCode_C\02_Programming_Topics\03_Hash_Table\02_intersection\cfg\methodCfg.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-29 11:32:41
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-23 23:35:00
+ * @LastEditTime : 2023-12-31 20:44:43
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -17,7 +17,7 @@
 /*                                MACRO CONSTANTS                                 */
 /*                                                                                */
 /**********************************************************************************/
-#define HASH_TABLE_METHOD_EN        0                           /* 哈希表法求解  1: enable, 0: disable */
-#define DOUBLE_POINTER_METHOD_EN    !HASH_TABLE_METHOD_EN       /* 双指针法求解  1: enable, 0: disable */
+#define HASH_TABLE_METHOD_EN        1       /* 哈希表法求解  1: enable, 0: disable */
+#define DOUBLE_POINTER_METHOD_EN    0       /* 双指针法求解  1: enable, 0: disable */
 
 #endif  /* __METHOD_CFG_H */
