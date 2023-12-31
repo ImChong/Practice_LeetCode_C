9e85df6ca93072e0562b2339d5ca8ea0792f4d2a
Chong Liu <liuc12@miamioh.edu>
2023-12-31 20:43:31 +0800

    update

--- 02_Programming_Topics/03_Hash_Table/01_isAnagram/inc/isAnagram.h
+++ 02_Programming_Topics/03_Hash_Table/01_isAnagram/inc/isAnagram.h
@@ -1,11 +1,17 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\03_Hash_Table\01_isAnagram\inc\isAnagram.h
+ * @FilePath     : \Practice_LeetCode_C\02_Programming_Topics\03_Hash_Table\01_isAnagram\inc\isAnagram.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-29 00:55:26
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-29 00:55:27
+ * @LastEditTime : 2023-12-31 20:41:56
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : solution header file
  */
+#ifndef __IS_ANAGRAM_H
+#define __IS_ANAGRAM_H
+
+int isAnagram(char *s, char *t);
+
+#endif /* __IS_ANAGRAM_H */
