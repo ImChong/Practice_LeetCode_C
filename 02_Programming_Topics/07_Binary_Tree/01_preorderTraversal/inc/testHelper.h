2d104b2399616fba9d46685f1614798351f32855
Chong Liu <liuc12@miamioh.edu>
2024-01-01 01:09:28 +0800

    update

---
+++ 02_Programming_Topics/07_Binary_Tree/01_preorderTraversal/inc/testHelper.h
@@ -0,0 +1,17 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\02_Programming_Topics\07_Binary_Tree\01_preorderTraversal\inc\testHelper.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2024-01-01 01:05:25
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2024-01-01 01:06:08
+ * =================================================================================
+ * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : test helper header file
+ */
+#ifndef __TEST_HELPER_H
+#define __TEST_HELPER_H
+
+void test_preOrderTraversal(void);
+
+#endif  /* __TEST_HELPER_H */
