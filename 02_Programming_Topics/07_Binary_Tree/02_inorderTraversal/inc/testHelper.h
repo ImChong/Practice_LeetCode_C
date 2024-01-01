1868ecdc5de00d762e41d2108fdedf073140a4f0
Chong Liu <liuc12@miamioh.edu>
2024-01-02 02:05:30 +0800

    update

--- 02_Programming_Topics/07_Binary_Tree/02_inorderTraversal/inc/testHelper.h
+++ 02_Programming_Topics/07_Binary_Tree/02_inorderTraversal/inc/testHelper.h
@@ -3,9 +3,15 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-02 02:00:46
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-02 02:01:18
+ * @LastEditTime : 2024-01-02 02:03:20
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : test helper header file
  */
+#ifndef __TEST_HELPER_H
+#define __TEST_HELPER_H
+
+void test_inOrderTraversal(void);
+
+#endif  /* __TEST_HELPER_H */
--- 02_Programming_Topics/07_Binary_Tree/02_inorderTraversal/src/testHelper.c
+++ 02_Programming_Topics/07_Binary_Tree/02_inorderTraversal/src/testHelper.c
@@ -3,9 +3,61 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-02 02:00:54
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-02 02:01:32
+ * @LastEditTime : 2024-01-02 02:03:44
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : test helper source file
  */
+/* standard header file (标准头文件) */
+#include <stdio.h>
+/* common function header file (通用头文件) */
+#include "commonTypeDef.h"
+#include "commonArray.h"
+#include "commonBinaryTree.h"
+/* test helper header file (测试函数头文件) */
+#include "testHelper.h"
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                  TEST FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: 测试中序遍历
+ * [1, 2, 3, 4, 5, 6, 7]
+ *  树结构表示：
+ *       1
+ *      / \
+ *     2   3
+ *    / \ / \
+ *   4  5 6  7
+ *
+ * 中序遍历 - 递归方法/迭代方法
+ * [4, 2, 5, 1, 6, 3, 7]
+ * =================================================================================
+ * @return {void}
+ */
+void test_inOrderTraversal(void) {
+    /* 实际结果 */
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int arrSize = ARR_SIZE(arr);
+    struct TreeNode *root = arrayToTree(arr, arrSize);
+    int actualSize_recursion = 0;
+    int *actual_recursion = inOrderTraversal_recursion(root, &actualSize_recursion);
+    int actualSize_iteration = 0;
+    int *actual_iteration = inOrderTraversal_iteration(root, &actualSize_iteration);
+
+    /* 预期结果 */
+    int expected[] = {4, 2, 5, 1, 6, 3, 7};
+    int expectedSize = ARR_SIZE(expected);
+
+    /* 比较结果 */
+    printf("InorderTraversal Recursion Method.\n");
+    validateArray('1', expected, expectedSize, actual_recursion, actualSize_recursion, SORT_FALSE);
+    printf("InorderTraversal Iteration Method.\n");
+    validateArray('2', expected, expectedSize, actual_iteration, actualSize_iteration, SORT_FALSE);
+
+    /* 释放内存 */
+    freeTree(root);
+}
