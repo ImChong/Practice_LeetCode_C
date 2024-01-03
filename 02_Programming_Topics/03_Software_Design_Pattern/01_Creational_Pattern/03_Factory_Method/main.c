9d5fe1c38d47e85ad6406d11fef6073a1ba218c0
Chong Liu <liuc12@miamioh.edu>
2024-01-04 00:51:04 +0800

    update

--- 02_Programming_Topics/03_Software_Design_Pattern/01_Creational_Pattern/03_Factory_Method/main.c
+++ 02_Programming_Topics/03_Software_Design_Pattern/01_Creational_Pattern/03_Factory_Method/main.c
@@ -3,12 +3,14 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-04 00:26:57
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-04 00:35:37
+ * @LastEditTime : 2024-01-04 00:40:12
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : factory method main function 工厂方法模式主函数
  */
+/* standard head file 标准头文件 */
+#include <stdlib.h>
 /* factory method head file 工厂方法模式头文件 */
 #include "factoryMethod.h"
 
@@ -23,12 +25,12 @@
  * @return {void}
  */
 void test_1(void) {
-    /* 实际结果 */
+    Factory *factoryA = createFactoryA();
+    Product *productA = factoryA->createProduct(factoryA);
+    productA->use(productA);
 
-    /* 预期结果 */
-
-    /* 比较结果 */
-
+    free(productA);
+    free(factoryA);
 }
 
 /**
@@ -39,6 +41,6 @@ void test_1(void) {
  * @return {int}            程序运行状态
  */
 int main(int argc, const char *argv[]) {
-
+    test_1();
     return 0;
 }
--- 02_Programming_Topics/03_Software_Design_Pattern/01_Creational_Pattern/03_Factory_Method/src/factoryMethod.c
+++ 02_Programming_Topics/03_Software_Design_Pattern/01_Creational_Pattern/03_Factory_Method/src/factoryMethod.c
@@ -3,12 +3,68 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-04 00:27:22
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-04 00:34:31
+ * @LastEditTime : 2024-01-04 00:48:01
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : factory method source file 工厂方法模式源文件
  */
+/* standard head file 标准头文件 */
+#include <stdio.h>
+#include <stdlib.h>
 /* factory method head file 工厂方法模式头文件 */
 #include "factoryMethod.h"
 
+/**********************************************************************************/
+/*                                                                                */
+/*                                UTILITY FUNCTIONS                               */
+/*                                                                                */
+/**********************************************************************************/
+/* ============================================================================== */
+/* product functions */
+/* ============================================================================== */
+/**
+ * @description: 使用产品 A
+ * =================================================================================
+ * @param {Product} *product        产品
+ * @return {void}
+ */
+void useProductA(Product *product) {
+    printf("Using Product A\n");
+}
+
+/**
+ * @description: 创建产品 A
+ * =================================================================================
+ * @param {Factory} *factory        工厂
+ * @return {Product *} product      产品
+ */
+Product *createProductA(Factory *factory) {
+    ConcreteProductA *product = (ConcreteProductA *)malloc(sizeof(ConcreteProductA));
+    product->base.use = useProductA;
+    return (Product *)product;
+}
+
+/* ============================================================================== */
+/* factory functions */
+/* ============================================================================== */
+/**
+ * @description: 创建产品 A
+ * =================================================================================
+ * @param {Factory} *factory
+ * @return {*}
+ */
+Product *createConcreteProductA(Factory *factory) {
+    return createProductA(factory);
+}
+
+/**
+ * @description: 创建工厂 A
+ * =================================================================================
+ * @return {*}
+ */
+Factory *createFactoryA() {
+    ConcreteFactoryA *factory = (ConcreteFactoryA *)malloc(sizeof(ConcreteFactoryA));
+    factory->base.createProduct = createConcreteProductA;
+    return (Factory *)factory;
+}
