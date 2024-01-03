eef435e19f4579d66af22afc93cbc8ae1591f056
Chong Liu <liuc12@miamioh.edu>
2024-01-03 13:30:53 +0800

    update

--- 02_Programming_Topics/03_Software_Design_Pattern/01_Singleton/main.c
+++ 02_Programming_Topics/03_Software_Design_Pattern/01_Singleton/main.c
@@ -3,9 +3,66 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-03 13:07:30
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-03 13:07:33
+ * @LastEditTime : 2024-01-03 13:30:32
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : singleton main file 单例模式主文件
  */
+/* standard header file (标准头文件) */
+#include <stdio.h>
+/* singleton header file 单例模式头文件 */
+#include "singleton.h"
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                HELPER FUNCTIONS                                */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: demo function
+ * =================================================================================
+ * @return {void}
+ */
+void demoFunction() {
+    /* get singleton */
+    Singleton *s = getSingleton();
+    printf("Singleton value: %d\n", s->value);
+    /* change the value of singleton */
+    s->value = 42;
+}
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                  TEST FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: test 1 function
+ * =================================================================================
+ * @return {void}
+ */
+void test_1(void) {
+    demoFunction();
+    /* call demoFunction() again */
+    demoFunction();
+    /* free singleton */
+    freeSingleton();
+}
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                  MAIN FUNCTION                                 */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: main function 主函数
+ * =================================================================================
+ * @param {int} argc        程序入参个数
+ * @param {char} *argv[]    程序入参字符串数组
+ * @return {int}            程序运行状态
+ */
+int main(int argc, const char *argv[]) {
+    test_1();
+    return 0;
+}
--- 02_Programming_Topics/03_Software_Design_Pattern/01_Singleton/src/singleton.c
+++ 02_Programming_Topics/03_Software_Design_Pattern/01_Singleton/src/singleton.c
@@ -3,9 +3,52 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-03 13:08:20
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-03 13:08:21
+ * @LastEditTime : 2024-01-03 13:27:49
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : singleton source file 单例模式源文件
  */
+/* standard header file (标准头文件) */
+#include <stdio.h>
+#include <stdlib.h>
+/* singleton header file 单例模式头文件 */
+#include "singleton.h"
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                 GLOBAL VARIABLES                               */
+/*                                                                                */
+/**********************************************************************************/
+static Singleton *g_instance = NULL;    /* singleton instance 单例模式实例 */
+
+/**********************************************************************************/
+/*                                                                                */
+/*                                UTILITY FUNCTIONS                               */
+/*                                                                                */
+/**********************************************************************************/
+/**
+ * @description: get singleton
+ * =================================================================================
+ * @return {Singleton *}
+ */
+Singleton *getSingleton(void) {
+    if (g_instance == NULL) {
+        g_instance = (Singleton *)malloc(sizeof(Singleton));
+        /* initialize the value of singleton */
+        g_instance->value = 0;
+    }
+    return g_instance;
+}
+
+/**
+ * @description: free singleton
+ * =================================================================================
+ * @return {void}
+ */
+void freeSingleton(void) {
+    if (g_instance != NULL) {
+        free(g_instance);
+        g_instance = NULL;
+    }
+}
