5267232f25d59059cf7462c5c0f5355fb473e4af
Chong Liu <liuc12@miamioh.edu>
2024-01-04 00:28:04 +0800

    update

---
+++ 02_Programming_Topics/03_Software_Design_Pattern/01_Creational_Pattern/03_Factory_Method/inc/factoryMethod.h
@@ -0,0 +1,17 @@
+/*
+ * @FilePath     : \Practice_LeetCode_C\02_Programming_Topics\03_Software_Design_Pattern\01_Creational_Pattern\03_Factory_Method\inc\factoryMethod.h
+ * @Author       : Chong Liu
+ * @CreateDate   : 2024-01-04 00:27:35
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2024-01-04 00:27:57
+ * =================================================================================
+ * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  :
+ */
+#ifndef __FACTORY_METHOD_H
+#define __FACTORY_METHOD_H
+
+
+
+#endif  /* __FACTORY_METHOD_H */
