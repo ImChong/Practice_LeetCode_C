370d2c7163bd47fbe8754b68d110a98bbc660a26
Chong Liu <liuc12@miamioh.edu>
2024-01-06 11:53:00 +0800

    Update runGoogleStyleCheck.sh

--- .github/workflows/testRunner/runGoogleStyleCheck.sh
+++ .github/workflows/testRunner/runGoogleStyleCheck.sh
@@ -4,16 +4,29 @@ echo "Running clang-format..."
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# run cpplint on all .c and .h files in the target directory
+# run clang-format on all .c and .h files in the target directory
 TARGET_DIR="./01_Common_Functions"
 
-# 查找所有的 C++ 文件并运行 clang-format
-find $TARGET_DIR -regex '.*\.\(cpp\|hpp\|cc\|h\)' -exec clang-format -style=google -output-replacements-xml {} \; | grep "<replacement " > /dev/null
+# Initialize a counter for the number of issues found
+ISSUE_COUNT=0
 
-# 检查上一个命令的返回值
-if [ $? -ne 1 ]; then
+# Process each file and check for style issues
+for file in $(find $TARGET_DIR -regex '.*\.\(cpp\|hpp\|cc\|h\)'); do
+    # Run clang-format and capture any needed replacements
+    OUTPUT=$(clang-format -style=google -output-replacements-xml $file)
+    if echo "$OUTPUT" | grep -q "<replacement "; then
+        # If replacements are found, print the file name and issue details
+        echo "Code style issue in file: $file"
+        echo "$OUTPUT"
+        # Increment the issue count
+        ISSUE_COUNT=$((ISSUE_COUNT+1))
+    fi
+done
+
+# Check if any issues were found
+if [ $ISSUE_COUNT -ne 0 ]; then
     echo "=============================="
-    echo "Code style issues found"
+    echo "Code style issues found: $ISSUE_COUNT"
     exit 1
 fi
 
