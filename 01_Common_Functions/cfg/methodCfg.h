0c0fbe3ecf6aa75b0cb28f43e99da30a73970d6b
Chong Liu <liuc12@miamioh.edu>
2024-01-08 02:29:55 +0800

    Update methodCfg.h

--- 01_Common_Functions/cfg/methodCfg.h
+++ 01_Common_Functions/cfg/methodCfg.h
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-08 00:52:30
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-08 02:22:47
+ * @LastEditTime : 2024-01-08 02:29:42
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -14,6 +14,15 @@
 #ifndef __METHOD_CFG_H /* NOLINT(build/header_guard) */
 #define __METHOD_CFG_H /* NOLINT(build/header_guard) */
 
+/* common function configuration file (通用函数配置文件) */
+#include "compileCfg.h"
 
+/* solution method configuration for common binary tree module (通用二叉树模块解题方法配置) */
+#if COMMON_BINARY_TREE_EN
+#define BTREE_ORDER_TRAVERSAL_RECURSION_METHOD_EN 1 /* 二叉树递归遍历方法编译使能  1: enable, 0: disable */
+#define BTREE_ORDER_TRAVERSAL_ITERATION_METHOD_EN 0 /* 二叉树迭代遍历方法编译使能  1: enable, 0: disable */
+#define BTREE_LEVEL_ORDER_ARRAY_METHOD_EN 1         /* 二叉树层序遍历方法编译使能  1: enable, 0: disable */
+#define BTREE_LEVEL_ORDER_STRUCT_METHOD_EN 0        /* 二叉树层序遍历方法编译使能  1: enable, 0: disable */
+#endif /* COMMON_BINARY_TREE_EN */
 
 #endif  /* __METHOD_CFG_H */ /* NOLINT(build/header_guard) */
