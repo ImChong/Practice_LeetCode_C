7ab155cbf37c0fdece4c03e0ef10b9661fa8dd2d
Chong Liu <liuc12@miamioh.edu>
2024-01-08 01:48:30 +0800

    Update methodCfg.h

--- 01_Common_Functions/cfg/methodCfg.h
+++ 01_Common_Functions/cfg/methodCfg.h
@@ -3,11 +3,13 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-08 00:52:30
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-08 01:05:33
+ * @LastEditTime : 2024-01-08 01:48:25
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : solution method configuration file (解题方法配置文件)
+ *    This file is intended to left empty.
+ *    The solution method configuration is set in the CMakelists.txt file instead.
  */
 #ifndef __METHOD_CFG_H /* NOLINT(build/header_guard) */
 #define __METHOD_CFG_H /* NOLINT(build/header_guard) */
