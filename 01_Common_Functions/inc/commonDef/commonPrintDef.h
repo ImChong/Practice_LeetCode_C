3cae539380313f99de4594d78bb4302332b21cfc
Chong Liu <liuc12@miamioh.edu>
2024-01-08 21:23:38 +0800

    Update commonPrintDef.h

--- 01_Common_Functions/inc/commonDef/commonPrintDef.h
+++ 01_Common_Functions/inc/commonDef/commonPrintDef.h
@@ -1,16 +1,16 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\inc\commonDef\commonPrintDef.h
+ * @FilePath     : \Practice_LeetCode_C\01_Common_Functions\inc\commonDef\commonPrintDef.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-22 12:55:48
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-03 10:06:35
+ * @LastEditTime : 2024-01-08 21:23:08
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  : 通用打印定义头文件
+ * @Description  : Common print definition header file
  */
-#ifndef __COMMON_PRINT_DEF_H    /* NOLINT(build/header_guard) */
-#define __COMMON_PRINT_DEF_H    /* NOLINT(build/header_guard) */
+#ifndef __COMMON_PRINT_DEF_H /* NOLINT(build/header_guard) */
+#define __COMMON_PRINT_DEF_H /* NOLINT(build/header_guard) */
 
 /* standard header file (标准头文件) */
 #include <stdio.h>
@@ -21,9 +21,9 @@
 /*                                                                                */
 /**********************************************************************************/
 /* （token-pasting：##）将两个标记符号合并为一个标记 */
-#define TOKEN_CONCATENATE(x, y) x ## y
+#define TOKEN_CONCATENATE(x, y) x##y
 
 /* 重命名打印函数: 打印时可省略输入\r\n */
 #define INFO(format, ...) printf(format "\r\n", ##__VA_ARGS__)
 
-#endif  /* __COMMON_PRINT_DEF_H */  /* NOLINT(build/header_guard) */
+#endif /* __COMMON_PRINT_DEF_H */ /* NOLINT(build/header_guard) */
