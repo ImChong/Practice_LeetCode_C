d098a515c25120dc29f8c9acea3665dee184a09d
Chong Liu <liuc12@miamioh.edu>
2024-01-09 01:26:23 +0800

    Update commonLinkedListDummy.h

--- 01_Common_Functions/inc/commonLinkedListDummy.h
+++ 01_Common_Functions/inc/commonLinkedListDummy.h
@@ -9,8 +9,8 @@
  * =================================================================================
  * @Description  : Common dummy linked list header file
  */
-#ifndef __COMMON_LINKED_LIST_DUMMY_H    /* NOLINT(build/header_guard) */
-#define __COMMON_LINKED_LIST_DUMMY_H    /* NOLINT(build/header_guard) */
+#ifndef __COMMON_LINKED_LIST_DUMMY_H /* NOLINT(build/header_guard) */
+#define __COMMON_LINKED_LIST_DUMMY_H /* NOLINT(build/header_guard) */
 
 /**********************************************************************************/
 /*                                                                                */
@@ -18,13 +18,13 @@
 /*                                                                                */
 /**********************************************************************************/
 struct ListNode {
-    int val;                    /* 节点数值 */
-    struct ListNode *next;      /* 下一节点地址 */
+    int val;               /* 节点数值 */
+    struct ListNode *next; /* 下一节点地址 */
 };
 
 typedef struct {
-    struct ListNode *head;      /* 链表虚拟头节点 */
-    int size;                   /* 链表长度 */
+    struct ListNode *head; /* 链表虚拟头节点 */
+    int size;              /* 链表长度 */
 } DummyLinkedList;
 
 /**********************************************************************************/
@@ -32,34 +32,31 @@ typedef struct {
 /*                                UTILITY FUNCTIONS                               */
 /*                                                                                */
 /**********************************************************************************/
-DummyLinkedList *DLinkedList_Init(void);     /* 创建使用虚拟头节点的链表 */
-int DLinkedList_GetAtIndex(DummyLinkedList *dummyHead, int index);        /* 获取链表中第index个节点的值 */
-void DLinkedList_AddAtHead(DummyLinkedList *dummyHead, int val);       /* 在链表头部添加一个节点 */
-void DLinkedList_AddAtTail(DummyLinkedList *dummyHead, int val);       /* 在链表尾部添加一个节点 */
-void DLinkedList_AddAtIndex(DummyLinkedList *dummyHead,
-                               int index,
-                               int val);               /* 在链表中第index个节点之前添加一个值为val的节点 */
-void DLinkedList_DeleteAtIndex(DummyLinkedList *dummyHead, int index);     /* 删除链表中第index个节点 */
-void DLinkedList_DeleteValue(DummyLinkedList *dummyHead, int val);       /* 从链表中删除值为val的所有节点 */
-int *DLinkedList_GetIndexes(DummyLinkedList *dummyHead,
-                               int val,
-                               int *returnSize);       /* 获取链表中值为val的所有节点的索引 */
+DummyLinkedList *DLinkedList_Init(void);                           /* 创建使用虚拟头节点的链表 */
+int DLinkedList_GetAtIndex(DummyLinkedList *dummyHead, int index); /* 获取链表中第index个节点的值 */
+void DLinkedList_AddAtHead(DummyLinkedList *dummyHead, int val);   /* 在链表头部添加一个节点 */
+void DLinkedList_AddAtTail(DummyLinkedList *dummyHead, int val);   /* 在链表尾部添加一个节点 */
+void DLinkedList_AddAtIndex(DummyLinkedList *dummyHead, int index,
+                            int val); /* 在链表中第index个节点之前添加一个值为val的节点 */
+void DLinkedList_DeleteAtIndex(DummyLinkedList *dummyHead, int index); /* 删除链表中第index个节点 */
+void DLinkedList_DeleteValue(DummyLinkedList *dummyHead, int val);     /* 从链表中删除值为val的所有节点 */
+int *DLinkedList_GetIndexes(DummyLinkedList *dummyHead, int val,
+                            int *returnSize); /* 获取链表中值为val的所有节点的索引 */
 
 /**********************************************************************************/
 /*                                                                                */
 /*                                HELPER FUNCTIONS                                */
 /*                                                                                */
 /**********************************************************************************/
-void DLinkedList_ArrayInit(DummyLinkedList *dummyHead, int *nums, int numsSize);     /* 将数组转换为链表 */
-void DLinkedList_Print(DummyLinkedList *dummyHead);      /* 打印链表 */
-void DLinkedList_Free(DummyLinkedList *dummyHead);     /* 释放链表 */
+void DLinkedList_ArrayInit(DummyLinkedList *dummyHead, int *nums, int numsSize); /* 将数组转换为链表 */
+void DLinkedList_Print(DummyLinkedList *dummyHead);                              /* 打印链表 */
+void DLinkedList_Free(DummyLinkedList *dummyHead);                               /* 释放链表 */
 
 /**********************************************************************************/
 /*                                                                                */
 /*                                  TEST FUNCTION                                 */
 /*                                                                                */
 /**********************************************************************************/
-void Validate_DLinkedList(char testNum, DummyLinkedList *expect, DummyLinkedList *actual);        /* 验证链表 */
+void Validate_DLinkedList(char testNum, DummyLinkedList *expect, DummyLinkedList *actual); /* 验证链表 */
 
-
-#endif  /* __COMMON_LINKED_LIST_DUMMY_H */   /* NOLINT(build/header_guard) */
+#endif /* __COMMON_LINKED_LIST_DUMMY_H */ /* NOLINT(build/header_guard) */
