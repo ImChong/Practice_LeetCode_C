089ca971114e5e9b81db45447bf8812580f274ff
Chong Liu <liuc12@miamioh.edu>
2024-01-08 21:38:30 +0800

    Update commonHashTable.h

--- 01_Common_Functions/inc/commonHashTable.h
+++ 01_Common_Functions/inc/commonHashTable.h
@@ -9,8 +9,8 @@
  * =================================================================================
  * @Description  : Common hash table header file
  */
-#ifndef __COMMON_HASH_TABLE_H       /* NOLINT(build/header_guard) */
-#define __COMMON_HASH_TABLE_H       /* NOLINT(build/header_guard) */
+#ifndef __COMMON_HASH_TABLE_H /* NOLINT(build/header_guard) */
+#define __COMMON_HASH_TABLE_H /* NOLINT(build/header_guard) */
 
 /**********************************************************************************/
 /*                                                                                */
@@ -19,16 +19,16 @@
 /**********************************************************************************/
 /* 哈希节点 */
 struct HashNode {
-    int val;                    /* 元素值 */
-    int index;                  /* 元素索引（如果不需要索引，此项可以设置为 -1） */
-    int counter;                /* 元素个数（如果需要索引，此项则默认为 1） */
-    struct HashNode *next;      /* 下一个节点 */
+    int val;               /* 元素值 */
+    int index;             /* 元素索引（如果不需要索引，此项可以设置为 -1） */
+    int counter;           /* 元素个数（如果需要索引，此项则默认为 1） */
+    struct HashNode *next; /* 下一个节点 */
 };
 
 /* 哈希表 */
 struct HashTable {
-    int size;                       /* 哈希表大小 */
-    struct HashNode **hashSlots;    /* 哈希槽位 */
+    int size;                    /* 哈希表大小 */
+    struct HashNode **hashSlots; /* 哈希槽位 */
 };
 
 /**********************************************************************************/
@@ -36,18 +36,17 @@ struct HashTable {
 /*                                UTILITY FUNCTIONS                               */
 /*                                                                                */
 /**********************************************************************************/
-struct HashTable *HashTable_Init(int size);                            /* 创建哈希表 */
-void HashTable_Insert(struct HashTable *table, int val, int index);      /* 插入哈希节点 */
-int HashTable_Search(struct HashTable *table, int val);                  /* 查找哈希表（返回元素个数） */
-void HashTable_Remove(struct HashTable *table, int val);                 /* 删除哈希节点 */
+struct HashTable *HashTable_Init(int size);                         /* 创建哈希表 */
+void HashTable_Insert(struct HashTable *table, int val, int index); /* 插入哈希节点 */
+int HashTable_Search(struct HashTable *table, int val);             /* 查找哈希表（返回元素个数） */
+void HashTable_Remove(struct HashTable *table, int val);            /* 删除哈希节点 */
 
 /**********************************************************************************/
 /*                                                                                */
 /*                                HELPER FUNCTIONS                                */
 /*                                                                                */
 /**********************************************************************************/
-void HashTable_Print(struct HashTable *table);                           /* 打印哈希表 */
-void HashTable_Free(struct HashTable *table);                            /* 释放哈希表 */
+void HashTable_Print(struct HashTable *table); /* 打印哈希表 */
+void HashTable_Free(struct HashTable *table);  /* 释放哈希表 */
 
-
-#endif  /* __COMMON_HASH_TABLE_H */     /* NOLINT(build/header_guard) */
+#endif /* __COMMON_HASH_TABLE_H */ /* NOLINT(build/header_guard) */
