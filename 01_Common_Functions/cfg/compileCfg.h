5c59cfa9eab651936b6814e8f6744c941bfc72ff
Chong Liu <liuc12@miamioh.edu>
2024-01-09 17:16:23 +0800

    update

--- 01_Common_Functions/cfg/compileCfg.h
+++ 01_Common_Functions/cfg/compileCfg.h
@@ -17,24 +17,24 @@
 /*                                MACRO CONSTANTS                                 */
 /*                                                                                */
 /**********************************************************************************/
-#define COMMON_ARRAY_EN 1             /* 通用数组编译使能  1: enable, 0: disable */
-#define COMMON_BINARY_TREE_EN 1       /* 通用二叉树编译使能  1: enable, 0: disable */
-#define COMMON_CMD_EN 1               /* 通用命令行编译使能  1: enable, 0: disable */
-#define COMMON_HASH_TABLE_EN 1        /* 通用哈希表编译使能  1: enable, 0: disable */
-#define COMMON_LINKED_LIST_EN 1       /* 通用链表编译使能  1: enable, 0: disable */
+#define COMMON_ARRAY_EN             1 /* 通用数组编译使能  1: enable, 0: disable */
+#define COMMON_BINARY_TREE_EN       1 /* 通用二叉树编译使能  1: enable, 0: disable */
+#define COMMON_CMD_EN               1 /* 通用命令行编译使能  1: enable, 0: disable */
+#define COMMON_HASH_TABLE_EN        1 /* 通用哈希表编译使能  1: enable, 0: disable */
+#define COMMON_LINKED_LIST_EN       1 /* 通用链表编译使能  1: enable, 0: disable */
 #define COMMON_LINKED_LIST_DUMMY_EN 1 /* 通用虚拟头节点链表编译使能  1: enable, 0: disable */
-#define COMMON_QUEUE_EN 1             /* 通用队列编译使能  1: enable, 0: disable */
-#define COMMON_RING_BUFFER_EN 1       /* 通用环形缓冲区编译使能  1: enable, 0: disable */
-#define COMMON_SINGLE_VALUE_EN 1      /* 通用单值函数编译使能  1: enable, 0: disable */
-#define COMMON_STACK_EN 1             /* 通用栈编译使能  1: enable, 0: disable */
-#define COMMON_STRING_EN 1            /* 通用字符串编译使能  1: enable, 0: disable */
+#define COMMON_QUEUE_EN             1 /* 通用队列编译使能  1: enable, 0: disable */
+#define COMMON_RING_BUFFER_EN       1 /* 通用环形缓冲区编译使能  1: enable, 0: disable */
+#define COMMON_SINGLE_VALUE_EN      1 /* 通用单值函数编译使能  1: enable, 0: disable */
+#define COMMON_STACK_EN             1 /* 通用栈编译使能  1: enable, 0: disable */
+#define COMMON_STRING_EN            1 /* 通用字符串编译使能  1: enable, 0: disable */
 
 /* solution method configuration for common binary tree module (通用二叉树模块解题方法配置) */
 #if COMMON_BINARY_TREE_EN
 #define BTREE_ORDER_TRAVERSAL_RECURSION_METHOD_EN 1 /* 二叉树递归遍历方法编译使能  1: enable, 0: disable */
 #define BTREE_ORDER_TRAVERSAL_ITERATION_METHOD_EN 0 /* 二叉树迭代遍历方法编译使能  1: enable, 0: disable */
-#define BTREE_LEVEL_ORDER_ARRAY_METHOD_EN 1         /* 二叉树层序遍历方法编译使能  1: enable, 0: disable */
-#define BTREE_LEVEL_ORDER_STRUCT_METHOD_EN 0        /* 二叉树层序遍历方法编译使能  1: enable, 0: disable */
-#endif /* COMMON_BINARY_TREE_EN */
+#define BTREE_LEVEL_ORDER_ARRAY_METHOD_EN         1 /* 二叉树层序遍历方法编译使能  1: enable, 0: disable */
+#define BTREE_LEVEL_ORDER_STRUCT_METHOD_EN        0 /* 二叉树层序遍历方法编译使能  1: enable, 0: disable */
+#endif                                              /* COMMON_BINARY_TREE_EN */
 
 #endif /* __COMPILE_CFG_H */ /* NOLINT(build/header_guard) */
--- 01_Common_Functions/test/commonArrayTest.cc
+++ 01_Common_Functions/test/commonArrayTest.cc
@@ -1,9 +1,9 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\00_Basics\01_Common_Func\test\commonArrayTest.cc
+ * @FilePath     : \Practice_LeetCode_C\01_Common_Functions\test\commonArrayTest.cc
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-01 02:17:51
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-15 23:15:25
+ * @LastEditTime : 2024-01-09 17:11:28
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
