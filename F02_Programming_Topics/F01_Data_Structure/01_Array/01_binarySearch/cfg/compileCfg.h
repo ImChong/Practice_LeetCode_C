ebfa41e5423ab2140bc4eea1d6c22dce47d53be0
Chong Liu <liuc12@miamioh.edu>
2024-01-11 18:43:31 +0800

    Update compileCfg.h

--- F02_Programming_Topics/F01_Data_Structure/01_Array/01_binarySearch/cfg/compileCfg.h
+++ F02_Programming_Topics/F01_Data_Structure/01_Array/01_binarySearch/cfg/compileCfg.h
@@ -1,9 +1,9 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\02_Programming_Topics\01_Data_Structure\01_Array\01_binarySearch\cfg\compileCfg.h
+ * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\01_Array\01_binarySearch\cfg\compileCfg.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-11-23 01:23:40
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-09 16:56:41
+ * @LastEditTime : 2024-01-11 18:43:23
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -29,5 +29,13 @@
 #define COMMON_SINGLE_VALUE_EN 1      /* 通用单值函数编译使能  1: enable, 0: disable */
 #define COMMON_STACK_EN 0             /* 通用栈编译使能  1: enable, 0: disable */
 #define COMMON_STRING_EN 0            /* 通用字符串编译使能  1: enable, 0: disable */
+
+/* solution method configuration for common binary tree module (通用二叉树模块解题方法配置) */
+#if COMMON_BINARY_TREE_EN
+    #define BTREE_ORDER_TRAVERSAL_RECURSION_METHOD_EN 1 /* 二叉树递归遍历方法编译使能  1: enable, 0: disable */
+    #define BTREE_ORDER_TRAVERSAL_ITERATION_METHOD_EN 0 /* 二叉树迭代遍历方法编译使能  1: enable, 0: disable */
+    #define BTREE_LEVEL_ORDER_ARRAY_METHOD_EN 1         /* 二叉树层序遍历方法编译使能  1: enable, 0: disable */
+    #define BTREE_LEVEL_ORDER_STRUCT_METHOD_EN 0        /* 二叉树层序遍历方法编译使能  1: enable, 0: disable */
+#endif /* COMMON_BINARY_TREE_EN */
 
 #endif /* __COMPILE_CFG_H */
