ae45b5facdbaff5b6c58c02b3d70017301acaecb
Chong Liu <liuc12@miamioh.edu>
2024-01-15 02:27:12 +0800

    Update main.c

--- F02_Programming_Topics/F01_Data_Structure/01_Array/05_generateMatrix/main.c
+++ F02_Programming_Topics/F01_Data_Structure/01_Array/05_generateMatrix/main.c
@@ -8,9 +8,7 @@
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : leetcode 59.螺旋矩阵II：https://leetcode.cn/problems/spiral-matrix-ii/
- * https://programmercarl.com/0059.%E8%9E%BA%E6%97%8B%E7%9F%A9%E9%98%B5II.html
- *
- * 给一个正整数 n ，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ *      https://programmercarl.com/0059.%E8%9E%BA%E6%97%8B%E7%9F%A9%E9%98%B5II.html
  */
 /* solution header file (题解头文件) */
 #include "generateMatrix.h"
