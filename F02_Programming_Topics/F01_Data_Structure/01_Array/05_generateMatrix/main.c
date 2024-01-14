0c94f203b9435f77a8e205bb88c6a59fa056c6ff
Chong Liu <liuc12@miamioh.edu>
2024-01-15 02:20:38 +0800

    Update main.c

--- F02_Programming_Topics/F01_Data_Structure/01_Array/05_generateMatrix/main.c
+++ F02_Programming_Topics/F01_Data_Structure/01_Array/05_generateMatrix/main.c
@@ -1,18 +1,19 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\01_Array\05_generateMatrix\main.c
+ * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\01_Array\05_generateMatrix\main.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-16 08:57:10
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-29 15:11:02
+ * @LastEditTime : 2024-01-15 02:20:04
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  : 59.螺旋矩阵II：https://leetcode.cn/problems/spiral-matrix-ii/
+ * @Description  : leetcode 59.螺旋矩阵II：https://leetcode.cn/problems/spiral-matrix-ii/
  * https://programmercarl.com/0059.%E8%9E%BA%E6%97%8B%E7%9F%A9%E9%98%B5II.html
  *
  * 给一个正整数 n ，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
- *
  */
+/* solution header file (题解头文件) */
+#include "generateMatrix.h"
 /* Standard header file (标准头文件) */
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,8 +22,6 @@
 #include "commonArray.h"
 /* solution method configuration file (解题方法配置文件) */
 #include "methodCfg.h"
-/* solution header file (题解头文件) */
-#include "generateMatrix.h"
 
 /**********************************************************************************/
 /*                                                                                */
@@ -30,9 +29,9 @@
 /*                                                                                */
 /**********************************************************************************/
 /**
- * @description: 测试 1
- * 输入：n = 3
- * 输出：[[1,2,3],[8,9,4],[7,6,5]]
+ * @description: Test 1
+ * Input: n = 3
+ * Output: [[1,2,3],[8,9,4],[7,6,5]]
  * =================================================================================
  * @return {void}
  */
@@ -64,9 +63,9 @@ void Test1(void) {
 }
 
 /**
- * @description: 测试 2
- * 输入：n = 1
- * 输出：[[1]]
+ * @description: Test 2
+ * Input: n = 1
+ * Output: [[1]]
  * =================================================================================
  * @return {void}
  */
@@ -96,9 +95,9 @@ void Test2(void) {
 }
 
 /**
- * @description: 测试 3
- * 输入：n = 4
- * 输出：[[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]
+ * @description: Test 3
+ * Input: n = 4
+ * Output: [[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]
  * =================================================================================
  * @return {void}
  */
