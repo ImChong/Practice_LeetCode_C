f9bc10a38d6ace6ee1610d49ea07d24bc84bc0a9
Chong Liu <liuc12@miamioh.edu>
2024-01-15 10:48:11 +0800

    Update commonPrintDef.h

--- F01_Common_Functions/inc/commonDef/commonPrintDef.h
+++ F01_Common_Functions/inc/commonDef/commonPrintDef.h
@@ -1,9 +1,9 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Common_Functions\inc\commonDef\commonPrintDef.h
+ * @FilePath     : \Practice_LeetCode_C\F01_Common_Functions\inc\commonDef\commonPrintDef.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-22 12:55:48
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-08 21:23:08
+ * @LastEditTime : 2024-01-15 10:48:05
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -20,10 +20,10 @@
 /*                                MACRO FUNCTIONS                                 */
 /*                                                                                */
 /**********************************************************************************/
-/* （token-pasting：##）将两个标记符号合并为一个标记 */
+/* （token-pasting：##）concatenate two tokens (将两个标记连接起来) */
 #define TOKEN_CONCATENATE(x, y) x##y
 
-/* 重命名打印函数: 打印时可省略输入\r\n */
+/* Rename printf function to ignore the \r\n at the end of the string (重命名printf函数，忽略字符串末尾的\r\n) */
 #define INFO(format, ...) printf(format "\r\n", ##__VA_ARGS__)
 
 #endif /* F01_COMMON_FUNCTIONS_INC_COMMONDEF_COMMONPRINTDEF_H_ */
