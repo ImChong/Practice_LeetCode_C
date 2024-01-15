776581ae44b05f0ddc947f9ea60f2f8999fb0087
Chong Liu <liuc12@miamioh.edu>
2024-01-15 11:14:51 +0800

    Update commonBinaryTree.h

--- F01_Common_Functions/inc/commonBinaryTree.h
+++ F01_Common_Functions/inc/commonBinaryTree.h
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-08 09:53:52
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-15 11:01:01
+ * @LastEditTime : 2024-01-15 11:14:30
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -37,58 +37,60 @@ void BTree_Free(struct TreeNode *root);                     /* Free tree */
 /*                               STRUCT DEFINITION                                */
 /*                                                                                */
 /**********************************************************************************/
-/* Tree Array Queue Structure */
+/* Tree array queue structure */
 typedef struct {
-    struct TreeNode **array; /* 树节点数组 */
-    int head;                /* 队首指针 */
-    int tail;                /* 队尾指针 */
-    int size;                /* 队列大小 */
+    struct TreeNode **array; /* Tree node queue array */
+    int head;                /* Queue head index */
+    int tail;                /* Queue tail index */
+    int size;                /* Queue size */
 } TreeArrQueue;
 
-/* 列表树队列操作 */
-TreeArrQueue *BTree_InitArrQueue(int queueSize);                   /* 创建队列 */
-void BTree_EnArrQueue(TreeArrQueue *queue, struct TreeNode *node); /* 将节点加入队列 */
-struct TreeNode *BTree_DeArrQueue(TreeArrQueue *queue);            /* 从队列中取出一个节点 */
-int BTree_ArrQueueIsEmpty(TreeArrQueue *queue);                    /* 判断队列是否为空 */
-int BTree_ArrQueueIsFull(TreeArrQueue *queue);                     /* 判断队列是否为满 */
-void BTree_ArrQueuePrint(TreeArrQueue *queue);                     /* 打印队列 */
-void BTree_ArrQueueFree(TreeArrQueue *queue);                      /* 释放队列 */
+/* Tree array queue operations */
+TreeArrQueue *BTree_InitArrQueue(int queueSize);                   /* Initialize queue */
+void BTree_EnArrQueue(TreeArrQueue *queue, struct TreeNode *node); /* Enqueue a node */
+struct TreeNode *BTree_DeArrQueue(TreeArrQueue *queue);            /* Dequeue a node */
+int BTree_ArrQueueIsEmpty(TreeArrQueue *queue);                    /* Check if the queue is empty */
+int BTree_ArrQueueIsFull(TreeArrQueue *queue);                     /* Check if the queue is full */
+void BTree_ArrQueuePrint(TreeArrQueue *queue);                     /* Print queue */
+void BTree_ArrQueueFree(TreeArrQueue *queue);                      /* Free queue */
 
 /**********************************************************************************/
 /*                                                                                */
 /*                               STRUCT DEFINITION                                */
 /*                                                                                */
 /**********************************************************************************/
-/* 树链表节点 */
+/* Tree linked list queue node structure */
 struct TreeListNode {
-    struct TreeNode *node;     /* 树节点指针 */
-    struct TreeListNode *next; /* 指向下一元素的指针 */
+    struct TreeNode *node;     /* Tree node */
+    struct TreeListNode *next; /* Pointer to next tree linked list queue node */
 };
 
-/* 树链表队列头 */
+/* Tree linked list queue structure */
 typedef struct {
-    struct TreeListNode *head; /* 链表头指针 */
-    int size;                  /* 链表大小 */
+    struct TreeListNode *head; /* Queue head */
+    int size;                  /* Queue size */
 } TreeListQueue;
 
-/* 链表树队列操作 */
-TreeListQueue *BTree_InitListQueue(void);                            /* 创建队列 */
-void BTree_EnListQueue(TreeListQueue *queue, struct TreeNode *node); /* 将节点加入队列 */
-struct TreeNode *BTree_DeListQueue(TreeListQueue *queue);            /* 从队列中取出一个节点 */
-int BTree_ListQueueIsEmpty(TreeListQueue *queue);                    /* 判断队列是否为空 */
-int BTree_ListQueueIsFull(TreeListQueue *queue);                     /* 判断队列是否为满 */
-void BTree_ListQueuePrint(TreeListQueue *queue);                     /* 打印队列 */
-void BTree_ListQueueFree(TreeListQueue *queue);                      /* 释放队列 */
+/* Tree linked list queue operations */
+TreeListQueue *BTree_InitListQueue(void);                            /* Initialize queue */
+void BTree_EnListQueue(TreeListQueue *queue, struct TreeNode *node); /* Enqueue a node */
+struct TreeNode *BTree_DeListQueue(TreeListQueue *queue);            /* Dequeue a node */
+int BTree_ListQueueIsEmpty(TreeListQueue *queue);                    /* Check if the queue is empty */
+int BTree_ListQueueIsFull(TreeListQueue *queue);                     /* Check if the queue is full */
+void BTree_ListQueuePrint(TreeListQueue *queue);                     /* Print queue */
+void BTree_ListQueueFree(TreeListQueue *queue);                      /* Free queue */
 
 /**********************************************************************************/
 /*                                                                                */
 /*                                UTILITY FUNCTIONS                               */
 /*                                                                                */
 /**********************************************************************************/
-int *BTree_PreOrderTraversal(struct TreeNode *root, int *returnSize);                          /* 前序遍历 */
-int *BTree_InOrderTraversal(struct TreeNode *root, int *returnSize);                           /* 中序遍历 */
-int *BTree_PostOrderTraversal(struct TreeNode *root, int *returnSize);                         /* 后序遍历 */
-int **BTree_LevelOrder(struct TreeNode *root, int *returnSize, int **returnColumnSizes);       /* 层序遍历 */
-int **BTree_LevelOrderBottom(struct TreeNode *root, int *returnSize, int **returnColumnSizes); /* 自底向上层序遍历 */
+int *BTree_PreOrderTraversal(struct TreeNode *root, int *returnSize);
+int *BTree_InOrderTraversal(struct TreeNode *root, int *returnSize);
+int *BTree_PostOrderTraversal(struct TreeNode *root, int *returnSize);
+/* Level order traversal from top to bottom */
+int **BTree_LevelOrder(struct TreeNode *root, int *returnSize, int **returnColumnSizes);
+/* Level order traversal from bottom to top */
+int **BTree_LevelOrderBottom(struct TreeNode *root, int *returnSize, int **returnColumnSizes);
 
 #endif /* F01_COMMON_FUNCTIONS_INC_COMMONBINARYTREE_H_ */
