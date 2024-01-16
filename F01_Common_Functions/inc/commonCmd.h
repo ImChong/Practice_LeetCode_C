119f2688585a6475393c9cf1d4999db1977ade13
Chong Liu <liuc12@miamioh.edu>
2024-01-16 22:55:44 +0800

    update

--- F01_Common_Functions/inc/commonCmd.h
+++ F01_Common_Functions/inc/commonCmd.h
@@ -1,5 +1,5 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Common_Functions\inc\commonCmd.h
+ * @FilePath     : \Practice_LeetCode_C\F01_Common_Functions\inc\commonCmd.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-21 14:12:03
  * @LastEditors: Chong Liu liuc12@miamioh.edu
