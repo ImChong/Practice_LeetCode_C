ac61a171f7362060a13a1931086428040b0faf36
Chong Liu <liuc12@miamioh.edu>
2024-01-19 08:48:01 +0800

    Update commonHashTable.h

--- F01_Common_Functions/inc/commonHashTable.h
+++ F01_Common_Functions/inc/commonHashTable.h
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-22 13:35:04
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-19 08:43:48
+ * @LastEditTime : 2024-01-19 08:47:23
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -17,7 +17,6 @@
 /*                               STRUCT DEFINITION                                */
 /*                                                                                */
 /**********************************************************************************/
-
 /* Hash node structure for index-based operations */
 struct IndexHashNode {
     int value;               /* Node value */
@@ -31,12 +30,24 @@ struct IndexHashTable {
     struct IndexHashNode **slots; /* Hash table slots */
 };
 
+/* Initialize a hash table */
+struct IndexHashTable *IndexHashTable_Init(int size);
+/* Insert a hash node */
+void IndexHashTable_Insert(struct IndexHashTable *table, int value, int index);
+/* Get number of hash nodes with the given value */
+int IndexHashTable_Search(struct IndexHashTable *table, int value);
+/* Remove a hash node */
+void IndexHashTable_Remove(struct IndexHashTable *table, int value);
+/* Print hash table */
+void IndexHashTable_Print(struct IndexHashTable *table);
+/* Free hash table */
+void IndexHashTable_Free(struct IndexHashTable *table);
+
 /**********************************************************************************/
 /*                                                                                */
 /*                               STRUCT DEFINITION                                */
 /*                                                                                */
 /**********************************************************************************/
-
 /* Hash node structure for counter-based operations */
 struct CounterHashNode {
     int value;               /* Node value */
@@ -49,6 +60,19 @@ struct CounterHashTable {
     int size;                    /* Hash table size */
     struct CounterHashNode **slots; /* Hash table slots */
 };
+
+/* Initialize a hash table */
+struct CounterHashTable *CounterHashTable_Init(int size);
+/* Insert a hash node */
+void CounterHashTable_Insert(struct CounterHashTable *table, int value, int index);
+/* Get number of hash nodes with the given value */
+int CounterHashTable_Search(struct CounterHashTable *table, int value);
+/* Remove a hash node */
+void CounterHashTable_Remove(struct CounterHashTable *table, int value);
+/* Print hash table */
+void CounterHashTable_Print(struct CounterHashTable *table);
+/* Free hash table */
+void CounterHashTable_Free(struct CounterHashTable *table);
 
 /**********************************************************************************/
 /*                                                                                */
@@ -69,11 +93,17 @@ struct HashTable {
     struct HashNode **hashSlots; /* Hash table slots */
 };
 
-struct HashTable *HashTable_Init(int size);                         /* Initialize a hash table */
-void HashTable_Insert(struct HashTable *table, int value, int index); /* Insert a hash node */
-int HashTable_Search(struct HashTable *table, int value);             /* Get number of hash nodes with the given value */
-void HashTable_Remove(struct HashTable *table, int value);            /* Remove a hash node */
-void HashTable_Print(struct HashTable *table); /* Print hash table */
-void HashTable_Free(struct HashTable *table);  /* Free hash table */
+/* Initialize a hash table */
+struct HashTable *HashTable_Init(int size);
+/* Insert a hash node */
+void HashTable_Insert(struct HashTable *table, int value, int index);
+/* Get number of hash nodes with the given value */
+int HashTable_Search(struct HashTable *table, int value);
+/* Remove a hash node */
+void HashTable_Remove(struct HashTable *table, int value);
+/* Print hash table */
+void HashTable_Print(struct HashTable *table);
+/* Free hash table */
+void HashTable_Free(struct HashTable *table);
 
 #endif /* F01_COMMON_FUNCTIONS_INC_COMMONHASHTABLE_H_ */
