d5f10383620493b2c51b03751576ed64899076cd
Chong Liu <liuc12@miamioh.edu>
2024-01-23 00:29:55 +0800

    update

--- F02_Programming_Topics/F01_Data_Structure/02_Linked_List/01_removeElements/main.c
+++ F02_Programming_Topics/F01_Data_Structure/02_Linked_List/01_removeElements/main.c
@@ -1,53 +1,22 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programmercarl_Series\02_Linked_List\01_removeElements\main.c
+ * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\02_Linked_List\01_removeElements\main.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-16 08:57:10
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-18 14:33:54
+ * @LastEditTime : 2024-01-23 00:29:23
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : 203. 移除链表元素：https://leetcode.cn/problems/remove-linked-list-elements/
  * https://programmercarl.com/0203.%E7%A7%BB%E9%99%A4%E9%93%BE%E8%A1%A8%E5%85%83%E7%B4%A0.html
  */
+/* solution header file (题解头文件) */
+#include "removeElements.h"
 /* Standard header file (标准头文件) */
 #include <stdio.h>
-#include <stdlib.h>
 /* Common function header file (通用头文件) */
 #include "commonArray.h"
 #include "commonLinkedList.h"
-
-/**********************************************************************************/
-/**********************************************************************************/
-/***                                                                            ***/
-/***                               TARGET FUNCTION                              ***/
-/***                                                                            ***/
-/**********************************************************************************/
-/**********************************************************************************/
-/**
- * @description: 移除链表元素
- * TODO: 运行时长和内存占用可提升
- * =================================================================================
- * @param {ListNode} *head              链表虚拟头节点
- * @param {int} val                     目标值
- * @return {struct ListNode} *head      返回链表虚拟头节点
- */
-struct ListNode *removeElements(struct ListNode *head, int val) {
-    struct ListNode dummy;                          /* 创建虚拟头节点 */
-    dummy.next = head;                              /* 将传入链表接在虚拟头节点后面 */
-
-    struct ListNode *curr = &dummy;                 /* 创建指针：用于遍历列表 */
-    while (curr->next != NULL) {                    /* 遍历链表 */
-        if (curr->next->val == val) {                   /* 当前节点的下一个节点的值等于目标值 */
-            struct ListNode *tmp = curr->next;              /* 保存当前节点的下一个节点 */
-            curr->next = curr->next->next;                  /* 当前节点的下一个节点指向下下个节点 */
-            free(tmp);                                      /* 释放当前节点的下一个节点 */
-        } else {                                        /* 当前节点的下一个节点的值不等于目标值 */
-            curr = curr->next;                              /* 当前节点指向下一个节点 */
-        }
-    }
-    return dummy.next;                              /* 返回链表虚拟头节点 */
-}
 
 /**********************************************************************************/
 /*                                                                                */
@@ -56,6 +25,8 @@ struct ListNode *removeElements(struct ListNode *head, int val) {
 /**********************************************************************************/
 /**
  * @description: Test 1
+ * Input: LinkedList = [1, 2, 6, 3, 4, 5, 6], RemoveVal = 6
+ * Output: [1, 2, 3, 4, 5]
  * =================================================================================
  * @return {void}
  */
@@ -82,6 +53,8 @@ void Test1(void) {
 
 /**
  * @description: Test 2
+ * Input: LinkedList = [1, 1, 1, 1, 1], RemoveVal = 1
+ * Output: []
  * =================================================================================
  * @return {void}
  */
@@ -108,6 +81,8 @@ void Test2(void) {
 
 /**
  * @description: Test 3
+ * Input: LinkedList = [1, 2, 3, 4, 5], RemoveVal = 6
+ * Output: [1, 2, 3, 4, 5]
  * =================================================================================
  * @return {void}
  */
--- F02_Programming_Topics/F01_Data_Structure/02_Linked_List/01_removeElements/src/removeElements.c
+++ F02_Programming_Topics/F01_Data_Structure/02_Linked_List/01_removeElements/src/removeElements.c
@@ -1,11 +1,49 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\02_Linked_List\01_removeElements\src\removeElements.c
+ * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\02_Linked_List\01_removeElements\src\removeElements.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-29 00:19:21
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-29 00:19:34
+ * @LastEditTime : 2024-01-23 00:27:53
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  :
+ * @Description  : Solution source file for remove elements
  */
+/* solution header file (题解头文件) */
+#include "removeElements.h"
+/* Standard header file (标准头文件) */
+#include <stdlib.h>
+/* Common function header file (通用头文件) */
+#include "commonLinkedList.h"
+
+/**********************************************************************************/
+/**********************************************************************************/
+/***                                                                            ***/
+/***                               TARGET FUNCTION                              ***/
+/***                                                                            ***/
+/**********************************************************************************/
+/**********************************************************************************/
+/**
+ * @description: 移除链表元素
+ * TODO: 运行时长和内存占用可提升
+ * =================================================================================
+ * @param {ListNode} *head              链表虚拟头节点
+ * @param {int} val                     目标值
+ * @return {struct ListNode} *head      返回链表虚拟头节点
+ */
+struct ListNode *removeElements(struct ListNode *head, int val) {
+    struct ListNode dummy;                          /* 创建虚拟头节点 */
+    dummy.next = head;                              /* 将传入链表接在虚拟头节点后面 */
+
+    struct ListNode *curr = &dummy;                 /* 创建指针：用于遍历列表 */
+    while (curr->next != NULL) {                    /* 遍历链表 */
+        if (curr->next->val == val) {                   /* 当前节点的下一个节点的值等于目标值 */
+            struct ListNode *tmp = curr->next;              /* 保存当前节点的下一个节点 */
+            curr->next = curr->next->next;                  /* 当前节点的下一个节点指向下下个节点 */
+            free(tmp);                                      /* 释放当前节点的下一个节点 */
+        } else {                                        /* 当前节点的下一个节点的值不等于目标值 */
+            curr = curr->next;                              /* 当前节点指向下一个节点 */
+        }
+    }
+    return dummy.next;                              /* 返回链表虚拟头节点 */
+}
