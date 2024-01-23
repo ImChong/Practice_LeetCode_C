cfa7f30c7c3b31bb8b16f798cc7e3e870bcedcf4
Chong Liu <liuc12@miamioh.edu>
2024-01-23 09:22:26 +0800

    Update linkedList.c

--- F02_Programming_Topics/F01_Data_Structure/02_Linked_List/00_common/00_noDummyMethod/src/linkedList.c
+++ F02_Programming_Topics/F01_Data_Structure/02_Linked_List/00_common/00_noDummyMethod/src/linkedList.c
@@ -1,11 +1,11 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\02_Linked_List\00_common\00_noDummyMethod\src\linkedList.c
+ * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\02_Linked_List\00_common\00_noDummyMethod\src\linkedList.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-29 00:21:03
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-29 00:21:55
+ * @LastEditTime : 2024-01-23 09:22:13
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
- * @Description  : This file is a place holder for the linked list implementation.
+ * @Description  : This file is a place holder for the linked list source file.
  */
