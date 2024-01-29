fed68412fb589881711300f6808c7d8f396b5fea
Chong Liu <liuc12@miamioh.edu>
2024-01-29 11:56:20 +0800

    Update main.c

--- F02_Programming_Topics/F01_Data_Structure/01_Array/01_binarySearch/main.c
+++ F02_Programming_Topics/F01_Data_Structure/01_Array/01_binarySearch/main.c
@@ -3,17 +3,17 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2024-01-10 11:06:55
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-01-26 10:31:19
+ * @LastEditTime : 2024-01-29 11:56:02
  * =================================================================================
  * Copyright (c) 2024 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : leetcode 704. 二分查找：https://leetcode.cn/problems/binary-search/
  *      https://programmercarl.com/0704.%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE
  */
-/* solution header file (题解头文件) */
-#include "binary_search.h"
 /* Standard header file (标准头文件) */
 #include <stdio.h>
+/* solution header file (题解头文件) */
+#include "binary_search.h"      /* NOLINT */
 /* Common function header file (通用头文件) */
 #include "F01_Common_Functions/inc/common_array.h"
 #include "F01_Common_Functions/inc/common_single_value.h"
