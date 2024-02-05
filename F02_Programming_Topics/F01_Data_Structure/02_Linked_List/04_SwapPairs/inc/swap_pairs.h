8287ee08bfb656cbbf67dbec6cdb2eda8ac9c4b9
Chong Liu <liuc12@miamioh.edu>
2024-02-05 11:09:53 +0800

    Update swap_pairs.h

--- F02_Programming_Topics/F01_Data_Structure/02_Linked_List/04_SwapPairs/inc/swap_pairs.h
+++ F02_Programming_Topics/F01_Data_Structure/02_Linked_List/04_SwapPairs/inc/swap_pairs.h
@@ -1,18 +1,20 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\02_Linked_List\04_swapPairs\inc\swap_pairs.h
+ * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\02_Linked_List\04_SwapPairs\inc\swap_pairs.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-29 00:48:53
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-02-02 20:02:05
+ * @LastEditTime : 2024-02-05 11:09:50
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  :
  */
+#ifndef F02_PROGRAMMING_TOPICS_F01_DATA_STRUCTURE_02_LINKED_LIST_04_SWAPPAIRS_INC_SWAP_PAIRS_H_
+#define F02_PROGRAMMING_TOPICS_F01_DATA_STRUCTURE_02_LINKED_LIST_04_SWAPPAIRS_INC_SWAP_PAIRS_H_
 
-#ifndef __FIXME
-#define __FIXME
+/* Common function header file (通用头文件) */
+#include "F01_Common_Functions/inc/common_linked_list.h"
 
 struct ListNode *SwapPairs(struct ListNode* head);
 
-#endif  /* __FIXME */
+#endif  /* F02_PROGRAMMING_TOPICS_F01_DATA_STRUCTURE_02_LINKED_LIST_04_SWAPPAIRS_INC_SWAP_PAIRS_H_ */
