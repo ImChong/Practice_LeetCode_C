851db8c2caf246ce8e49f928c53858013f500bc7
Chong Liu <liuc12@miamioh.edu>
2024-02-05 11:07:21 +0800

    Update reverse_list.h

--- F02_Programming_Topics/F01_Data_Structure/02_Linked_List/03_ReverseList/inc/reverse_list.h
+++ F02_Programming_Topics/F01_Data_Structure/02_Linked_List/03_ReverseList/inc/reverse_list.h
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-29 00:48:12
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2024-02-05 11:05:29
+ * @LastEditTime : 2024-02-05 11:07:04
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -11,6 +11,9 @@
  */
 #ifndef F02_PROGRAMMING_TOPICS_F01_DATA_STRUCTURE_02_LINKED_LIST_03_REVERSELIST_INC_REVERSE_LIST_H_
 #define F02_PROGRAMMING_TOPICS_F01_DATA_STRUCTURE_02_LINKED_LIST_03_REVERSELIST_INC_REVERSE_LIST_H_
+
+/* Common function header file (通用头文件) */
+#include "F01_Common_Functions/inc/common_linked_list.h"
 
 struct ListNode *ReverseList(struct ListNode* head);
 
