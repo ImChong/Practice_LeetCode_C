bb3c89adc52d04b29c8dcc4f0fc675ff6a699d9e
Chong Liu <liuc12@miamioh.edu>
2024-02-08 21:57:31 +1300

    update

--- F02_Programming_Topics/F01_Data_Structure/02_Linked_List/09_MergeTwoLists/inc/merge_two_lists.h
+++ F02_Programming_Topics/F01_Data_Structure/02_Linked_List/09_MergeTwoLists/inc/merge_two_lists.h
@@ -1,11 +1,21 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\02_Linked_List\08_mergeTwoLists\inc\mergeTwoLists.h
+ * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\02_Linked_List\09_MergeTwoLists\inc\merge_two_lists.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-12-29 00:53:11
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-12-29 00:53:12
+ * @LastEditTime : 2024-02-08 21:55:17
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  :
  */
+
+#ifndef F02_PROGRAMMING_TOPICS_F01_DATA_STRUCTURE_02_LINKED_LIST_09_MERGETWOLISTS_INC_MERGE_TWO_LISTS_H_
+#define F02_PROGRAMMING_TOPICS_F01_DATA_STRUCTURE_02_LINKED_LIST_09_MERGETWOLISTS_INC_MERGE_TWO_LISTS_H_
+
+/* Common function header file (通用头文件) */
+#include "F01_Common_Functions/inc/common_linked_list.h"
+
+struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2);
+
+#endif  /* F02_PROGRAMMING_TOPICS_F01_DATA_STRUCTURE_02_LINKED_LIST_09_MERGETWOLISTS_INC_MERGE_TWO_LISTS_H_ */
--- F02_Programming_Topics/F01_Data_Structure/02_Linked_List/09_MergeTwoLists/main.c
+++ F02_Programming_Topics/F01_Data_Structure/02_Linked_List/09_MergeTwoLists/main.c
@@ -1,9 +1,9 @@
 /*
- * @FilePath     : \Practice_LeetCode_C\01_Programming_Topics\02_Linked_List\08_mergeTwoLists\main.c
+ * @FilePath     : \Practice_LeetCode_C\F02_Programming_Topics\F01_Data_Structure\02_Linked_List\09_MergeTwoLists\main.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-08-18 23:18:36
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-11-26 12:08:04
+ * @LastEditTime : 2024-02-08 21:54:15
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -14,53 +14,11 @@
 /* Standard header file (标准头文件) */
 #include <stdio.h>
 #include <stdlib.h>
+/* solution header file (题解头文件) */
+#include "F02_Programming_Topics/F01_Data_Structure/02_Linked_List/09_MergeTwoLists/inc/merge_two_lists.h"
 /* Common function header file (通用头文件) */
 #include "F01_Common_Functions/inc/common_array.h"
 #include "F01_Common_Functions/inc/common_linked_list.h"
-
-/**********************************************************************************/
-/**********************************************************************************/
-/***                                                                            ***/
-/***                               TARGET FUNCTION                              ***/
-/***                                                                            ***/
-/**********************************************************************************/
-/**********************************************************************************/
-/**
- * @description: 合并两个有序链表
- * =================================================================================
- * @param {ListNode} *list1     链表1
- * @param {ListNode} *list2     链表2
- * @return {ListNode} *head     合并后的链表头节点
- */
-struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2) {
-    if (!list1) {
-        return list2;
-    }
-    if (!list2) {
-        return list1;
-    }
-
-    struct ListNode *head = (struct ListNode *)malloc(sizeof(struct ListNode));
-    struct ListNode *currNode = head;
-
-    while (list1 && list2) {
-        if (list1->val < list2->val) {
-            currNode->next = list1;
-            list1 = list1->next;
-        } else {
-            currNode->next = list2;
-            list2 = list2->next;
-        }
-        currNode = currNode->next;
-    }
-
-    if (list1) {
-        currNode->next = list1;
-    } else {
-        currNode->next = list2;
-    }
-    return head->next;
-}
 
 /**********************************************************************************/
 /*                                                                                */
